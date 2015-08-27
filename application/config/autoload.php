c3a2f89811220fe22b6b6176b65b55e30fe471de
Lonnie Ezell <lonnieje@gmail.com>
2015-08-27 00:09:44 -0500

    Initial skeleton config files for Autoloader and DI Container.

---
+++ application/config/autoload.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------
+ * AUTO-LOADER
+ * -------------------------------------------------------------------
+ * This file defines the namespaces and class maps so the Autoloader
+ * can find the files as needed.
+ */
+
+/**
+ * -------------------------------------------------------------------
+ * Namespaces
+ * -------------------------------------------------------------------
+ * This maps the locations of any namespaces in your application
+ * to their location on the file system. These are used by the
+ * Autoloader to locate files the first time they have been instantiated.
+ *
+ * The '/application' and '/system' directories are already mapped for
+ * you. You may change the name of the 'App' namespace if you wish,
+ * but this should be done prior to creating any namespaced classes,
+ * else you will need to modify all of those classes for this to work.
+ *
+ * DO NOT change the name of the CodeIgniter namespace or your application
+ * WILL break. *
+ * Prototype:
+ *
+ *   $config['psr4'] = [
+ *       'CodeIgniter' => SYSPATH
+ *   `];
+ */
+$config['psr4'] = [
+	'App'         => realpath(APPPATH),
+	'CodeIgniter' => realpath(SYSPATH),
+];
+
+/**
+ * -------------------------------------------------------------------
+ * Class Map
+ * -------------------------------------------------------------------
+ * The class map provides a map of class names and their exact
+ * location on the drive. Classes loaded in this manner will have
+ * slightly faster performance because they will not have to be
+ * searched for within one or more directories as they would if they
+ * were being autoloaded through a namespace.
+ *
+ * Prototype:
+ *
+ *   $config['classmap'] = [
+ *       'MyClass'   => '/path/to/class/file.php'
+ *   ];
+ */
+$config['classmap'] = [];
---
+++ application/config/services.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------
+ * SERVICES
+ * -------------------------------------------------------------------
+ * This file contains a map of name-spaced classes and their aliases.
+ * These are used by the DI class to provide instances of classes.
+ *
+ * The 'alias' (key) is how you will reference the class instance through DI.
+ * The class name (value) is the fully name-spaced class name to use.
+ * If you want to substitute a different class in place of the current one,
+ * just change the name of the class to the fully name-spaced class you
+ * want to use.
+ *
+ * Examples:
+ *      $ci = \CodeIgniter\DI::getInstance();
+ *      $bm = $ci->benchmark;
+ *      $ci->benchmark->mark('some_mark_start');
+ */
+
+$config['services'] = [
+
+	// alias            class name
+	//--------------------------------------------------------------------
+
+	// The core CodeIgniter files
+	'benchmark'         => '\CodeIgniter\Benchmark\Benchmark',
+	'config'            => '\CodeIgniter\Config\Config',
+	'logger'            => '\CodeIgniter\Log\Log',
+	'router'            => '\CodeIgniter\Router\Router',
+	'routes'            => '\CodeIgniter\Router\RouteCollection',
+
+	// Your custom files can be added here.
+];
+
+return $config;
