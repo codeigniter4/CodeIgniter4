b429f7d368e0a26e13767646684ea74a5e8fe8c5
Lonnie Ezell <lonnieje@gmail.com>
2015-10-04 22:18:04 -0500

    Provide a convenience factor to the DI() method to allow you to retrieve a single instance of a class.

--- system/Common.php
+++ system/Common.php
@@ -14,10 +14,18 @@ if (! function_exists('DI'))
 	 * A convenience method for getting the current instance
 	 * of the dependency injection container.
 	 *
+	 * If a class "alias" is passed in as the first parameter
+	 * then try to create that class using the single() method.
+	 *
 	 * @return \CodeIgniter\DI\DI instance
 	 */
-	function DI()
+	function DI($alias=null)
 	{
+		if (! empty($alias) && is_string($alias))
+		{
+			return \CodeIgniter\DI\DI::getInstance()->single($alias);
+		}
+
 		return \CodeIgniter\DI\DI::getInstance();
 	}
 }
