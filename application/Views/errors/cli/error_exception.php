c946a99505f36a2a73075d9db4ff4b32e82c10b2
kenjis <kenji.uui@gmail.com>
2016-03-29 15:59:48 +0900

    Fix layout of cli error exception template

--- application/Views/errors/cli/error_exception.php
+++ application/Views/errors/cli/error_exception.php
@@ -9,13 +9,13 @@ Line Number: <?php echo $exception->getLine(); ?>
 
 <?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
 
-	Backtrace:
-	<?php foreach ($exception->getTrace() as $error): ?>
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
-			File: <?php echo $error['file'], "\n"; ?>
-			Line: <?php echo $error['line'], "\n"; ?>
-			Function: <?php echo $error['function'], "\n\n"; ?>
-		<?php endif ?>
-	<?php endforeach ?>
+Backtrace:
+<?php	foreach ($exception->getTrace() as $error): ?>
+<?php		if (isset($error['file'])): ?>
+	File: <?php echo $error['file'], "\n"; ?>
+	Line: <?php echo $error['line'], "\n"; ?>
+	Function: <?php echo $error['function'], "\n\n"; ?>
+<?php		endif ?>
+<?php	endforeach ?>
 
 <?php endif ?>
