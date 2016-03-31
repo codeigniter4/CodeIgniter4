f300806b2a8a206f34a3ef8ae0d40fde078ba835
Lonnie Ezell <lonnieje@gmail.com>
2016-03-30 23:36:14 -0500

    Adding Grouping tests for Query Builder

--- tests/system/Database/Builder/GroupTest.php
+++ tests/system/Database/Builder/GroupTest.php
@@ -44,4 +44,72 @@ class GroupTest extends \CIUnitTestCase
 	}
 
 	//--------------------------------------------------------------------
+
+	public function testAndGroups()
+	{
+		$builder = new BaseBuilder('user', $this->db);
+
+		$builder->groupStart()
+					->where('id >', 3)
+					->where('name !=', 'Luke')
+				->groupEnd()
+				->where('name', 'Darth');
+
+		$expectedSQL   = "SELECT * FROM \"user\" WHERE   ( \"id\" > :id AND \"name\" != :name  ) AND \"name\" = :name0";
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testOrGroups()
+	{
+		$builder = new BaseBuilder('user', $this->db);
+
+		$builder->where('name', 'Darth')
+				->orGroupStart()
+		            ->where('id >', 3)
+		            ->where('name !=', 'Luke')
+		        ->groupEnd();
+
+		$expectedSQL   = "SELECT * FROM \"user\" WHERE \"name\" = :name OR   ( \"id\" > :id AND \"name\" != :name0  )";
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testNotGroups()
+	{
+		$builder = new BaseBuilder('user', $this->db);
+
+		$builder->where('name', 'Darth')
+		        ->notGroupStart()
+		        ->where('id >', 3)
+		        ->where('name !=', 'Luke')
+		        ->groupEnd();
+
+		$expectedSQL   = "SELECT * FROM \"user\" WHERE \"name\" = :name AND NOT   ( \"id\" > :id AND \"name\" != :name0  )";
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testOrNotGroups()
+	{
+		$builder = new BaseBuilder('user', $this->db);
+
+		$builder->where('name', 'Darth')
+		        ->orNotGroupStart()
+		        ->where('id >', 3)
+		        ->where('name !=', 'Luke')
+		        ->groupEnd();
+
+		$expectedSQL   = "SELECT * FROM \"user\" WHERE \"name\" = :name OR NOT   ( \"id\" > :id AND \"name\" != :name0  )";
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
+	}
+
+	//--------------------------------------------------------------------
 }
