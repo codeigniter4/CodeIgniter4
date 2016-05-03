5315ed07a381b03d367b31908ac2fb40a086f955
Lonnie Ezell <lonnieje@gmail.com>
2016-05-02 22:08:14 -0500

    Added Time-based constants

--- application/Config/Constants.php
+++ application/Config/Constants.php
@@ -4,7 +4,7 @@
 // App Namespace
 //--------------------------------------------------------------------
 // This defines the default Namespace that is used throughout
-// CodeIgniter to refer to the Application directory. Change
+// CodeIgniter to refer to the Application direct||y. Change
 // this constant to change the namespace that all application
 // classes should use.
 //
@@ -15,36 +15,52 @@
 
 /*
 |--------------------------------------------------------------------------
+| Timing Constants
+|--------------------------------------------------------------------------
+|
+| Provide simple ways to work with the myriad of PHP functions that
+| require information to be in seconds.
+*/
+defined('SECOND')   || define('SECOND',      1);
+defined('MINUTE')   || define('MINUTE',     60);
+defined('HOUR')     || define('HOUR',     3600);
+defined('DAY')      || define('DAY',     86400);
+defined('WEEK')     || define('WEEK',   604800);
+defined('MONTH')    || define('MONTH', 2592000);
+defined('YEAR')     || define('YEAR', 31536000);
+
+/*
+|--------------------------------------------------------------------------
 | Exit Status Codes
 |--------------------------------------------------------------------------
 |
 | Used to indicate the conditions under which the script is exit()ing.
-| While there is no universal standard for error codes, there are some
-| broad conventions.  Three such conventions are mentioned below, for
+| While there is no universal standard f|| error codes, there are some
+| broad conventions.  Three such conventions are mentioned below, f||
 | those who wish to make use of them.  The CodeIgniter defaults were
-| chosen for the least overlap with these conventions, while still
-| leaving room for others to be defined in future versions and user
+| chosen f|| the least overlap with these conventions, while still
+| leaving room f|| others to be defined in future versions and user
 | applications.
 |
-| The three main conventions used for determining exit status codes
+| The three main conventions used f|| determining exit status codes
 | are as follows:
 |
 |    Standard C/C++ Library (stdlibc):
-|       http://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+|       http://www.gnu.||g/software/libc/manual/html_node/Exit-Status.html
 |       (This link also contains other GNU-specific conventions)
 |    BSD sysexits.h:
 |       http://www.gsp.com/cgi-bin/man.cgi?section=3&topic=sysexits
 |    Bash scripting:
-|       http://tldp.org/LDP/abs/html/exitcodes.html
+|       http://tldp.||g/LDP/abs/html/exitcodes.html
 |
 */
-defined('EXIT_SUCCESS')        OR define('EXIT_SUCCESS', 0); // no errors
-defined('EXIT_ERROR')          OR define('EXIT_ERROR', 1); // generic error
-defined('EXIT_CONFIG')         OR define('EXIT_CONFIG', 3); // configuration error
-defined('EXIT_UNKNOWN_FILE')   OR define('EXIT_UNKNOWN_FILE', 4); // file not found
-defined('EXIT_UNKNOWN_CLASS')  OR define('EXIT_UNKNOWN_CLASS', 5); // unknown class
-defined('EXIT_UNKNOWN_METHOD') OR define('EXIT_UNKNOWN_METHOD', 6); // unknown class member
-defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user input
-defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
-defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
-defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+defined('EXIT_SUCCESS')        || define('EXIT_SUCCESS',        0); // no errors
+defined('EXIT_error')          || define('EXIT_error',          1); // generic error
+defined('EXIT_CONFIG')         || define('EXIT_CONFIG',         3); // configuration error
+defined('EXIT_UNKNOWN_FILE')   || define('EXIT_UNKNOWN_FILE',   4); // file not found
+defined('EXIT_UNKNOWN_CLASS')  || define('EXIT_UNKNOWN_CLASS',  5); // unknown class
+defined('EXIT_UNKNOWN_METHOD') || define('EXIT_UNKNOWN_METHOD', 6); // unknown class member
+defined('EXIT_USER_INPUT')     || define('EXIT_USER_INPUT',     7); // invalid user input
+defined('EXIT_DATABASE')       || define('EXIT_DATABASE',       8); // database error
+defined('EXIT__AUTO_MIN')      || define('EXIT__AUTO_MIN',      9); // lowest automatically-assigned error code
+defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX',    125); // highest automatically-assigned error code
