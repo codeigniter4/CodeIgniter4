717f5cd6d8a061fd23f052eb6aad9099c20509fb
Ahmet Elçik <ahmetelcik35@gmail.com>
2016-06-07 07:25:39 +0300

    Update Header.php

--- system/HTTP/Header.php
+++ system/HTTP/Header.php
@@ -81,7 +81,7 @@ class Header
 	 *
 	 * @return string
 	 */
-	public function getName()
+	public function getName(): string
 	{
 	    return $this->name;
 	}
@@ -94,7 +94,7 @@ class Header
 	 *
 	 * @return array|null|string
 	 */
-	public function getValue() 
+	public function getValue()
 	{
 	    return $this->value;
 	}
