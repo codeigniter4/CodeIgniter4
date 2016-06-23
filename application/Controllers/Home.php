b176c2853b39472366332cac64e277c8293d26e6
Ricardo Soares <rsoares@outlook.com>
2016-06-23 14:24:51 +0000

    merge fork

--- application/Controllers/Home.php
+++ application/Controllers/Home.php
@@ -5,6 +5,18 @@ class Home extends \CodeIgniter\Controller
 
 	public function index()
 	{
+        $db = \Config\Database::connect('default', true);
+        $dados = $db->query("SELECT * FROM NEW_TABLE ORDER BY ID DESC")->getRow();
+        
+        //var_dump("INSERT INTO NEW_TABLE (NOME) VALUES (".$db->escapeString("sdsdsad").")"); die();
+        
+        //$db->query("INSERT INTO NEW_TABLE (NOME) VALUES (".$db->escapeString("asas'd").")");
+        
+        var_dump($db->escapeString(["sdsdsa'd", "sdasd"]));  
+        if (is_null($dados)) { 
+            die();
+        }
+        
 		echo view('welcome_message');
 	}
 
