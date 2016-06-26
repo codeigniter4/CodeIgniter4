6f6290c129141c6a87cd254c3e04a98f956a212d
Instructor, Computer Systems Technology <jim_parry@bcit.ca>
2016-06-26 11:06:30 -0700

    Update welcome_message.php
    Fixed incorrect user guide link!

--- application/Views/welcome_message.php
+++ application/Views/welcome_message.php
@@ -125,7 +125,7 @@
 
 				<p>If you are exploring CodeIgniter for the very first time, you 
 					should start by reading the (in progress) 
-					<a href="http://codeigniter.com/ci4-ug">User Guide</a>.</p>
+					<a href="https://bcit-ci.github.io/CodeIgniter4">User Guide</a>.</p>
 
 			</div>
 
