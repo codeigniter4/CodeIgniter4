0242b6a00e64be9d62bd2e1967ea817c0e641ae6
Lonnie Ezell <lonnieje@gmail.com>
2016-06-27 22:31:49 -0500

    Memcached cache driver

--- application/Config/Cache.php
+++ application/Config/Cache.php
@@ -66,6 +66,24 @@ class Cache extends BaseConfig
 	public $prefix = '';
 
 	/*
+	| -------------------------------------------------------------------------
+	| Memcached settings
+	| -------------------------------------------------------------------------
+	| Your Memcached servers can be specified below, if you are using
+	| the Memcached drivers.
+	|
+	|	See: https://codeigniter.com/user_guide/libraries/caching.html#memcached
+	|
+	*/
+	public $memcached = [
+		'default' => [
+			'host'   => '127.0.0.1',
+			'port'   => 11211,
+			'weight' => 1
+		]
+	];
+
+	/*
 	|--------------------------------------------------------------------------
 	| Available Cache Handlers
 	|--------------------------------------------------------------------------
