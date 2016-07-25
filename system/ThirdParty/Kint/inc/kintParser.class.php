e71ea2ed1aceabacdeface279ae1cc8208dbf3b3
Master Yoda <jim_parry@bcit.ca>
2016-07-25 10:38:28 -0700

    Revert inadvertent change inside Kint

--- system/ThirdParty/Kint/inc/kintParser.class.php
+++ system/ThirdParty/Kint/inc/kintParser.class.php
@@ -129,7 +129,7 @@ abstract class kintParser extends kintVariableData
 				$parser       = new $className;
 				$parser->name = $name; # the parser may overwrite the name value, so set it first
 
-				if ( $parser->parse( $variable ) !== false ) {
+				if ( $parser->_parse( $variable ) !== false ) {
 					$varData->_alternatives[] = $parser;
 				}
 			}
