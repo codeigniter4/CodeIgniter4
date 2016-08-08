0b8fb3182493132c70e989b488af5e9cbf15e3aa
Lonnie Ezell <lonnieje@gmail.com>
2016-08-08 00:39:00 -0500

    Basic tests for prepared queries, and fixing the select test issue.

---
+++ tests/system/Database/Live/PreparedQueryTest.php
@@ -0,0 +1,55 @@
+<?php namespace CodeIgniter\Database\Live;
+
+use CodeIgniter\Database\BasePreparedQuery;
+
+class PreparedQueryTest extends \CIDatabaseTestCase
+{
+	protected $refresh = true;
+
+	protected $seed = 'CITestSeeder';
+
+	//--------------------------------------------------------------------
+
+	public function testPrepareReturnsPreparedQuery()
+	{
+	    $query = $this->db->prepare(function($db){
+	    	return $db->table('user')->insert([
+	    		'name' => 'a',
+				'email' => 'b@example.com'
+			]);
+		});
+
+		$this->assertTrue($query instanceof BasePreparedQuery);
+
+		$ec = $this->db->escapeChar;
+		$pre = $this->db->DBPrefix;
+
+		$expected = "INSERT INTO {$ec}{$pre}user{$ec} ({$ec}name{$ec}, {$ec}email{$ec}) VALUES (?, ?)";
+		$this->assertEquals($expected, $query->getQueryString());
+
+		$query->close();
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testExecuteRunsQueryAndReturnsResultObject()
+	{
+		$query = $this->db->prepare(function($db){
+			return $db->table('user')->insert([
+				'name' => 'a',
+				'email' => 'b@example.com'
+			]);
+		});
+
+		$query->execute('foo', 'foo@example.com');
+		$query->execute('bar', 'bar@example.com');
+
+		$this->seeInDatabase($this->db->DBPrefix.'user', ['name' => 'foo', 'email' => 'foo@example.com']);
+		$this->seeInDatabase($this->db->DBPrefix.'user', ['name' => 'bar', 'email' => 'bar@example.com']);
+
+		$query->close();
+	}
+
+	//--------------------------------------------------------------------
+
+}
