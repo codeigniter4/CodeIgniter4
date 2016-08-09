65b0e847459ab6474becab06579c251af590168a
Diego Sala <dsala@clinicadecot.org>
2016-08-10 00:48:03 +0200

    Update TextHelperTest.php

--- tests/system/Helpers/TextHelperTest.php
+++ tests/system/Helpers/TextHelperTest.php
@@ -2,9 +2,8 @@
 
 class TextHelperTest extends \CIUnitTestCase
 {
-    private $_long_string = 'Once upon a time, a framework had no tests.  It sad.
-                So some nice people began to write tests.  The more time that went on,
-                the happier it became.  Everyone was happy.';
+    private $_long_string = 'Once upon a time, a framework had no tests. It sad. So some nice people began to write tests.
+    The more time that went on, the happier it became.  Everyone was happy.';
     
     public function setUp()
     {
@@ -239,7 +238,8 @@ class TextHelperTest extends \CIUnitTestCase
     public function test_excerpt()
     {
         $string = $this->_long_string;
-        $result = ' Once upon a time, a framework had no tests. It sad. So some nice people began to write tests. The  ...';
+        $result = 'Once upon a time, a framework had no tests. It sad. So some nice people began to write tests.
+    The more time that went on, the happier it became.  Everyone was happy. ...';
         $this->assertEquals(excerpt($string), $result);
     }
     
