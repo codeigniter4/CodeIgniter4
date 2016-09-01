8106fbc2bc68967e99a20313dc11a1746129fc23
Lonnie Ezell <lonnieje@gmail.com>
2016-08-31 23:48:34 -0500

    Fleshing out the remaining stock rules. Next up - some better required rules

--- system/Validation/Rules.php
+++ system/Validation/Rules.php
@@ -5,11 +5,81 @@ use Config\Database;
 class Rules
 {
 	/**
+	 * Alpha
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function alpha(string $str): bool
+	{
+		return ctype_alpha($str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Alpha-numeric with underscores and dashes
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function alpha_dash(string $str): bool
+	{
+		return (bool)preg_match('/^[a-z0-9_-]+$/i', $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Alpha-numeric
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function alpha_numeric(string $str): bool
+	{
+		return ctype_alnum((string)$str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Alpha-numeric w/ spaces
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function alpha_numeric_spaces(string $str): bool
+	{
+		return (bool)preg_match('/^[A-Z0-9 ]+$/i', $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Decimal number
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function decimal(string $str): bool
+	{
+		return (bool)preg_match('/^[\-+]?[0-9]+\.[0-9]+$/', $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
 	 * The value does not match another field in $data.
 	 *
 	 * @param string $str
 	 * @param string $field
-	 * @param array  $data   Other field/value pairs
+	 * @param array  $data Other field/value pairs
 	 *
 	 * @return bool
 	 */
@@ -23,13 +93,118 @@ class Rules
 	//--------------------------------------------------------------------
 
 	/**
+	 * Returns true if $str is $val characters long.
+	 *
+	 * @param string $str
+	 * @param string $val
+	 * @param array  $data
+	 *
+	 * @return bool
+	 */
+	public function exact_length(string $str, string $val, array $data): bool
+	{
+		if (! is_numeric($val))
+		{
+			return false;
+		}
+
+		return ((int)$val == mb_strlen($str));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Greater than
+	 *
+	 * @param    string
+	 * @param    int
+	 *
+	 * @return    bool
+	 */
+	public function greater_than(string $str, string $min): bool
+	{
+		return is_numeric($str) ? ($str > $min) : false;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Equal to or Greater than
+	 *
+	 * @param    string
+	 * @param    int
+	 *
+	 * @return    bool
+	 */
+	public function greater_than_equal_to(string $str, string $min): bool
+	{
+		return is_numeric($str) ? ($str >= $min) : false;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Value should be within an array of values
+	 *
+	 * @param	string
+	 * @param	string
+	 * @return	bool
+	 */
+	public function in_list(string $value, string $list, array $data): bool
+	{
+		return in_array($value, explode(',', $list), TRUE);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Integer
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function integer(string $str): bool
+	{
+		return (bool)preg_match('/^[\-+]?[0-9]+$/', $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Is a Natural number  (0,1,2,3, etc.)
+	 *
+	 * @param	string
+	 * @return	bool
+	 */
+	public function is_natural(string $str): bool
+	{
+		return ctype_digit((string) $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Is a Natural number, but not a zero  (1,2,3, etc.)
+	 *
+	 * @param	string
+	 * @return	bool
+	 */
+	public function is_natural_no_zero(string $str): bool
+	{
+		return ($str != 0 && ctype_digit((string) $str));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
 	 * Checks the database to see if the given value is unique. Can
 	 * ignore a single record by field/value to make it useful during
 	 * record updates.
 	 *
 	 * Example:
-	 * 	is_unique[table.field,ignore_field,ignore_value]
-	 * 	is_unique[users.email,id,5]
+	 *    is_unique[table.field,ignore_field,ignore_value]
+	 *    is_unique[users.email,id,5]
 	 *
 	 * @param string $str
 	 * @param string $field
@@ -43,17 +218,49 @@ class Rules
 		list($field, $ignoreField, $ignoreValue) = array_pad(explode(',', $field), 3, null);
 
 		// Break the table and field apart
-	    sscanf($field, '%[^.].%[^.]', $table, $field);
+		sscanf($field, '%[^.].%[^.]', $table, $field);
 
-		$db = Database::connect();
-		$row = $db->table($table)->where($field, $str);
+		$db  = Database::connect();
+		$row = $db->table($table)
+				  ->where($field, $str);
 
-		if (! empty($ignoreField) && !empty($ignoreValue))
+		if (! empty($ignoreField) && ! empty($ignoreValue))
 		{
 			$row = $row->where("{$ignoreField} !=", $ignoreValue);
 		}
 
-		return (bool)($row->get()->getRow() === null);
+		return (bool)($row->get()
+						  ->getRow() === null);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Less than
+	 *
+	 * @param    string
+	 * @param    int
+	 *
+	 * @return    bool
+	 */
+	public function less_than(string $str, string $max): bool
+	{
+		return is_numeric($str) ? ($str < $max) : false;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Equal to or Less than
+	 *
+	 * @param    string
+	 * @param    int
+	 *
+	 * @return    bool
+	 */
+	public function less_than_equal_to(string $str, string $max): bool
+	{
+		return is_numeric($str) ? ($str <= $max) : false;
 	}
 
 	//--------------------------------------------------------------------
@@ -63,7 +270,7 @@ class Rules
 	 *
 	 * @param string $str
 	 * @param string $field
-	 * @param array  $data   Other field/value pairs
+	 * @param array  $data Other field/value pairs
 	 *
 	 * @return bool
 	 */
@@ -77,11 +284,68 @@ class Rules
 	//--------------------------------------------------------------------
 
 	/**
+	 * Returns true if $str is $val or fewer characters in length.
+	 *
+	 * @param string $str
+	 * @param string $val
+	 * @param array  $data
+	 *
+	 * @return bool
+	 */
+	public function max_length(string $str, string $val, array $data): bool
+	{
+		if (! is_numeric($val))
+		{
+			return false;
+		}
+
+		return ($val >= mb_strlen($str));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns true if $str is at least $val length.
+	 *
+	 * @param string $str
+	 * @param string $val
+	 * @param array  $data
+	 *
+	 * @return bool
+	 */
+	public function min_length(string $str, string $val, array $data): bool
+	{
+		if (! is_numeric($val))
+		{
+			return false;
+		}
+
+		return ($val <= mb_strlen($str));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Numeric
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function numeric(string $str): bool
+	{
+		return (bool)preg_match('/^[\-+]?[0-9]*\.?[0-9]+$/', $str);
+
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
 	 * Compares value against a regular expression pattern.
 	 *
 	 * @param string $str
 	 * @param string $pattern
-	 * @param array  $data     Other field/value pairs
+	 * @param array  $data Other field/value pairs
 	 *
 	 * @return bool
 	 */
@@ -100,12 +364,138 @@ class Rules
 	/**
 	 * Required
 	 *
-	 * @param	string
-	 * @return	bool
+	 * @param    string
+	 *
+	 * @return    bool
 	 */
 	public function required($str): bool
 	{
-		return is_array($str) ? (bool) count($str) : (trim($str) !== '');
+		return is_array($str) ? (bool)count($str) : (trim($str) !== '');
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Valid Base64
+	 *
+	 * Tests a string for characters outside of the Base64 alphabet
+	 * as defined by RFC 2045 http://www.faqs.org/rfcs/rfc2045
+	 *
+	 * @param	string
+	 * @return	bool
+	 */
+	public function valid_base64(string $str): bool
+	{
+		return (base64_encode(base64_decode($str)) === $str);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Checks for a correctly formatted email address
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function valid_email(string $str): bool
+	{
+		if (function_exists('idn_to_ascii') && $atpos = strpos($str, '@'))
+		{
+			$str = substr($str, 0, ++$atpos).idn_to_ascii(substr($str, $atpos));
+		}
+
+		return (bool)filter_var($str, FILTER_VALIDATE_EMAIL);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Valid Emails
+	 *
+	 * @param    string
+	 *
+	 * @return    bool
+	 */
+	public function valid_emails(string $str): bool
+	{
+		if (strpos($str, ',') === false)
+		{
+			return $this->valid_email(trim($str));
+		}
+
+		foreach (explode(',', $str) as $email)
+		{
+			if (trim($email) !== '' && $this->valid_email(trim($email)) === false)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Validate an IP address
+	 *
+	 * @param        $ip     IP Address
+	 * @param string $which  IP protocol: 'ipv4' or 'ipv6'
+	 * @param array  $data
+	 *
+	 * @return bool
+	 */
+	public function valid_ip(string $ip, string $which = null, array $data): bool
+	{
+		switch (strtolower($which))
+		{
+			case 'ipv4':
+				$which = FILTER_FLAG_IPV4;
+				break;
+			case 'ipv6':
+				$which = FILTER_FLAG_IPV6;
+				break;
+			default:
+				$which = null;
+				break;
+		}
+
+		return (bool)filter_var($ip, FILTER_VALIDATE_IP, $which);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Checks a URL to ensure it's formed correctly.
+	 *
+	 * @param string $str
+	 *
+	 * @return bool
+	 */
+	public function valid_url(string $str): bool
+	{
+		if (empty($str))
+		{
+			return false;
+		}
+		elseif (preg_match('/^(?:([^:]*)\:)?\/\/(.+)$/', $str, $matches))
+		{
+			if (empty($matches[2]))
+			{
+				return false;
+			}
+			elseif (! in_array($matches[1], ['http', 'https'], true))
+			{
+				return false;
+			}
+
+			$str = $matches[2];
+		}
+
+		$str = 'http://'.$str;
+
+		return (filter_var($str, FILTER_VALIDATE_URL) !== false);
 	}
 
 	//--------------------------------------------------------------------
--- tests/system/Validation/ValidationTest.php
+++ tests/system/Validation/ValidationTest.php
@@ -358,4 +358,805 @@ class ValidationTest extends \CIUnitTestCase
 
 	//--------------------------------------------------------------------
 
+	public function testMinLengthReturnsFalseWithNonNumericVal()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'min_length[bar]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMinLengthReturnsTrueWithSuccess()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'min_length[2]'
+		]);
+
+		$this->assertTrue($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMinLengthReturnsTrueWithExactLength()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'min_length[3]'
+		]);
+
+		$this->assertTrue($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMinLengthReturnsFalseWhenWrong()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'min_length[4]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMaxLengthReturnsFalseWithNonNumericVal()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'max_length[bar]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMaxLengthReturnsTrueWithSuccess()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'max_length[4]'
+		]);
+
+		$this->assertTrue($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMaxLengthReturnsTrueWithExactLength()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'max_length[3]'
+		]);
+
+		$this->assertTrue($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testMaxLengthReturnsFalseWhenWrong()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'max_length[2]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testExactLengthReturnsTrueOnSuccess()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'exact_length[3]'
+		]);
+
+		$this->assertTrue($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testExactLengthReturnsFalseWhenShort()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'exact_length[2]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testExactLengthReturnsFalseWhenLong()
+	{
+		$data = [
+			'foo' => 'bar',
+		];
+
+		$this->validation->setRules([
+			'foo' => 'exact_length[4]'
+		]);
+
+		$this->assertFalse($this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider urlProvider
+	 */
+	public function testValidURL(string $url, bool $expected)
+	{
+		$data = [
+			'foo' => $url,
+		];
+
+		$this->validation->setRules([
+			'foo' => 'valid_url'
+		]);
+
+	    $this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function urlProvider()
+	{
+	    return [
+	    	['www.codeigniter.com', true],
+			['http://codeigniter.com', true],
+			//https://bugs.php.net/bug.php?id=51192
+			['http://accept-dashes.tld', true],
+			['http://reject_underscores', false],
+			// https://github.com/bcit-ci/CodeIgniter/issues/4415
+			['http://[::1]/ipv6', true],
+			['htt://www.codeigniter.com', false],
+			['', false],
+			['code igniter', false]
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider emailProviderSingle
+	 *
+	 * @param $email
+	 * @param $expected
+	 */
+	public function testValidEmail($email, $expected)
+	{
+		$data = [
+			'foo' => $email,
+		];
+
+		$this->validation->setRules([
+			'foo' => 'valid_email'
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider emailProviderSingle
+	 *
+	 * @param $email
+	 * @param $expected
+	 */
+	public function testValidEmails($email, $expected)
+	{
+		$data = [
+			'foo' => $email,
+		];
+
+		$this->validation->setRules([
+			'foo' => 'valid_emails'
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function emailProviderSingle()
+	{
+	    return [
+	    	['email@sample.com', true],
+			['valid_email', false]
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	public function emailsProvider()
+	{
+	    return [
+			['1@sample.com,2@sample.com', true],
+			['1@sample.com, 2@sample.com', true],
+			['email@sample.com', true],
+			['@sample.com,2@sample.com,validemail@email.ca', false]
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider ipProvider
+	 *
+	 * @param $ip
+	 * @param $which
+	 * @param $expected
+	 */
+	public function testValidIP($ip, $which, $expected)
+	{
+		$data = [
+			'foo' => $ip,
+		];
+
+		$this->validation->setRules([
+			'foo' => "valid_ip[{$which}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function ipProvider()
+	{
+	    return [
+	    	['127.0.0.1', null, true],
+			['127.0.0.1', 'ipv4', true],
+			['2001:0db8:85a3:0000:0000:8a2e:0370:7334', null, true],
+			['2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'ipv6', true],
+			['2001:0db8:85a3:0000:0000:8a2e:0370:7334', 'ipv4', false],
+			['127.0.0.1', 'ipv6', false],
+			['H001:0db8:85a3:0000:0000:8a2e:0370:7334', null, false],
+			['127.0.0.259', null, false]
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider alphaProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testAlpha($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "alpha"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function alphaProvider()
+	{
+	    return [
+	    	['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ', true],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ ', false],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ1', false],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ*', false]
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider alphaNumericProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testAlphaNumeric($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "alpha_numeric"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function alphaNumericProvider()
+	{
+		return [
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789', true],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789\ ', false],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789_', false],
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider alphaNumericProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testAlphaNumericSpace($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "alpha_numeric_spaces"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function alphaNumericSpaceProvider()
+	{
+		return [
+			[' abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789', true],
+			[' abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789-', false],
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider alphaDashProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testAlphaDash($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "alpha_dash"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function alphaDashProvider()
+	{
+		return [
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789-', true],
+			['abcdefghijklmnopqrstuvwxyzABCDEFGHLIJKLMNOPQRSTUVWXYZ0123456789-\ ', false],
+		];
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @dataProvider numericProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testNumeric($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "numeric"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function numericProvider()
+	{
+		return [
+			['0', true],
+			['12314', true],
+			['-42', true],
+			['+42', true],
+			['123a', false],
+			['--1', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider integerProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testInteger($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "integer"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function integerProvider()
+	{
+		return [
+			['0', true],
+			['42', true],
+			['-1', true],
+			['123a', false],
+			['1.9', false],
+			['--1', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider decimalProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testDecimal($str, $expected)
+	{
+		$data = [
+			'foo' => $str,
+		];
+
+		$this->validation->setRules([
+			'foo' => "decimal"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function decimalProvider()
+	{
+		return [
+			['1.0', true],
+			['-0.98', true],
+			['0', false],
+			['1.0a', false],
+			['-i', false],
+			['--1', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider greaterThanProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testGreaterThan($first, $second, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "greater_than[{$second}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function greaterThanProvider()
+	{
+		return [
+			['-10', '-11', true],
+			['10', '9', true],
+			['10', '10', false],
+			['10', 'a', false],
+			['10a', '10', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider greaterThanEqualProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testGreaterThanEqual($first, $second, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "greater_than_equal_to[{$second}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function greaterThanEqualProvider()
+	{
+		return [
+			['0', '0', true],
+			['1', '0', true],
+			['-1', '0', false],
+			['10a', '0', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider lessThanProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testLessThan($first, $second, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "less_than[{$second}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function lessThanProvider()
+	{
+		return [
+			['4', '5', true],
+			['-1', '0', true],
+			['4', '4', false],
+			['10a', '5', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider lessThanEqualProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testLessEqualThan($first, $second, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "less_than_equal_to[{$second}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function lessThanEqualProvider()
+	{
+		return [
+			['-1', '0', true],
+			['-1', '-1', true],
+			['4', '4', true],
+			['0', '-1', false],
+			['10a', '0', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider inListProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testInList($first, $second, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "in_list[{$second}]"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function inListProvider()
+	{
+		return [
+			['red', 'red,Blue,123', true],
+			['Blue', 'red,Blue,123', true],
+			['123', 'red,Blue,123', true],
+			['Red', 'red,Blue,123', false],
+			[' red', 'red,Blue,123', false],
+			['1234', 'red,Blue,123', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider naturalProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testNatural($first, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "is_natural"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function naturalProvider()
+	{
+		return [
+			['0', true],
+			['12', true],
+			['42a', false],
+			['-1', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider naturalZeroProvider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testNaturalNoZero($first, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "is_natural_no_zero"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function naturalZeroProvider()
+	{
+		return [
+			['0', false],
+			['12', true],
+			['42a', false],
+			['-1', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
+	/**
+	 * @dataProvider base64Provider
+	 *
+	 * @param $str
+	 * @param $expected
+	 */
+	public function testBase64($first, $expected)
+	{
+		$data = [
+			'foo' => $first,
+		];
+
+		$this->validation->setRules([
+			'foo' => "valid_base64"
+		]);
+
+		$this->assertEquals($expected, $this->validation->run($data));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function base64Provider()
+	{
+		return [
+			[base64_encode('string'), true],
+			['FA08GG', false],
+		];
+	}
+
+	//-------------------------------------------------------------------
+
 }
