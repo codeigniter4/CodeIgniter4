9cc7c85f678ae7c7b787193e3a23175ba62383d9
Portaflex <dsala@clinicadecot.es>
2016-09-01 23:53:14 +0200

    foreign_key_forge

--- system/Database/Forge.php
+++ system/Database/Forge.php
@@ -455,7 +455,7 @@ class Forge
 	    foreach ($this->foreignKeys as $fk)
 	    {
 	       $sql = 'ALTER TABLE '.$table.' ADD CONSTRAINT fk_'.$fk['fkey'].' FOREIGN KEY ('.
-	       	        $fk['key'].') REFERENCES '.$this->db->DBPrefix.$fk['table'].' ('.$fk['fkey'].
+	       	        $fk['key'].') REFERENCES '.$this->db->DBPrefix.'.'.$fk['table'].' ('.$fk['fkey'].
 	       	        ') ON UPDATE '.$fk['onUpdate'].' ON DELETE '.$fk['onDelete'];
 	       $this->db->query($sql);
 	    }
