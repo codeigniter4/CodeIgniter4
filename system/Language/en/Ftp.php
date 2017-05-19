cc3c6540f6a31c1d1e79fffe00f55bfdd04f9378
Ali Çetin <etinali@gmail.com>
2017-05-19 20:39:55 +0300

    FTP Class reference by CI3;

---
+++ system/Language/en/Ftp.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	CodeIgniter Dev Team
+ * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 3.0.0
+ * @filesource
+ */
+
+return [
+	'ftpNoConnection'		=> 'Unable to locate a valid connection ID. Please make sure you are connected before performing any file routines.',
+	'ftpUnableToConnect'	=> 'Unable to connect to your FTP server using the supplied hostname.',
+	'ftpUnableToLogin'		=> 'Unable to login to your FTP server. Please check your username and password.',
+	'ftpUnableToMkdir'		=> 'Unable to create the directory you have specified.',
+	'ftpUnableToChangedir'	=> 'Unable to change directories.',
+	'ftpUnableToChmod'		=> 'Unable to set file permissions. Please check your path.',
+	'ftpUnableToUpload'		=> 'Unable to upload the specified file. Please check your path.',
+	'ftpUnableToDownload'	=> 'Unable to download the specified file. Please check your path.',
+	'ftpNoSourceFile'		=> 'Unable to locate the source file. Please check your path.',
+	'ftpUnableToRename'		=> 'Unable to rename the file.',
+	'ftpUnableToDelete'		=> 'Unable to delete the file.',
+	'ftpUnableToMove'		=> 'Unable to move the file. Please make sure the destination directory exists.',
+];
