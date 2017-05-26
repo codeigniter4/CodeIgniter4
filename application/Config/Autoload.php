4ad2834670de47d4cc95d2bfa7f4e1944fdb2260
Lonnie Ezell <lonnieje@gmail.com>
2017-05-25 23:52:43 -0500

    [ci skip] Fixing path to test migrations

--- application/Config/Autoload.php
+++ application/Config/Autoload.php
@@ -51,9 +51,9 @@ class Autoload extends \CodeIgniter\Config\AutoloadConfig
 		 */
 		$psr4 = [
 			'Config'                     => APPPATH.'Config',
-			APP_NAMESPACE                => APPPATH,			// For custom namespace
-			'App'                        => APPPATH,			// To ensure filters, etc still found,
-			'Tests\Support'              => TESTPATH,           // So custom migrations can run during testing
+			APP_NAMESPACE                => APPPATH,			    // For custom namespace
+			'App'                        => APPPATH,			    // To ensure filters, etc still found,
+			'Tests\Support'              => TESTPATH.'_support',    // So custom migrations can run during testing
 		];
 
 		/**
