1024d6350eb88e31f643b3941d8572c52309296d
Lonnie Ezell <lonnieje@gmail.com>
2017-06-19 00:16:59 -0500

    More constructors

--- system/I18n/Time.php
+++ system/I18n/Time.php
@@ -8,7 +8,8 @@ use IntlDateFormatter;
 /**
  * Class Time
  *
- * A wrapper around IntlCalendar to provide a slightly nicer UX.
+ * A localized date/time package inspired
+ * by Nesbot/Carbon and CakePHP/Chronos.
  *
  * Requires the intl PHP extension.
  *
@@ -33,29 +34,30 @@ class Time extends DateTime
 	protected $toStringFormat = IntlDateFormatter::SHORT;
 
 	/**
-	 * words used to determine if a time string needs to be modified.
+	 * Used to check time string to determine if it is relative time or not....
 	 *
-	 * @var array
+	 * @var string
 	 */
-	protected static $relativeKeywords = [
-		'+', '-', 'ago', 'first', 'last', 'next', 'this', 'today', 'tomorrow', 'yesterday',
-	];
+	protected static $relativePattern = '/this|next|last|tomorrow|yesterday|midnight|today|[+-]|first|last|ago/i';
 
 	//--------------------------------------------------------------------
 	// Constructors
 	//--------------------------------------------------------------------
 
-	public function __construct(string $time=null, string $timezone=null, string $locale=null)
+	public function __construct(string $time=null, $timezone=null, string $locale=null)
 	{
 		$timezone = ! empty($timezone) ? $timezone : date_default_timezone_get();
-		$this->timezone = new DateTimeZone($timezone);
+		$this->timezone = $timezone instanceof DateTimeZone ? $timezone : new DateTimeZone($timezone);
 
 		// If no locale was provided, grab it from Locale (set by IncomingRequest for web requests)
 		$this->locale = ! empty($locale) ? $locale : Locale::getDefault();
 
 		if (! empty($time))
 		{
-			if (static::hasRelativeKeywords($time))
+			// If the time string was a relative string (i.e. 'next Tuesday')
+			// then we need to adjust the time going in so that we have a current
+			// timezone to work with.
+			if (is_string($time) && static::hasRelativeKeywords($time))
 			{
 				$instance = new DateTime('now', $this->timezone);
 				$instance->modify($time);
@@ -72,12 +74,12 @@ class Time extends DateTime
 	/**
 	 * Returns a new Time instance with the timezone set.
 	 *
-	 * @param string|null $timezone
+	 * @param string|DateTimeZone|null $timezone
 	 * @param string|null $locale
 	 *
 	 * @return \CodeIgniter\I18n\Time
 	 */
-	public static function now(string $timezone=null, string $locale=null)
+	public static function now($timezone=null, string $locale=null)
 	{
 		return new Time(null, $timezone, $locale);
 	}
@@ -96,17 +98,167 @@ class Time extends DateTime
 	 *
 	 * @return \CodeIgniter\I18n\Time
 	 */
-	public static function parse(string $datetime, string $timezone=null, string $locale=null)
+	public static function parse(string $datetime, $timezone=null, string $locale=null)
 	{
 		return new Time($datetime, $timezone, $locale);
 	}
 
 	//--------------------------------------------------------------------
 
-	public static function hasRelativeKeywords(string $time)
+	/**
+	 * Return a new time with the time set to midnight.
+	 *
+	 * @param null        $timezone
+	 * @param string|null $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function today($timezone=null, string $locale=null)
 	{
-
+		return new Time(date('Y-m-d 00:00:00'), $timezone, $locale);
 	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns an instance set to midnight yesterday morning.
+	 *
+	 * @param null        $timezone
+	 * @param string|null $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function yesterday($timezone=null, string $locale=null)
+	{
+		return new Time(date('Y-m-d 00:00:00', strtotime('-1 day')), $timezone, $locale);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns an instance set to midnight tomorrow morning.
+	 *
+	 * @param null        $timezone
+	 * @param string|null $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function tomorrow($timezone=null, string $locale=null)
+	{
+		return new Time(date('Y-m-d 00:00:00', strtotime('+1 day')), $timezone, $locale);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns a new instance based on the year, month and day. If any of those three
+	 * are left empty, will default to the current value.
+	 *
+	 * @param int|null $year
+	 * @param int|null $month
+	 * @param int|null $day
+	 * @param          $timezone
+	 * @param string   $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function createFromDate(int $year=null, int $month=null, int $day=null, $timezone=null, string $locale=null)
+	{
+		return static::create($year, $month, $day, null, null, null, $timezone, $locale);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns a new instance with the date set to today, and the time set to the values passed in.
+	 *
+	 * @param int|null    $hour
+	 * @param int|null    $minutes
+	 * @param int|null    $seconds
+	 * @param null        $timezone
+	 * @param string|null $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function createFromTime(int $hour=null, int $minutes=null, int $seconds=null, $timezone=null, string $locale=null)
+	{
+		return static::create(null, null, null, $hour, $minutes, $seconds, $timezone, $locale);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns a new instance with the date time values individually set.
+	 *
+	 * @param int|null    $year
+	 * @param int|null    $month
+	 * @param int|null    $day
+	 * @param int|null    $hour
+	 * @param int|null    $minutes
+	 * @param int|null    $seconds
+	 * @param null        $timezone
+	 * @param string|null $locale
+	 *
+	 * @return \CodeIgniter\I18n\Time
+	 */
+	public static function create(int $year=null, int $month=null, int $day=null, int $hour=null, int $minutes=null, int $seconds=null, $timezone=null, string $locale=null)
+	{
+		$year  = is_null($year)  ? date('Y') : $year;
+		$month = is_null($month) ? date('m') : $month;
+		$day   = is_null($day)   ? date('d') : $day;
+		$hour    = empty($hour)    ? 0 : $hour;
+		$minutes = empty($minutes) ? 0 : $minutes;
+		$seconds = empty($seconds) ? 0 : $seconds;
+
+		return new Time(date("Y-m-d H:i:s", strtotime("{$year}-{$month}-{$day} {$hour}:{$minutes}:{$seconds}")), $timezone, $locale);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Converts the current instance to a mutable DateTime object.
+	 *
+	 * @return \DateTime
+	 */
+	public function toDateTime()
+	{
+		$dateTime = new \DateTime(null, $this->getTimezone());
+		$dateTime->setTimestamp($this->getTimestamp());
+
+		return $dateTime;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Check a time string to see if it includes a relative date (like 'next Tuesday').
+	 *
+	 * @param string $time
+	 *
+	 * @return bool
+	 */
+	protected static function hasRelativeKeywords(string $time): bool
+	{
+		// skip common format with a '-' in it
+		if (preg_match('/[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/', $time) !== 1)
+		{
+			return preg_match(static::$relativePattern, $time) > 0;
+		}
+
+		return false;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Returns the value of this instance in the format 'Y-m-d H:i:s'
+	 */
+	public function toDateTimeString()
+	{
+		return $this->format('Y-m-d H:i:s');
+	}
+
+	//--------------------------------------------------------------------
 
 	/**
 	 * Outputs a short format version of the datetime.
@@ -115,6 +267,6 @@ class Time extends DateTime
 	 */
 	public function __toString()
 	{
-		return IntlDateFormatter::formatObject($this, $this->toStringFormat, $this->locale);
+		return IntlDateFormatter::formatObject($this->toDateTime(), $this->toStringFormat, $this->locale);
 	}
 }
--- tests/system/I18n/TimeTest.php
+++ tests/system/I18n/TimeTest.php
@@ -26,4 +26,155 @@ class TimeTest extends \CIUnitTestCase
 		$this->assertEquals($formatter->format(strtotime('now')), (string)$time);
 	}
 
+	public function testTimeWithTimezone()
+	{
+		$time = new Time('now', 'Europe/London');
+
+		$formatter = new IntlDateFormatter(
+			'en_US',
+			IntlDateFormatter::SHORT,
+			IntlDateFormatter::SHORT,
+			'Europe/London',  // Default for CodeIgniter
+			IntlDateFormatter::GREGORIAN
+		);
+
+		$this->assertEquals($formatter->format(strtotime('now')), (string)$time);
+	}
+
+	public function testTimeWithTimezoneAndLocale()
+	{
+		$time = new Time('now', 'Europe/London', 'fr_FR');
+
+		$formatter = new IntlDateFormatter(
+			'fr_FR',
+			IntlDateFormatter::SHORT,
+			IntlDateFormatter::SHORT,
+			'Europe/London',  // Default for CodeIgniter
+			IntlDateFormatter::GREGORIAN
+		);
+
+		$this->assertEquals($formatter->format(strtotime('now')), (string)$time);
+	}
+
+	public function testTimeWithDateTimeZone()
+	{
+		$time = new Time('now', new \DateTimeZone('Europe/London'), 'fr_FR');
+
+		$formatter = new IntlDateFormatter(
+			'fr_FR',
+			IntlDateFormatter::SHORT,
+			IntlDateFormatter::SHORT,
+			'Europe/London',  // Default for CodeIgniter
+			IntlDateFormatter::GREGORIAN
+		);
+
+		$this->assertEquals($formatter->format(strtotime('now')), (string)$time);
+	}
+
+	public function testToDateTime()
+	{
+		$time = new Time();
+
+		$obj = $time->toDateTime();
+
+		$this->assertTrue($obj instanceof \DateTime);
+	}
+
+	public function testNow()
+	{
+		$time = Time::now();
+		$time1 = new \DateTime();
+
+		$this->assertTrue($time instanceof Time);
+		$this->assertEquals($time->getTimestamp(), $time1->getTimestamp());
+	}
+
+	public function testParse()
+	{
+		$time = Time::parse('next Tuesday');
+		$time1 = new \DateTime();
+		$time1->modify('next Tuesday');
+
+		$this->assertEquals($time->getTimestamp(), $time1->getTimestamp());
+	}
+
+	public function testToDateTimeString()
+	{
+		$time = Time::parse('2017-01-12 00:00');
+
+		$this->assertEquals('1/12/17, 12:00 AM', (string)$time);
+		$this->assertEquals('2017-01-12 00:00:00', $time->toDateTimeString());
+	}
+
+	public function testToDateTimeStringWithTimeZone()
+	{
+		$time = Time::parse('2017-01-12 00:00', 'Europe/London');
+
+		$expects = new \DateTime('2017-01-12', new \DateTimeZone('Europe/London'));
+
+		$this->assertEquals($expects->format('Y-m-d H:i:s'), $time->toDateTimeString());
+	}
+
+	public function testToday()
+	{
+		$time = Time::today();
+
+		$this->assertEquals(date('Y-m-d 00:00:00'), $time->toDateTimeString());
+	}
+
+	public function testTodayLocalized()
+	{
+		$time = Time::today('Europe/London');
+
+		$this->assertEquals(date('Y-m-d 00:00:00'), $time->toDateTimeString());
+	}
+
+	public function testYesterday()
+	{
+		$time = Time::yesterday();
+
+		$this->assertEquals(date('Y-m-d 00:00:00', strtotime('-1 day')), $time->toDateTimeString());
+	}
+
+	public function testTomorrow()
+	{
+		$time = Time::tomorrow();
+
+		$this->assertEquals(date('Y-m-d 00:00:00', strtotime('+1 day')), $time->toDateTimeString());
+	}
+
+	public function testCreateFromDate()
+	{
+		$time = Time::createFromDate(2017, 03, 05);
+
+		$this->assertEquals(date('Y-m-d 00:00:00', strtotime('2017-03-05 00:00:00')), $time->toDateTimeString());
+	}
+
+	public function testCreateFromDateLocalized()
+	{
+		$time = Time::createFromDate(2017, 03, 05, 'Europe/London');
+
+		$this->assertEquals(date('Y-m-d 00:00:00', strtotime('2017-03-05 00:00:00')), $time->toDateTimeString());
+	}
+
+	public function testCreateFromTime()
+	{
+		$time = Time::createFromTime(10, 03, 05);
+
+		$this->assertEquals(date('Y-m-d 10:03:05'), $time->toDateTimeString());
+	}
+
+	public function testCreateFromTimeEvening()
+	{
+		$time = Time::createFromTime(20, 03, 05);
+
+		$this->assertEquals(date('Y-m-d 20:03:05'), $time->toDateTimeString());
+	}
+
+	public function testCreateFromTimeLocalized()
+	{
+		$time = Time::createFromTime(10, 03, 05, 'Europe/London');
+
+		$this->assertEquals(date('Y-m-d 10:03:05'), $time->toDateTimeString());
+	}
 }
