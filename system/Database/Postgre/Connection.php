aae9a054c28352310a5b7e12837d8a12b1bcdcbe
Lonnie Ezell <lonnieje@gmail.com>
2017-07-05 08:11:54 -0500

    Fixed Postgre issue from row to getRow. Fixes #587

--- system/Database/Postgre/Connection.php
+++ system/Database/Postgre/Connection.php
@@ -403,7 +403,7 @@ class Connection extends BaseConnection implements ConnectionInterface
 			{
 				$sql = "SELECT pg_get_serial_sequence('{$table}', '{$column}') AS seq";
 				$query = $this->query($sql);
-				$query = $query->row();
+				$query = $query->getRow();
 				$seq = $query->seq;
 			}
 			else
