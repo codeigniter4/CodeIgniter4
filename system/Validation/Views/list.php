b591cab9159bed0a9e3c713bb2a25befadaf7068
Lonnie Ezell <lonnieje@gmail.com>
2017-07-05 08:30:40 -0500

    [ci skip] Check for errors first when listing validation errors. Fixes #574

--- system/Validation/Views/list.php
+++ system/Validation/Views/list.php
@@ -1,7 +1,9 @@
-<div class="alert alert-danger" role="alert">
-    <ul>
-    <?php foreach ($errors as $error) : ?>
-        <li><?= esc($error) ?></li>
-    <?php endforeach ?>
-    </ul>
-</div>
+<?php if (count($errors) > 0) : ?>
+    <div class="alert alert-danger" role="alert">
+        <ul>
+        <?php foreach ($errors as $error) : ?>
+            <li><?= esc($error) ?></li>
+        <?php endforeach ?>
+        </ul>
+    </div>
+<?php endif ?>
