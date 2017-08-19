6f043617bbd2e5fa33493895fa99eb2c8bff4dca
chistel <danvc18@gmail.com>
2017-08-19 11:46:57 +0100

    setpath updated
    removed setpath method since its been changed to a function in common.php

--- system/HTTP/Files/UploadedFile.php
+++ system/HTTP/Files/UploadedFile.php
@@ -153,7 +153,7 @@ class UploadedFile extends File implements UploadedFileInterface
 	 */
 	public function move(string $targetPath, string $name = null, bool $overwrite = false)
 	{
-		$targetPath = $this->setPath($targetPath); //set the target path
+		$targetPath = setPath($targetPath); //set the target path
 
 		if ($this->hasMoved)
 		{
@@ -184,26 +184,6 @@ class UploadedFile extends File implements UploadedFileInterface
 
 		return true;
 	}
-
-	/**
-	 * create file target path if
-	 * the set path does not exist
-	 * @return string The path set or created.
-	 */
-   	protected function setPath($path)
-   	{
-     		if (!is_dir($path))
-     		{
-         		mkdir($path, 0777, true);
-         		//create the index.html file
-         		if (!file_exists($path.'index.html'))
-         		{
-            			$file = fopen($path.'index.html', 'x+');
-            			fclose($file);
-         		}
-     		}
-     		return $path;
- 	}
 
 	//--------------------------------------------------------------------
 
