66c85156e2a765b59c20a81d39aaaae92441ec75
Lonnie Ezell <lonnieje@gmail.com>
2017-11-17 00:15:44 -0600

    New config tab for the debug toolbar. Would like it to become a place to warn about potential errors or misconfigurations, also.

--- system/Debug/Toolbar/Views/toolbar.tpl.php
+++ system/Debug/Toolbar/Views/toolbar.tpl.php
@@ -58,7 +58,34 @@
             </a>
         </span>
 
-        <h1><a id="debug-bar-link" href="javascript:void(0)">Debug Bar</a></h1>
+        <h1>
+            <span class="ci-label">
+                <a href="javascript: void(0)" data-tab="ci-config" >
+                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                         width="18.60px" height="24.0px" viewBox="0 0 18.60 28.000000"
+                         preserveAspectRatio="xMidYMid meet">
+                        <g transform="translate(0.000000,28.000000) scale(0.010000,-0.010000)" fill="#dd4814" stroke="none">
+                            <path d="M737 1963 c22 -79 -7 -185 -78 -290 -18 -26 -107 -122 -197 -213
+          -239 -240 -336 -371 -403 -544 -79 -206 -78 -408 5 -582 64 -134 212 -264 361
+          -314 l60 -20 -30 22 c-210 152 -229 387 -48 588 25 27 48 50 51 50 4 0 7 -27
+          7 -61 0 -57 2 -62 37 -95 30 -27 46 -34 78 -34 56 0 99 24 116 65 29 69 16
+          120 -50 205 -105 134 -117 233 -43 347 l31 48 7 -47 c13 -82 58 -129 250 -258
+          209 -141 306 -261 328 -405 11 -72 -1 -161 -31 -218 -27 -53 -112 -143 -165
+          -174 -24 -14 -43 -26 -43 -28 0 -2 24 4 53 14 241 83 427 271 482 486 19 76
+          19 202 -1 285 -35 152 -146 305 -299 412 l-70 49 -6 -33 c-8 -48 -26 -76 -59
+          -93 -45 -23 -103 -19 -138 10 -67 57 -78 146 -37 305 30 116 32 206 5 291 -27
+          89 -104 206 -162 247 -17 13 -18 12 -11 -15z"/>
+                        </g>
+                    </svg>
+                    <?= \CodeIgniter\CodeIgniter::CI_VERSION ?>
+                </a>
+            </span>
+        </h1>
+
+        <!-- Open/Close Toggle -->
+        <a id="debug-bar-link" href="javascript:void(0)">
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEPSURBVEhL7ZVLDoJAEEThRuoGDwSEG+jCuFU34s3AK3APP1VDDSGMqI1xx0s6M/2rnlHEaMZElmWrPM+vsDvsYbQ7+us0TReSC2EBrEHxCevRYuppYLXkQpC8sVCuGfTvqSE3hFdFwUGuGfRvqSE35NUAfKZrbQNQm2jrMA+gOK+M+FmhDsRL5voHMA8gFGecq0JOXLWlQg7E7AMIxZnjOiZOEJ82gFCcedUE4gS56QP8yf8ywItz7e+RituKlkkDBoIOH4Nd4HZD4NsGYJ/Abn1xEVOcuZ8f0zc/tHiYmzTAwscBvDIK/veyQ9K/rnewjdF26q0kF1IUxZIFPAVW98x/a+qp8L2M/+HMhETRE6S8TxpZ7KGXAAAAAElFTkSuQmCC">
+        </a>
 	</div>
 
 	<!-- Timeline -->
@@ -261,3 +288,10 @@
 			</table>
 		<?php endif; ?>
 	</div>
+
+    <!-- Config Values -->
+    <div id="ci-config" class="tab">
+        <h2>System Configuration</h2>
+
+        <?= \CodeIgniter\Debug\Toolbar\Collectors\Config::display() ?>
+    </div>
