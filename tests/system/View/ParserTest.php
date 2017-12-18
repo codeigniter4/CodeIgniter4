7760ef411ffe0fa2c387facba30fe5ba2ccdbd8a
Lonnie Ezell <lonnieje@gmail.com>
2017-12-17 23:17:19 -0600

    Parser should respect escaping contexts given in setData. Fixes #788

--- tests/system/View/ParserTest.php
+++ tests/system/View/ParserTest.php
@@ -218,7 +218,30 @@ class ParserTest extends \CIUnitTestCase
 		$this->assertEquals($expected, \esc($value, 'html'));
 	}
 
-	// ------------------------------------------------------------------------
+	//------------------------------------------------------------------------
+
+	/**
+	 * @see https://github.com/bcit-ci/CodeIgniter4/issues/788
+	 */
+	public function testEscapingRespectsSetDataRaw()
+	{
+		$parser = new Parser($this->config, $this->viewsDir, $this->loader);
+
+		$template = '{ foo }';
+
+		$parser->setData(['foo' => '<script>'], 'raw');
+		$this->assertEquals('<script>', $parser->renderString($template));
+	}
+
+	public function testEscapingSetDataWithOtherContext()
+	{
+		$parser = new Parser($this->config, $this->viewsDir, $this->loader);
+
+		$template = '{ foo }';
+
+		$parser->setData(['foo' => 'http://foo.com'], 'url');
+		$this->assertEquals('http%3A%2F%2Ffoo.com', $parser->renderString($template));
+	}
 
 	public function testFilterWithNoArgument()
 	{
