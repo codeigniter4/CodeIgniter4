1c085b2035afcd70d3659ceca2a87d0974786cd3
Lonnie Ezell <lonnieje@gmail.com>
2018-01-30 23:59:28 -0600

    Validation test fixes

--- tests/system/Validation/RulesTest.php
+++ tests/system/Validation/RulesTest.php
@@ -2,10 +2,8 @@
 
 use Config\Database;
 
-class RulesTest extends \CIDatabaseTestCase
+class RulesTest extends \CIUnitTestCase
 {
-	protected $refresh =true;
-
 	/**
 	 * @var Validation
 	 */
@@ -331,66 +329,6 @@ class RulesTest extends \CIDatabaseTestCase
 		]);
 
 		$this->assertFalse($this->validation->run($data));
-	}
-
-	//--------------------------------------------------------------------
-
-	/**
-	 * @group DatabaseLive
-	 */
-	public function testIsUniqueFalse()
-	{
-		$data = [
-			'email' => 'derek@world.com',
-		];
-
-		$this->validation->setRules([
-			'email' => 'is_unique[user.email]',
-		]);
-
-		$this->assertFalse($this->validation->run($data));
-	}
-
-	//--------------------------------------------------------------------
-
-	/**
-	 * @group DatabaseLive
-	 */
-	public function testIsUniqueTrue()
-	{
-		$data = [
-			'email' => 'derek@world.co.uk',
-		];
-
-		$this->validation->setRules([
-			'email' => 'is_unique[user.email]',
-		]);
-
-		$this->assertTrue($this->validation->run($data));
-	}
-
-	//--------------------------------------------------------------------
-
-	/**
-	 * @group DatabaseLive
-	 */
-	public function testIsUniqueIgnoresParams()
-	{
-		$db  = Database::connect();
-		$row = $db->table('user')
-		          ->limit(1)
-		          ->get()
-		          ->getRow();
-
-		$data = [
-			'email' => 'derek@world.co.uk',
-		];
-
-		$this->validation->setRules([
-			'email' => "is_unique[user.email,id,{$row->id}]",
-		]);
-
-		$this->assertTrue($this->validation->run($data));
 	}
 
 	//--------------------------------------------------------------------
