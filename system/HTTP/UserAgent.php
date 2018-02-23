22a5d9f0eaa61f8bead6b662fcea85f2efcd127d
Lonnie Ezell <lonnieje@gmail.com>
2018-02-22 23:24:59 -0600

    Allow toString on UserAgent

--- system/HTTP/UserAgent.php
+++ system/HTTP/UserAgent.php
@@ -451,7 +451,7 @@ class UserAgent
 	//--------------------------------------------------------------------
 
 	/**
-	 * Outputs the original Agent String when cast as a string. 
+	 * Outputs the original Agent String when cast as a string.
 	 *
 	 * @return string
 	 */
