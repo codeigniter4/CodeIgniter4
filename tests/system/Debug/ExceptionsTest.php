fc69a5ffb004fbd250029a6010358472cfca209d
Natan Felles <natanfelles@gmail.com>
2018-03-20 07:40:27 -0300

    Update ExceptionsTest

--- tests/system/Debug/ExceptionsTest.php
+++ tests/system/Debug/ExceptionsTest.php
@@ -4,7 +4,7 @@ class ExceptionsTest extends \CIUnitTestCase
 {
 	public function testNew()
 	{
-		$actual = new Exceptions(new \Config\Exceptions());
+		$actual = new Exceptions(new \Config\Exceptions(), new \CodeIgniter\HTTP\IncomingRequest(), new \CodeIgniter\HTTP\Response());
 		$this->assertInstanceOf(Exceptions::class, $actual);
 	}
 }
