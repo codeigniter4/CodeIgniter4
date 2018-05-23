7d105ae3b879e721b86586371b4eb48c633fad52
Master Yoda <jim_parry@bcit.ca>
2018-05-23 12:52:06 -0700

    Removing ignored log file - part 2

--- log-2018-05-22.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') || exit('No direct script access allowed'); ?>
-
-WARNING - 2018-05-22 21:55:19:387835 --> This is a test log
-WARNING - 2018-05-22 22:00:52:481195 --> This is a test log
-WARNING - 2018-05-22 22:09:01:581118 --> This is a test log
-WARNING - 2018-05-22 22:09:36:560338 --> This is a test log
-WARNING - 2018-05-22 22:12:41:184724 --> This is a test log
