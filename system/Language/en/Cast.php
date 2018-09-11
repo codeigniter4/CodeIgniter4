8496c6bb354cc1e90b5e06d6450188a47dc0a91a
Paweł Nowacki <pawel.nowacki@teleit.pl>
2018-09-11 09:17:07 +0200

    Update Cast.php

--- system/Language/en/Cast.php
+++ system/Language/en/Cast.php
@@ -12,11 +12,12 @@
  * 
  * @codeCoverageIgnore
  */
+<?php
 return [
-	'Cast.jsonErrorDepth' 			=> 'Maximum stack depth exceeded',
-	'Cast.jsonErrorStateMismatch' 	=> 'Underflow or the modes mismatch',
-	'Cast.jsonErrorCtrlChar'		=> 'Unexpected control character found',
-	'Cast.jsonErrorSyntax'			=> 'Syntax error, malformed JSON',
-	'Cast.jsonErrorUtf8'			=> 'Malformed UTF-8 characters, possibly incorrectly encoded',
-	'Cast.jsonErrorUnknown'			=> 'Unknown error'
+	'Cast.jsonErrorDepth' 		=> 'Maximum stack depth exceeded',
+	'Cast.jsonErrorStateMismatch'	=> 'Underflow or the modes mismatch',
+	'Cast.jsonErrorCtrlChar'	=> 'Unexpected control character found',
+	'Cast.jsonErrorSyntax'		=> 'Syntax error, malformed JSON',
+	'Cast.jsonErrorUtf8'		=> 'Malformed UTF-8 characters, possibly incorrectly encoded',
+	'Cast.jsonErrorUnknown'		=> 'Unknown error'
 ];
