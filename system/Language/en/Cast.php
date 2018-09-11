fc93a03e6b69f267129459c4cf7380b4cb02d259
Paweł Nowacki <pawel.nowacki@teleit.pl>
2018-09-11 09:15:14 +0200

    language file (Cast) for system/Exception/CastException.php

---
+++ system/Language/en/Cast.php
@@ -0,0 +1,9 @@
+<?php
+return [
+	'Cast.jsonErrorDepth' 			=> 'Maximum stack depth exceeded',
+	'Cast.jsonErrorStateMismatch' 	=> 'Underflow or the modes mismatch',
+	'Cast.jsonErrorCtrlChar'		=> 'Unexpected control character found',
+	'Cast.jsonErrorSyntax'			=> 'Syntax error, malformed JSON',
+	'Cast.jsonErrorUtf8'			=> 'Malformed UTF-8 characters, possibly incorrectly encoded',
+	'Cast.jsonErrorUnknown'			=> 'Unknown error'
+];
