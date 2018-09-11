919b3bde667968a17f8cf27b4e63efe769bd9499
Paweł Nowacki <pawel.nowacki@teleit.pl>
2018-09-11 11:06:29 +0200

    Update CastException.php

--- system/Exceptions/CastException.php
+++ system/Exceptions/CastException.php
@@ -13,30 +13,26 @@ class CastException extends CriticalError
 	protected $code = 3;
 
 	public static function forInvalidJsonFormatException(int $error)
-	{
-		if($error === JSON_ERROR_DEPTH)
+	{	
+		switch($error)
 		{
-			throw new static(lang('Cast.jsonErrorDepth'));
-		}
-		else if($error === JSON_ERROR_STATE_MISMATCH)
-		{
-			throw new static(lang('Cast.jsonErrorStateMismatch'));
-		}
-		else if($error === JSON_ERROR_CTRL_CHAR)
-		{
-			throw new static(lang('Cast.jsonErrorCtrlChar'));
-		}
-		else if($error === JSON_ERROR_SYNTAX)
-		{
-			throw new static(lang('Cast.jsonErrorSyntax'));
-		}
-		else if($error === JSON_ERROR_UTF8)
-		{
-			throw new static(lang('Cast.jsonErrorUtf8'));
-		}
-		else
-		{
-			throw new static(lang('Cast.jsonErrorUnknown'));
+			case JSON_ERROR_DEPTH:
+				throw new static(lang('Cast.jsonErrorDepth'));
+				break;
+			case JSON_ERROR_STATE_MISMATCH:
+				throw new static(lang('Cast.jsonErrorStateMismatch'));
+				break;
+			case JSON_ERROR_CTRL_CHAR:
+				throw new static(lang('Cast.jsonErrorCtrlChar'));
+				break;
+			case JSON_ERROR_SYNTAX:
+				throw new static(lang('Cast.jsonErrorSyntax'));
+				break;
+			case JSON_ERROR_UTF8:
+				throw new static(lang('Cast.jsonErrorUtf8'));
+				break;
+			default:
+				throw new static(lang('Cast.jsonErrorUnknown'));
 		}
 
 	}
