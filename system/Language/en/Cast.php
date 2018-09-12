cdaffddddfc1c786fafb56bfe5873d7403424e92
Paweł Nowacki <pawel.nowacki@teleit.pl>
2018-09-12 08:46:52 +0200

    Keys: Cast.stringName => stringName

--- system/Language/en/Cast.php
+++ system/Language/en/Cast.php
@@ -14,10 +14,10 @@
  */
 
 return [
-	'Cast.jsonErrorDepth' 		=> 'Maximum stack depth exceeded',
-	'Cast.jsonErrorStateMismatch'	=> 'Underflow or the modes mismatch',
-	'Cast.jsonErrorCtrlChar'	=> 'Unexpected control character found',
-	'Cast.jsonErrorSyntax'		=> 'Syntax error, malformed JSON',
-	'Cast.jsonErrorUtf8'		=> 'Malformed UTF-8 characters, possibly incorrectly encoded',
-	'Cast.jsonErrorUnknown'		=> 'Unknown error'
+	'jsonErrorDepth' 		=> 'Maximum stack depth exceeded',
+	'jsonErrorStateMismatch'	=> 'Underflow or the modes mismatch',
+	'jsonErrorCtrlChar'	=> 'Unexpected control character found',
+	'jsonErrorSyntax'		=> 'Syntax error, malformed JSON',
+	'jsonErrorUtf8'		=> 'Malformed UTF-8 characters, possibly incorrectly encoded',
+	'jsonErrorUnknown'		=> 'Unknown error'
 ];
