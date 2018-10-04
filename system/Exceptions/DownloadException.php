fa7d8ff9ae8b03307616e67c42219c45ed9f2ae9
ytetsuro <phper.0o0@gmail.com>
2018-10-05 01:43:22 +0900

    feat: add DownloadException.
    Signed-off-by: ytetsuro <phper.0o0@gmail.com>

---
+++ system/Exceptions/DownloadException.php
@@ -0,0 +1,26 @@
+<?php namespace CodeIgniter\Exceptions;
+
+/**
+ * Class DownloadException
+ *
+ * @package CodeIgniter\Exceptions
+ */
+class DownloadException extends \RuntimeException implements ExceptionInterface
+{
+
+	public static function forCannotSetFilePath(string $path)
+	{
+		return new static(lang('HTTP.cannotSetFilePath', [$path]));
+	}
+
+	public static function forCannotSetBinary()
+	{
+		return new static(lang('HTTP.cannotSetBinary'));
+	}
+
+	public static function forNotFoundDownloadSource(string $extension)
+	{
+		return new static(lang('HTTP.notFoundDownloadSource', [$extension]));
+	}
+
+}
