678cf6c5e8ca0eb460753155d832aca1497b9be1
Chris Adams <chris407x@gmail.com>
2018-10-13 09:24:15 +0000

    adding bootstrap test

---
+++ tests/system/Test/BootstrapFCPATHTest.php
@@ -0,0 +1,90 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BootstrapFCPATHTest
+ *
+ * This test confirms that the tests/_support/_bootstrap.php
+ * will set the correct FCPATH regardless of the current directory
+ *
+ * It writes a file in the temp directory that loads the bootstrap file
+ * then compares its echo FCPATH; to the correct FCPATH returned
+ * from correctFCPATH();
+ *
+ */
+class BootstrapFCPATHTest extends TestCase
+{
+	private $currentDir = __dir__;
+	private $dir1 = '/tmp/dir1';
+	private $file1 = '/tmp/dir1/testFile.php';
+
+	protected function setUp() {
+		parent::setUp();
+		$this->deleteFiles();
+		$this->deleteDirectories();
+		$this->buildDirectories();
+		$this->writeFiles();
+	}
+
+	protected function tearDown() {
+		parent::tearDown();
+		$this->deleteFiles();
+		$this->deleteDirectories();
+	}
+
+	public function testSetFCPATH(){
+		$result1 = $this->readOutput($this->file1);
+		self::assertEquals($this->correctFCPATH(), $result1);
+	}
+
+	private function correctFCPATH(){
+		return realpath(__dir__ . '/../../../public') . DIRECTORY_SEPARATOR;
+	}
+
+	private function buildDirectories() : void
+	{
+		mkdir( $this->dir1 , 0777, TRUE);
+	}
+
+
+	private function deleteDirectories() : void
+	{
+		// these need to be executed in reverse order: dir 2 in inside dir1
+		if(is_dir($this->dir1)){
+			rmdir( $this->dir1 );
+		}
+	}
+
+	private function writeFiles() : void
+	{
+		file_put_contents( $this->file1 , $this->fileContents());
+		chmod($this->file1, 0777);
+
+	}
+
+	private function deleteFiles() : void
+	{
+		if(file_exists($this->file1)){
+			unlink( $this->file1 );
+		}
+	}
+
+	private function fileContents(){
+
+		$fileContents = '';
+		$fileContents .= '<?php' . PHP_EOL;
+		$fileContents .= "include_once '" . $this->currentDir . "' . '/../../../tests/_support/_bootstrap.php';". PHP_EOL;
+		$fileContents .= '// return value of FCPATH'. PHP_EOL;
+		$fileContents .= 'echo FCPATH;'. PHP_EOL;
+
+		return $fileContents;
+
+	}
+
+	private function readOutput($file){
+		return  system('php -f ' . $file);
+	}
+
+
+}
