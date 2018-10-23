291b1a805805753a8d9ad4ddc6ac8e92ababac1c
vibbow <vibbow@hotmail.com>
2018-10-23 16:25:49 +0800

    Remove duplicated backslash

--- system/Autoloader/Autoloader.php
+++ system/Autoloader/Autoloader.php
@@ -359,7 +359,7 @@ class Autoloader
 		// be a path.
 		// http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap03.html#tag_03_278
 		// Modified to allow backslash and colons for on Windows machines.
-		$filename = preg_replace('/[^a-zA-Z0-9\s\/\-\_\.\:\\\\]/', '', $filename);
+		$filename = preg_replace('/[^a-zA-Z0-9\s\/\-\_\.\:\\]/', '', $filename);
 
 		// Clean up our filename edges.
 		$filename = trim($filename, '.-_');
