3fa40e87c40f5cc6638bf8ee3cddbeb588b61566
ytetsuro <phper.0o0@gmail.com>
2018-10-29 00:55:47 +0900

    style: fix style.
    Signed-off-by: ytetsuro <phper.0o0@gmail.com>

--- system/HTTP/DownloadResponse.php
+++ system/HTTP/DownloadResponse.php
@@ -288,7 +288,7 @@ class DownloadResponse extends Message implements ResponseInterface
 
 		$this->removeHeader('Content-Type'); // replace existing content type
 		$this->setHeader('Content-Type', $mime);
-		if (!empty($charset)) {
+		if ( ! empty($charset)) {
 			$this->charset = $charset;
 		}
 
