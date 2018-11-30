7f73520395a7e5ba2961b76a826ba04518777238
Lonnie Ezell <lonnieje@gmail.com>
2018-11-30 00:19:58 -0600

    Database connect now stores separate instances for multiple custom configurations. Fixes #1533

---
+++ tests/system/Database/Live/ConnectTest.php
@@ -0,0 +1,42 @@
+<?php namespace CodeIgniter\Database\Live;;
+
+use CodeIgniter\Test\CIDatabaseTestCase;
+use Config\Database;
+
+class AliasTest extends CIDatabaseTestCase
+{
+	protected $group1;
+
+	protected $group2;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$config = config('database');
+
+		$this->group1 = $config->default;
+		$this->group2 = $config->default;
+
+		$this->group1['strictOn'] = false;
+		$this->group2['strictOn'] = true;
+	}
+
+	public function testConnectWithMultipleCustomGroups()
+	{
+		// We should have our test database connection already.
+		$instances = $this->getPrivateProperty(Database::class, 'instances');
+		$this->assertEquals(1, count($instances));
+
+		$db1 = Database::connect($this->group1);
+		$db2 = Database::connect($this->group2);
+
+		$this->assertNotSame($db1, $db2);
+
+		$instances = $this->getPrivateProperty(Database::class, 'instances');
+		$this->assertEquals(3, count($instances));
+	}
+
+	//--------------------------------------------------------------------
+
+}
