a80196cdf2cba3148967c3cece27383cc97ec090
Natan Felles <natanfelles@gmail.com>
2018-12-02 04:18:59 -0200

    Respond Toolbar Event in a closure

--- application/Config/Events.php
+++ application/Config/Events.php
@@ -29,6 +29,7 @@ if (ENVIRONMENT !== 'production')
 {
 	Events::on('DBQuery', 'CodeIgniter\Debug\Toolbar\Collectors\Database::collect');
 
-	// Handles the display of the toolbar itself. MUST remain here for toolbar to be displayed.
-	Events::on('pre_system', 'CodeIgniter\Debug\Toolbar::eventHandler');
+	Events::on('pre_system', function () {
+		Services::toolbar()->respond();
+	});
 }
