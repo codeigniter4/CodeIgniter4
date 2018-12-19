9e435caec9e002465a1c49ab3470d6bc48bcec72
Lonnie Ezell <lonnieje@gmail.com>
2018-12-18 23:03:48 -0600

    TranslateURIDashes fix. See #1564

--- tests/system/Router/RouterTest.php
+++ tests/system/Router/RouterTest.php
@@ -28,6 +28,8 @@ class RouterTest extends \CIUnitTestCase
 
 		$routes = [
 			'users'                                           => 'Users::index',
+			'user-setting/show-list'                          => 'User_setting::show_list',
+			'user-setting/(:segment)'                         => 'User_setting::detail/$1',
 			'posts'                                           => 'Blog::posts',
 			'pages'                                           => 'App\Pages::list_all',
 			'posts/(:num)'                                    => 'Blog::show/$1',
@@ -439,4 +441,53 @@ class RouterTest extends \CIUnitTestCase
 		$this->assertEquals('auth_post', $router->methodName());
 	}
 
+	/**
+	 * @see https://github.com/codeigniter4/CodeIgniter4/issues/1564
+	 */
+	public function testTranslateURIDashes()
+	{
+		$router = new Router($this->collection);
+
+		$router->handle('user-setting/show-list');
+
+		$router->setTranslateURIDashes(true);
+
+		$this->assertEquals('\User_setting', $router->controllerName());
+		$this->assertEquals('show_list', $router->methodName());
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @see https://github.com/codeigniter4/CodeIgniter4/issues/1564
+	 */
+	public function testTranslateURIDashesForParams()
+	{
+		$router = new Router($this->collection);
+		$router->setTranslateURIDashes(true);
+
+		$router->handle('user-setting/2018-12-02');
+
+		$this->assertEquals('\User_setting', $router->controllerName());
+		$this->assertEquals('detail', $router->methodName());
+		$this->assertEquals(['2018-12-02'], $router->params());
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * @see https://github.com/codeigniter4/CodeIgniter4/issues/1564
+	 */
+	public function testTranslateURIDashesForAutoRoute()
+	{
+		$router = new Router($this->collection);
+		$router->setTranslateURIDashes(true);
+
+		$router->autoRoute('admin-user/show-list');
+
+		$this->assertEquals('Admin_user', $router->controllerName());
+		$this->assertEquals('show_list', $router->methodName());
+	}
+
+	//--------------------------------------------------------------------
 }
