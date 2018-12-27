7643dbfc956ebcf88cdf8b89725bf1a8802772d4
Jim Parry <jim_parry@bcit.ca>
2018-12-26 21:49:51 -0800

    Update copyright to 2019

--- system/Router/RouteCollection.php
+++ system/Router/RouteCollection.php
@@ -8,7 +8,7 @@ namespace CodeIgniter\Router;
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (c) 2014-2018 British Columbia Institute of Technology
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +30,7 @@ namespace CodeIgniter\Router;
  *
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
- * @copyright  2014-2018 British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
  * @since      Version 3.0.0
