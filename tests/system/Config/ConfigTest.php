ba4cdf5bc6f6b7f9ff21e2d0fd899dad07d9a0a9
Jim Parry <jim_parry@bcit.ca>
2019-01-01 15:13:09 -0800

    Remove Email module - missed tests

--- tests/system/Config/ConfigTest.php
+++ tests/system/Config/ConfigTest.php
@@ -1,17 +1,17 @@
 <?php namespace CodeIgniter\Config;
 
-use Config\Format;
+use Config\DocTypes;
 
 class ConfigTest extends \CIUnitTestCase
 {
 
 	public function testCreateSingleInstance()
 	{
-		$Config          = Config::get('Format', false);
-		$NamespaceConfig = Config::get('Config\\Format', false);
+		$Config          = Config::get('DocTypes', false);
+		$NamespaceConfig = Config::get('Config\\DocTypes', false);
 
-		$this->assertInstanceOf(Format::class, $Config);
-		$this->assertInstanceOf(Format::class, $NamespaceConfig);
+		$this->assertInstanceOf(DocTypes::class, $Config);
+		$this->assertInstanceOf(DocTypes::class, $NamespaceConfig);
 	}
 
 	public function testCreateInvalidInstance()
@@ -23,8 +23,8 @@ class ConfigTest extends \CIUnitTestCase
 
 	public function testCreateSharedInstance()
 	{
-		$Config  = Config::get('Format' );
-		$Config2 = Config::get('Config\\Format');
+		$Config  = Config::get('DocTypes' );
+		$Config2 = Config::get('Config\\DocTypes');
 
 		$this->assertTrue($Config === $Config2);
 	}
