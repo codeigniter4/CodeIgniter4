ae73abd4b1474921f0934a8d40726fc9000e27f2
Lonnie Ezell <lonnieje@gmail.com>
2019-02-15 15:57:12 -0600

    New View Layout functionality for simple template functionality.

---
+++ tests/system/View/Views/extend_two.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+<p>First</p>
+<?= $this->endSection() ?>
+
+
+<?= $this->section('content') ?>
+<p>Second</p>
+<?= $this->endSection() ?>
