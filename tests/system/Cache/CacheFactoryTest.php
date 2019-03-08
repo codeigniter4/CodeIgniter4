a73d8ac87fcdf6aa2b2e7984e7f3848837b255cd
Jim Parry <jim_parry@bcit.ca>
2019-03-07 16:09:53 -0800

    Test: Improve CacheFactory code coverage

--- tests/system/Cache/CacheFactoryTest.php
+++ tests/system/Cache/CacheFactoryTest.php
@@ -1,7 +1,9 @@
-<?php namespace CodeIgniter\Cache;
+<?php
+namespace CodeIgniter\Cache;
 
 class CacheFactoryTest extends \CIUnitTestCase
 {
+
 	private static $directory = 'CacheFactory';
 	private $cacheFactory;
 	private $config;
@@ -13,7 +15,7 @@ class CacheFactoryTest extends \CIUnitTestCase
 		$this->cacheFactory = new CacheFactory();
 
 		//Initialize path
-		$this->config             = new \Config\Cache();
+		$this->config = new \Config\Cache();
 		$this->config->storePath .= self::$directory;
 	}
 
@@ -77,7 +79,7 @@ class CacheFactoryTest extends \CIUnitTestCase
 
 	public function testGetDummyHandler()
 	{
-		if (! is_dir($this->config->storePath))
+		if ( ! is_dir($this->config->storePath))
 		{
 			mkdir($this->config->storePath, 0555, true);
 		}
@@ -87,7 +89,27 @@ class CacheFactoryTest extends \CIUnitTestCase
 		$this->assertInstanceOf(\CodeIgniter\Cache\Handlers\DummyHandler::class, $this->cacheFactory->getHandler($this->config));
 
 		//Initialize path
-		$this->config             = new \Config\Cache();
+		$this->config = new \Config\Cache();
 		$this->config->storePath .= self::$directory;
 	}
+
+	public function testHandlesBadHandler()
+	{
+		if ( ! is_dir($this->config->storePath))
+		{
+			mkdir($this->config->storePath, 0555, true);
+		}
+
+		$this->config->handler = 'dummy';
+
+		if (stripos('win', php_uname()) === 0)
+			$this->assertTrue(true); // can't test properly if we are on Windows
+		else
+			$this->assertInstanceOf(\CodeIgniter\Cache\Handlers\DummyHandler::class, $this->cacheFactory->getHandler($this->config, 'wincache', 'wincache'));
+
+		//Initialize path
+		$this->config = new \Config\Cache();
+		$this->config->storePath .= self::$directory;
+	}
+
 }
