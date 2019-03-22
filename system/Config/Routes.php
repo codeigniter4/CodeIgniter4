bef3174f22cd91d0f0a47fb7ae58e5d3b72b969d
MGatner <mgatner@icloud.com>
2019-03-21 21:08:36 -0400

    Move BaseController rule before CLI

--- system/Config/Routes.php
+++ system/Config/Routes.php
@@ -44,6 +44,12 @@
  * It is called by Config\Routes, and has the $routes RouteCollection
  * already loaded up and ready for us to use.
  */
+// Prevent access to BaseController
+$routes->add('basecontroller(:any)', function()
+{
+    throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+}); 
+
 // Migrations
 $routes->cli('migrations/(:segment)/(:segment)', '\CodeIgniter\Commands\MigrationsCommand::$1/$2');
 $routes->cli('migrations/(:segment)', '\CodeIgniter\Commands\MigrationsCommand::$1');
@@ -51,9 +57,3 @@ $routes->cli('migrations', '\CodeIgniter\Commands\MigrationsCommand::index');
 
 // CLI Catchall - uses a _remap to call Commands
 $routes->cli('ci(:any)', '\CodeIgniter\CLI\CommandRunner::index/$1');
-
-// Prevent access to BaseController
-$routes->add('basecontroller(:any)', function()
-{
-    throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
-}); 
