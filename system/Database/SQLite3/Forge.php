77562c7dab57e49cd2108fadb6c7e83a60ecce2e
Atish Amte <atishamte@gmail.com>
2019-03-22 18:16:54 +0530

    Typos correction in DB module

--- system/Database/SQLite3/Forge.php
+++ system/Database/SQLite3/Forge.php
@@ -39,7 +39,7 @@
 use CodeIgniter\Database\Exceptions\DatabaseException;
 
 /**
- * Forge for Postgre
+ * Forge for SQLite3
  */
 class Forge extends \CodeIgniter\Database\Forge
 {
@@ -98,7 +98,7 @@ class Forge extends \CodeIgniter\Database\Forge
 	 * @param string $db_name
 	 *
 	 * @return boolean
-	 * @throws \CodeIgniter\DatabaseException
+	 * @throws \CodeIgniter\Database\Exceptions\DatabaseException
 	 */
 	public function dropDatabase($db_name): bool
 	{
