0d1b6f239ef1786734ee60f5b4b5a82c05899d81
Atish Amte <atishamte@gmail.com>
2019-04-13 16:27:31 +0530

    Forge coverage datetime formats test cases

--- tests/system/Database/Live/ModelTest.php
+++ tests/system/Database/Live/ModelTest.php
@@ -2,6 +2,7 @@
 
 use CodeIgniter\Config\Config;
 use CodeIgniter\Database\Exceptions\DataException;
+use CodeIgniter\Entity;
 use CodeIgniter\I18n\Time;
 use CodeIgniter\Model;
 use CodeIgniter\Test\CIDatabaseTestCase;
@@ -1454,5 +1455,83 @@ class ModelTest extends CIDatabaseTestCase
 
 		$error = $model->errors();
 		$this->assertTrue(isset($error['description']));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testSaveNewEntityWithDateTime()
+	{
+		$entity = new class extends Entity{
+			protected $id;
+			protected $name;
+			protected $email;
+			protected $country;
+			protected $deleted;
+			protected $created_at;
+			protected $updated_at;
+
+			protected $_options = [
+				'datamap' => [],
+				'dates'   => [
+					'created_at',
+					'updated_at',
+					'deleted_at',
+				],
+				'casts'   => [],
+			];
+		};
+		$testModel = new UserModel();
+
+		$entity->name = 'Mark';
+		$entity->email = 'mark@example.com';
+		$entity->country = 'India';
+		$entity->deleted = 0;
+		$entity->created_at = new Time('now');
+
+		$this->setPrivateProperty($testModel, 'useTimestamps', true);
+
+		$this->assertTrue($testModel->save($entity));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testSaveNewEntityWithDate()
+	{
+		$entity = new class extends Entity
+		{
+			protected $id;
+			protected $name;
+			protected $created_at;
+			protected $updated_at;
+			protected $_options = [
+				'datamap' => [],
+				'dates'   => [
+					'created_at',
+					'updated_at',
+					'deleted_at',
+				],
+				'casts'   => [],
+			];
+		};
+		$testModel = new class extends Model
+		{
+			protected $table = 'empty';
+			protected $allowedFields = [
+				'name',
+			];
+			protected $returnType = 'object';
+			protected $useSoftDeletes = true;
+			protected $dateFormat = 'date';
+			public $name = '';
+		};
+
+		$entity->name = 'Mark';
+		$entity->created_at = new Time('now');
+
+		$this->setPrivateProperty($testModel, 'useTimestamps', true);
+
+		$this->assertTrue($testModel->save($entity));
+
+		$testModel->truncate();
 	}
 }
