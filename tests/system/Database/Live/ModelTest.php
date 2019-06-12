a3e5db34c93cc01bc25251b5f9a4cddbf1f55c86
MGatner <mgatner@icloud.com>
2019-06-12 12:09:58 -0400

    Add test for missing/invalid model dateFormat

--- tests/system/Database/Live/ModelTest.php
+++ tests/system/Database/Live/ModelTest.php
@@ -1223,6 +1223,20 @@ class ModelTest extends CIDatabaseTestCase
 
 	//--------------------------------------------------------------------
 
+	/**
+	 * @expectedException        \CodeIgniter\Exceptions\ModelException
+	 * @expectedExceptionMessage `Tests\Support\Models\UserModel` model class does not have a valid dateFormat.
+	 */
+	public function testThrowsWithNoDateFormat()
+	{
+		$model = new UserModel();
+		$this->setPrivateProperty($model, 'dateFormat', '');
+
+		$model->find(1);
+	}
+
+	//--------------------------------------------------------------------
+
 	public function testInsertID()
 	{
 		$model = new JobModel();
