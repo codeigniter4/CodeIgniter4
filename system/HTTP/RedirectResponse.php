be9c06abb13820135bd90846285c962d164a6abf
Philipp Kerling <pkerling@casix.org>
2019-09-08 21:25:56 +0200

    Use site_url for route-based RedirectResponse
    base_url does not actually add the base path if the given URL is
    absolute (i.e., starts with a slash) and does not add the indexPage.
    Both is necessary for the redirect response to work correctly in case
    of routes. site_url handles this correctly.
    
    Fixes #2119

--- system/HTTP/RedirectResponse.php
+++ system/HTTP/RedirectResponse.php
@@ -91,7 +91,7 @@ class RedirectResponse extends Response
 			throw HTTPException::forInvalidRedirectRoute($route);
 		}
 
-		return $this->redirect(base_url($route), $method, $code);
+		return $this->redirect(site_url($route), $method, $code);
 	}
 
 	/**
