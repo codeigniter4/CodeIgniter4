6d18f2da9b0596f660bd370e9a84826f747a404a
MGatner <mgatner@icloud.com>
2019-09-11 14:34:53 -0400

    Add test for other prefixes

--- tests/system/Database/Live/MetadataTest.php
+++ tests/system/Database/Live/MetadataTest.php
@@ -11,10 +11,20 @@ class MetadataTest extends CIDatabaseTestCase
 
 	protected $seed = 'Tests\Support\Database\Seeds\CITestSeeder';
 
-	public function testCanListTables()
+	/**
+	 * Array of expected tables.
+	 *
+	 * @var array
+	 */
+	protected $expectedTables;
+
+	protected function setUp()
 	{
-		$prefix = $this->db->getPRefix();
-		$expected = [
+		parent::setUp();
+
+		// Prepare the array of expected tables once
+		$prefix = $this->db->getPrefix();
+		$this->expectedTables = [
 			$prefix . 'migrations',
 			$prefix . 'user',
 			$prefix . 'job',
@@ -22,31 +32,54 @@ class MetadataTest extends CIDatabaseTestCase
 			$prefix . 'empty',
 			$prefix . 'secondary'
 		];
-		
+	}
+
+	public function testListTables()
+	{
 		$result = $this->db->listTables();
-		
-		$this->assertEquals($expected, $result);
+
+		$this->assertEquals($this->expectedTables, array_values($result));
 	}
 
 	//--------------------------------------------------------------------
 
-	public function testCanListTablesConstrainPrefix()
+	public function testListTablesConstrainPrefix()
 	{
-		$prefix = $this->db->getPRefix();
-		$expected = [
-			$prefix . 'migrations',
-			$prefix . 'user',
-			$prefix . 'job',
-			$prefix . 'misc',
-			$prefix . 'empty',
-			$prefix . 'secondary'
-		];
-		
 		$result = $this->db->listTables(true);
-		
-		$this->assertEquals($expected, $result);
+
+		$this->assertEquals($this->expectedTables, array_values($result));
 	}
-  
+
+	//--------------------------------------------------------------------
+
+	public function testConstrainPrefixIgnoresOtherTables()
+	{
+		$this->forge = \Config\Database::forge($this->DBGroup);
+
+		// Stash the prefix and change it
+		$DBPrefix = $this->db->getPrefix();
+		$this->db->setPrefix('tmp_');
+
+		// Create a table with the new prefix
+		$fields = [
+			'name'       => ['type' => 'varchar', 'constraint' => 31],
+			'created_at' => ['type' => 'datetime', 'null' => true],
+		];
+		$this->forge->addField($fields);
+		$this->forge->createTable('widgets');
+
+		// Restore the prefix and get the tables with the original prefix
+		$this->db->setPrefix($DBPrefix);
+		$result = $this->db->listTables(true);
+
+		$this->assertEquals($expected, array_values($result));
+
+		// Clean up temporary table
+		$this->db->setPrefix('tmp_');
+		$forge->dropTable('widgets');
+		$this->db->setPrefix($DBPrefix);
+	}
+
 	//--------------------------------------------------------------------
 
 }
