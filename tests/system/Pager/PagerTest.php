45191119cbc753ab8d576628ff6cc2de9d0a7454
Jim Parry <jim_parry@telus.net>
2019-09-23 06:57:46 -0700

    PagerTest setup consistency fix

--- tests/system/Pager/PagerTest.php
+++ tests/system/Pager/PagerTest.php
@@ -23,10 +23,20 @@ class PagerTest extends \CIUnitTestCase
 		parent::setUp();
 		helper('url');
 
-		$_SERVER['HTTP_HOST'] = 'example.com';
-		$_GET                 = [];
-		$this->config         = new Pager();
-		$this->pager          = new \CodeIgniter\Pager\Pager($this->config, Services::renderer());
+		$_SERVER['HTTP_HOST']   = 'example.com';
+		$_SERVER['REQUEST_URI'] = '/';
+		$_GET                   = [];
+
+		$config          = new App();
+		$config->baseURL = 'http://example.com/';
+		$request         = Services::request($config);
+		$request->uri    = new URI('http://example.com');
+
+		Services::injectMock('request', $request);
+
+		$_GET         = [];
+		$this->config = new Pager();
+		$this->pager  = new \CodeIgniter\Pager\Pager($this->config, Services::renderer());
 	}
 
 	public function testSetPathRemembersPath()
