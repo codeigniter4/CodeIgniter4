e56d3f5f4889c0ff54ca5088b94071b8331786ce
MGatner <mgatner@icloud.com>
2019-10-08 15:11:01 -0400

    Convert all language returns to single quote

--- system/Language/en/Encryption.php
+++ system/Language/en/Encryption.php
@@ -16,7 +16,7 @@
 return [
    'noDriverRequested'    => 'No driver requested; Miss Daisy will be so upset!',
    'noHandlerAvailable'   => 'Unable to find an available {0} encryption handler.',
-   'unKnownHandler'       => "'{0}' cannot be configured.",
+   'unKnownHandler'       => '"{0}" cannot be configured.',
    'starterKeyNeeded'     => 'Encrypter needs a starter key.',
    'authenticationFailed' => 'Decrypting: authentication failed.',
    'encryptionFailed'     => 'Encryption failed.',
