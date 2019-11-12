030e4b23e5f0c837d1791d82e1d5bb3d7be41847
Project lead, CodeIgniter <jim_parry@telus.net>
2019-11-11 16:17:04 -0800

    Correct HTTP message typo

--- system/Language/en/HTTP.php
+++ system/Language/en/HTTP.php
@@ -38,7 +38,7 @@ return [
    'cannotSetFilepath'          => 'When setting binary can not set filepath: {0}',
    'notFoundDownloadSource'     => 'Not found download body source.',
    'cannotSetCache'             => 'It does not supported caching for downloading.',
-   'cannotSetStatusCode'        => 'It does not supported chnage status code for downloading. code: {0}, reason: {1}',
+   'cannotSetStatusCode'        => 'It does not supported change status code for downloading. code: {0}, reason: {1}',
 
 	// Response
    'missingResponseStatus'      => 'HTTP Response is missing a status code',
