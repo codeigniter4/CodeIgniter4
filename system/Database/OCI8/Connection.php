31f24bfb4b547d4fa759fa20eadb5b897c14fabe
Tetsuro Yoshikawa <phper.0o0@gmail.com>
2019-12-31 16:10:12 +0900

    feat: add enable foreign key method.

--- system/Database/OCI8/Connection.php
+++ system/Database/OCI8/Connection.php
@@ -498,7 +498,21 @@ SQL;
 	 */
 	protected function _enableForeignKeyChecks()
 	{
-		return 'SET FOREIGN_KEY_CHECKS=1';
+		return <<<SQL
+BEGIN
+  FOR c IN
+  (SELECT c.owner, c.table_name, c.constraint_name
+   FROM user_constraints c, user_tables t
+   WHERE c.table_name = t.table_name
+   AND c.status = 'DISABLED'
+   AND c.constraint_type = 'R'
+   AND t.iot_type IS NULL
+   ORDER BY c.constraint_type DESC)
+  LOOP
+    dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '" enable constraint "' || c.constraint_name || '"');
+  END LOOP;
+END;
+SQL;
 	}
 
 	//--------------------------------------------------------------------
