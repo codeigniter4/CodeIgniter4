80aa37e511518ab92bae259df03f8c1bad16a5e7
Lonnie Ezell <lonnieje@gmail.com>
2020-01-12 22:21:01 -0600

    [ci skip] Clean up Routes file so it is simpler to jump in and edit.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,22 +1,5 @@
 <?php namespace Config;
 
-/**
- * --------------------------------------------------------------------
- * URI Routing
- * --------------------------------------------------------------------
- * This file lets you re-map URI requests to specific controller functions.
- *
- * Typically there is a one-to-one relationship between a URL string
- * and its corresponding controller class/method. The segments in a
- * URL normally follow this pattern:
- *
- *    example.com/class/method/id
- *
- * In some instances, however, you may want to remap this relationship
- * so that a different class/function is called than the one
- * corresponding to the URL.
- */
-
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes(true);
 
@@ -31,31 +14,6 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php'))
  * --------------------------------------------------------------------
  * Router Setup
  * --------------------------------------------------------------------
- * The RouteCollection object allows you to modify the way that the
- * Router works, by acting as a holder for it's configuration settings.
- * The following methods can be called on the object to modify
- * the default operations.
- *
- *    $routes->defaultNamespace()
- *
- * Modifies the namespace that is added to a controller if it doesn't
- * already have one. By default this is the global namespace (\).
- *
- *    $routes->defaultController()
- *
- * Changes the name of the class used as a controller when the route
- * points to a folder instead of a class.
- *
- *    $routes->defaultMethod()
- *
- * Assigns the method inside the controller that is ran when the
- * Router is unable to determine the appropriate method to run.
- *
- *    $routes->setAutoRoute()
- *
- * Determines whether the Router will attempt to match URIs to
- * Controllers when no specific route has been defined. If false,
- * only routes that have been defined here will be available.
  */
 $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
