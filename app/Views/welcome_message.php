bd3803d3b5a64e45a783437875eecfa2d12e94a7
Little J <contact@littlej.fr>
2020-02-12 22:35:03 +0100

    Fixed typo

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -211,7 +211,6 @@ function mobileMenu() {
   for (var i = 0; i < menuItems.length; i ++) {
     if(menuItems[i].style.display === 'block') {
       menuItems[i].style.display = 'none';
-
     } else {
       menuItems[i].style.display = 'block';
     }
