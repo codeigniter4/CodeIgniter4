c4aa9c8a219bf0febb84c03d702c7659a3ad4f78
Little J <contact@littlej.fr>
2020-02-23 20:01:05 +0100

    Changed identation to match the current one

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -114,13 +114,13 @@
 			padding: 2.5rem 1.75rem 3.5rem 1.75rem;
 		}
 		section h1 {
-      margin-bottom: 2.5rem;
-    }
-    section h2 {
-      font-size: 120%;
-      line-height: 2.5rem;
-      padding-top: 1.5rem;
-    }
+			margin-bottom: 2.5rem;
+		}
+		section h2 {
+			font-size: 120%;
+			line-height: 2.5rem;
+			padding-top: 1.5rem;
+		}
 		section pre {
 			background-color: rgba(247, 248, 249, 1);
 			border: 1px solid rgba(242, 242, 242, 1);
@@ -138,15 +138,15 @@
 			color: rgba(221, 72, 20, 1);
 		}
 		section svg {
-      margin-bottom: -5px;
-      margin-right: 5px;
-      width: 25px;
-    }
-    .further {
-      background-color: rgba(247, 248, 249, 1);
-      border-bottom: 1px solid rgba(242, 242, 242, 1);
-      border-top: 1px solid rgba(242, 242, 242, 1);
-    }
+			margin-bottom: -5px;
+			margin-right: 5px;
+			width: 25px;
+		}
+		.further {
+			background-color: rgba(247, 248, 249, 1);
+			border-bottom: 1px solid rgba(242, 242, 242, 1);
+			border-top: 1px solid rgba(242, 242, 242, 1);
+		}
 		.further h2:first-of-type {
 			padding-top: 0;
 		}
@@ -247,42 +247,42 @@
 
 <div class="further">
 
-  <section>
+	<section>
 
-    <h1>Go further</h1>
+		<h1>Go further</h1>
 
-    <h2>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><rect x='32' y='96' width='64' height='368' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><line x1='112' y1='224' x2='240' y2='224' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><line x1='112' y1='400' x2='240' y2='400' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><rect x='112' y='160' width='128' height='304' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><rect x='256' y='48' width='96' height='416' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><path d='M422.46,96.11l-40.4,4.25c-11.12,1.17-19.18,11.57-17.93,23.1l34.92,321.59c1.26,11.53,11.37,20,22.49,18.84l40.4-4.25c11.12-1.17,19.18-11.57,17.93-23.1L445,115C443.69,103.42,433.58,94.94,422.46,96.11Z' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/></svg>
-      Learn
-    </h2>
+		<h2>
+			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><rect x='32' y='96' width='64' height='368' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><line x1='112' y1='224' x2='240' y2='224' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><line x1='112' y1='400' x2='240' y2='400' style='fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px'/><rect x='112' y='160' width='128' height='304' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><rect x='256' y='48' width='96' height='416' rx='16' ry='16' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/><path d='M422.46,96.11l-40.4,4.25c-11.12,1.17-19.18,11.57-17.93,23.1l34.92,321.59c1.26,11.53,11.37,20,22.49,18.84l40.4-4.25c11.12-1.17,19.18-11.57,17.93-23.1L445,115C443.69,103.42,433.58,94.94,422.46,96.11Z' style='fill:none;stroke:#000;stroke-linejoin:round;stroke-width:32px'/></svg>
+			Learn
+		</h2>
 
-    <p>The User Guide contains an introduction, tutorial, a number of "how to"
-      guides, and then reference documentation for the components that make up
-      the framework. Check the <a href="https://codeigniter4.github.io/userguide"
-      target="_blank">User Guide</a> !</p>
+		<p>The User Guide contains an introduction, tutorial, a number of "how to"
+			guides, and then reference documentation for the components that make up
+			the framework. Check the <a href="https://codeigniter4.github.io/userguide"
+			target="_blank">User Guide</a> !</p>
 
-    <h2>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M431,320.6c-1-3.6,1.2-8.6,3.3-12.2a33.68,33.68,0,0,1,2.1-3.1A162,162,0,0,0,464,215c.3-92.2-77.5-167-173.7-167C206.4,48,136.4,105.1,120,180.9a160.7,160.7,0,0,0-3.7,34.2c0,92.3,74.8,169.1,171,169.1,15.3,0,35.9-4.6,47.2-7.7s22.5-7.2,25.4-8.3a26.44,26.44,0,0,1,9.3-1.7,26,26,0,0,1,10.1,2L436,388.6a13.52,13.52,0,0,0,3.9,1,8,8,0,0,0,8-8,12.85,12.85,0,0,0-.5-2.7Z' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><path d='M66.46,232a146.23,146.23,0,0,0,6.39,152.67c2.31,3.49,3.61,6.19,3.21,8s-11.93,61.87-11.93,61.87a8,8,0,0,0,2.71,7.68A8.17,8.17,0,0,0,72,464a7.26,7.26,0,0,0,2.91-.6l56.21-22a15.7,15.7,0,0,1,12,.2c18.94,7.38,39.88,12,60.83,12A159.21,159.21,0,0,0,284,432.11' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/></svg>
-      Discuss
-    </h2>
+		<h2>
+			<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><path d='M431,320.6c-1-3.6,1.2-8.6,3.3-12.2a33.68,33.68,0,0,1,2.1-3.1A162,162,0,0,0,464,215c.3-92.2-77.5-167-173.7-167C206.4,48,136.4,105.1,120,180.9a160.7,160.7,0,0,0-3.7,34.2c0,92.3,74.8,169.1,171,169.1,15.3,0,35.9-4.6,47.2-7.7s22.5-7.2,25.4-8.3a26.44,26.44,0,0,1,9.3-1.7,26,26,0,0,1,10.1,2L436,388.6a13.52,13.52,0,0,0,3.9,1,8,8,0,0,0,8-8,12.85,12.85,0,0,0-.5-2.7Z' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><path d='M66.46,232a146.23,146.23,0,0,0,6.39,152.67c2.31,3.49,3.61,6.19,3.21,8s-11.93,61.87-11.93,61.87a8,8,0,0,0,2.71,7.68A8.17,8.17,0,0,0,72,464a7.26,7.26,0,0,0,2.91-.6l56.21-22a15.7,15.7,0,0,1,12,.2c18.94,7.38,39.88,12,60.83,12A159.21,159.21,0,0,0,284,432.11' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/></svg>
+			Discuss
+		</h2>
 
-    <p>CodeIgniter is a community-developed open source project, with several
-       venues for the community members to gather and exchange ideas. View all
-       the threads on <a href="https://forum.codeigniter.com/"
-       target="_blank">CodeIgniter's forum</a>, or <a href="https://codeigniterchat.slack.com/"
-       target="_blank">chat on Slack</a> !</p>
+		<p>CodeIgniter is a community-developed open source project, with several
+			 venues for the community members to gather and exchange ideas. View all
+			 the threads on <a href="https://forum.codeigniter.com/"
+			 target="_blank">CodeIgniter's forum</a>, or <a href="https://codeigniterchat.slack.com/"
+			 target="_blank">chat on Slack</a> !</p>
 
-    <h2>
-       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><line x1='176' y1='48' x2='336' y2='48' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><line x1='118' y1='304' x2='394' y2='304' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><path d='M208,48v93.48a64.09,64.09,0,0,1-9.88,34.18L73.21,373.49C48.4,412.78,76.63,464,123.08,464H388.92c46.45,0,74.68-51.22,49.87-90.51L313.87,175.66A64.09,64.09,0,0,1,304,141.48V48' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/></svg>
-       Contribute
-    </h2>
+		<h2>
+			 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><line x1='176' y1='48' x2='336' y2='48' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><line x1='118' y1='304' x2='394' y2='304' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/><path d='M208,48v93.48a64.09,64.09,0,0,1-9.88,34.18L73.21,373.49C48.4,412.78,76.63,464,123.08,464H388.92c46.45,0,74.68-51.22,49.87-90.51L313.87,175.66A64.09,64.09,0,0,1,304,141.48V48' style='fill:none;stroke:#000;stroke-linecap:round;stroke-miterlimit:10;stroke-width:32px'/></svg>
+			 Contribute
+		</h2>
 
-    <p>CodeIgniter is a community driven project and accepts contributions
-       of code and documentation from the community. Why not
-       <a href="https://codeigniter.com/en/contribute" target="_blank">
-       join us</a> ?</p>
+		<p>CodeIgniter is a community driven project and accepts contributions
+			 of code and documentation from the community. Why not
+			 <a href="https://codeigniter.com/en/contribute" target="_blank">
+			 join us</a> ?</p>
 
-  </section>
+	</section>
 
 </div>
 
