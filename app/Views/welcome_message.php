e8ad1cee3e8413642a4a571e6a6a8f7ef746a613
Little J <contact@littlej.fr>
2020-02-29 06:44:11 +0100

    [DEBUG+WP] Convert <section> from CSS to SASS with comments

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -56,7 +56,7 @@
 	  -moz-border-radius: 5px;
 	  -webkit-border-radius: 5px;
 	  display: block;
-	  font-weight: 250;
+	  font-weight: 300;
 	  text-decoration: none; }
 
 	header li.menu-item a {
@@ -143,43 +143,43 @@
 	    background-color: #DD4814;
 	    color: #FFFFFF; } }
 	section {
-	  margin: 0 auto;
 	  max-width: 1100px;
+	  margin: 0 auto;
 	  padding: 2.5rem 1.75rem 3.5rem 1.75rem; }
 
 	section h1 {
 	  margin-bottom: 2.5rem; }
 
 	section h2 {
-	  font-size: 120%;
 	  line-height: 2.5rem;
-	  padding-top: 1.5rem; }
+	  padding-top: 1.5rem;
+	  font-size: 120%; }
 
 	section pre {
 	  border: 1px solid #f3f3f3;
-	  background-color: #f7f8f9;
-	  display: block;
-	  font-size: .9rem;
+	  background-color: #FAFAFA;
 	  margin: 2rem 0;
 	  padding: 1rem 1.5rem;
 	  white-space: pre-wrap;
-	  word-break: break-all; }
+	  word-break: break-all;
+	  font-size: .9rem;
+	  display: block; }
 
 	section code {
 	  display: block; }
 
 	section a {
-	  color: #dd4814; }
+	  color: #DD4814; }
 
 	section svg {
+	  width: 25px;
 	  margin-bottom: -5px;
-	  margin-right: 5px;
-	  width: 25px; }
+	  margin-right: 5px; }
 
 	.further {
 	  border-bottom: 1px solid #f3f3f3;
 	  border-top: 1px solid #f3f3f3;
-	  background-color: #f7f8f9; }
+	  background-color: #FAFAFA; }
 
 	.further h2:first-of-type {
 	  padding-top: 0; }
