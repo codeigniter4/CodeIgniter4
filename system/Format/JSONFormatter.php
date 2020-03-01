fc1dc8cbdc07fa917aa0ff0318ff5e984a31ba8f
MashinaMashina <31430016+MashinaMashina@users.noreply.github.com>
2020-03-01 21:50:34 +0400

    Fix JSON error on recursion in data
    Bugs:
    https://github.com/codeigniter4/CodeIgniter4/issues/2434 and https://github.com/codeigniter4/CodeIgniter4/issues/2586

--- system/Format/JSONFormatter.php
+++ system/Format/JSONFormatter.php
@@ -56,7 +56,7 @@ class JSONFormatter implements FormatterInterface
 	 */
 	public function format($data)
 	{
-		$options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
+		$options = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PARTIAL_OUTPUT_ON_ERROR;
 
 		$options = ENVIRONMENT === 'production' ? $options : $options | JSON_PRETTY_PRINT;
 
