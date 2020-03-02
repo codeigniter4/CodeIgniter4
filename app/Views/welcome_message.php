74bf1b790d9901381048c6ac7b2d940d077b6943
Little J <contact@littlej.fr>
2020-03-02 12:04:15 +0100

    [WP] Update favicon + Page title

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Welcome to CodeIgniter 4!</title>
+  <title>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></title>
   <meta name="description" content="The small framework with powerful features">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
