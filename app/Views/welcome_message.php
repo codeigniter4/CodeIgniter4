71fb6667d4dce2110e5dc56788a5931b3a5ba45d
Little J <contact@littlej.fr>
2020-03-02 08:04:25 +0100

    [WP] Update menu items

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -219,12 +219,17 @@
 			<li class="menu-toggle">
 				<button onclick="toggleMenu();">&#9776;</button>
 			</li>
-			<li class="menu-item hidden"><a href="#">Home</a></li>
-			<li class="menu-item hidden"><a href="https://codeigniter4.github.io/userguide/" target="_blank">Docs</a>
+			<li class="menu-item hidden">
+				<a href="https://codeigniter.com" target="_blank" title="CodeIgniter.com">Discover</a>
 			</li>
-			<li class="menu-item hidden"><a href="https://forum.codeigniter.com/" target="_blank">Community</a></li>
-			<li class="menu-item hidden"><a
-					href="https://github.com/codeigniter4/CodeIgniter4/blob/master/CONTRIBUTING.md" target="_blank">Contribute</a>
+			<li class="menu-item hidden">
+				<a href="https://codeigniter4.github.io/userguide/" target="_blank" title="User guide">Learn</a>
+			</li>
+			<li class="menu-item hidden">
+				<a href="https://forum.codeigniter.com/" target="_blank" title="Forum">Discuss</a>
+			</li>
+			<li class="menu-item hidden">
+				<a href="https://github.com/codeigniter4/CodeIgniter4/blob/master/CONTRIBUTING.md" target="_blank" title="Github">Contribute</a>
 			</li>
 		</ul>
 	</div>
