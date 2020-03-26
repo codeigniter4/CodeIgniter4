49bf7f95973b8c2da59904294a6e0a816a9c7214
Instrye <instrye@gmail.com>
2020-03-26 17:34:15 +0800

    fix. MYSQLI::DBDebug can't woker

---
+++ tests/system/Database/Live/DEBugTest.php
@@ -0,0 +1,26 @@
+<?php namespace CodeIgniter\Database\Live;
+
+use CodeIgniter\Test\CIDatabaseTestCase;
+
+/**
+ * @group DatabaseLive
+ */
+class DEBugTest extends CIDatabaseTestCase
+{
+
+	protected $refresh = true;
+
+	public function testDBDebugTrue()
+	{
+		$this->setPrivateProperty($this->db, 'DBDebug', true);
+		$this->expectException('mysqli_sql_exception');
+		$result = $this->db->simpleQuery('SELECT * FROM db_error');
+	}
+
+	public function testDBDebugFalse()
+	{
+		$this->setPrivateProperty($this->db, 'DBDebug', false);
+		$result = $this->db->simpleQuery('SELECT * FROM db_error');
+		$this->assertEquals(false, $result);
+	}
+}
