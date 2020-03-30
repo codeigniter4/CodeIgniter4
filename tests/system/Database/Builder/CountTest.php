064155f027790e434ad671cc55ad80c7226aeeb0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-30 14:08:31 +0700

    add test for group+having and having only

--- tests/system/Database/Builder/CountTest.php
+++ tests/system/Database/Builder/CountTest.php
@@ -42,6 +42,8 @@ class CountTest extends \CodeIgniter\Test\CIUnitTestCase
 		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $answer));
 	}
 
+	//--------------------------------------------------------------------
+
 	public function testCountAllResultsWithGroupBy()
 	{
 		$builder = new BaseBuilder('jobs', $this->db);
@@ -51,6 +53,37 @@ class CountTest extends \CodeIgniter\Test\CIUnitTestCase
 		$answer = $builder->where('id >', 3)->countAllResults(false);
 
 		$expectedSQL = 'SELECT COUNT(*) AS "numrows" FROM ( SELECT * FROM "jobs" WHERE "id" > :id: GROUP BY "id" ) CI_count_all_results';
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $answer));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testCountAllResultsWithGroupByAndHaving()
+	{
+		$builder = new BaseBuilder('jobs', $this->db);
+		$builder->groupBy('id');
+		$builder->having('1=1');
+		$builder->testMode();
+
+		$answer = $builder->where('id >', 3)->countAllResults(false);
+
+		$expectedSQL = 'SELECT COUNT(*) AS "numrows" FROM ( SELECT * FROM "jobs" WHERE "id" > :id: GROUP BY "id" HAVING 1 = 1 ) CI_count_all_results';
+
+		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $answer));
+	}
+
+	//--------------------------------------------------------------------
+
+	public function testCountAllResultsWithHavingOnly()
+	{
+		$builder = new BaseBuilder('jobs', $this->db);
+		$builder->having('1=1');
+		$builder->testMode();
+
+		$answer = $builder->where('id >', 3)->countAllResults(false);
+
+		$expectedSQL = 'SELECT COUNT(*) AS "numrows" FROM "jobs" WHERE "id" > :id: HAVING 1 = 1';
 
 		$this->assertEquals($expectedSQL, str_replace("\n", ' ', $answer));
 	}
