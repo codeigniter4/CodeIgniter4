5479255b53cd6714ebb7ef7b2a0ca9623f212166
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-04-04 06:45:15 +0700

    add archive property to archive properties data on send

--- system/Test/Mock/MockEmail.php
+++ system/Test/Mock/MockEmail.php
@@ -4,8 +4,17 @@ use CodeIgniter\Email\Email;
 
 class MockEmail extends Email
 {
+	/**
+	 * Record of mock emails sent.
+	 *
+	 * @var array
+	 */
+	public $archive = [];
+
 	public function send($autoClear = true)
 	{
+		$this->archive = get_object_vars($this);
+
 		if ($autoClear)
 		{
 			$this->clear();
