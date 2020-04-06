3546cf9f4eb095f628fddc16896536d48e626f03
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-04-06 20:07:17 +0700

    make named constructor in Exception consistent: use return instead of throw

--- system/Exceptions/ConfigException.php
+++ system/Exceptions/ConfigException.php
@@ -16,6 +16,6 @@ class ConfigException extends CriticalError
 
 	public static function forDisabledMigrations()
 	{
-		throw new static(lang('Migrations.disabled'));
+		return new static(lang('Migrations.disabled'));
 	}
 }
