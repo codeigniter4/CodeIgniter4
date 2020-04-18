5627dd2a8b637a845f2dceba91803bd6553cb9f2
paul45 <paul.tallet.pinet@free.fr>
2020-04-19 00:54:19 +0200

    Update Model.php

--- system/Model.php
+++ system/Model.php
@@ -1138,7 +1138,7 @@ class Model
 
 		// Store it in the Pager library so it can be
 		// paginated in the views.
-		$this->pager = $pager->store($group, $page, $perPage, $total,$segment);
+		$this->pager = $pager->store($group, $page, $perPage, $total, $segment);
 		$perPage     = $this->pager->getPerPage($group);
 		$offset      = ($page - 1) * $perPage;
 
