fb2f413087b117a608736f533a6d41a2188de19a
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-05-13 19:56:53 +0800

    Force devs to turn off zlib.output_compression ini

--- system/Language/en/Core.php
+++ system/Language/en/Core.php
@@ -15,8 +15,9 @@
  */
 
 return [
-   'invalidFile'      => 'Invalid file: {0}',
-   'copyError'        => 'An error was encountered while attempting to replace the file({0}). Please make sure your file directory is writable.',
-   'missingExtension' => '{0} extension is not loaded.',
-   'noHandlers'       => '{0} must provide at least one Handler.',
+   'copyError'                    => 'An error was encountered while attempting to replace the file({0}). Please make sure your file directory is writable.',
+   'enabledZlibOutputCompression' => 'Your zlib.output_compression ini directive is turned on. This will not work well with output buffers.',
+   'invalidFile'                  => 'Invalid file: {0}',
+   'missingExtension'             => '{0} extension is not loaded.',
+   'noHandlers'                   => '{0} must provide at least one Handler.',
 ];
