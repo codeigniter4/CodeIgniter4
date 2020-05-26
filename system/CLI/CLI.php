14f9aed7ab46c2aa16dac247ca710efeeb30cef8
mostafakhudair <59371810+mostafakhudair@users.noreply.github.com>
2020-05-26 12:15:49 +0200

    Fix CLI output colors
    Remove dark_blue it uses light_blue code
    correct light_yellow and yellow colors

--- system/CLI/CLI.php
+++ system/CLI/CLI.php
@@ -94,7 +94,6 @@ class CLI
 		'black'        => '0;30',
 		'dark_gray'    => '1;30',
 		'blue'         => '0;34',
-		'dark_blue'    => '1;34',
 		'light_blue'   => '1;34',
 		'green'        => '0;32',
 		'light_green'  => '1;32',
@@ -104,8 +103,8 @@ class CLI
 		'light_red'    => '1;31',
 		'purple'       => '0;35',
 		'light_purple' => '1;35',
-		'light_yellow' => '0;33',
-		'yellow'       => '1;33',
+		'yellow'       => '0;33',
+		'light_yellow' => '1;33',
 		'light_gray'   => '0;37',
 		'white'        => '1;37',
 	];
