44472e54e3d30ba0480b7176e0714cad1bcfb334
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-06-22 19:45:37 +0200

    New Exception methods
    new exception methods forInvalidFormatter & forInvalidMime using in getFormatter

--- system/Format/Exceptions/FormatException.php
+++ system/Format/Exceptions/FormatException.php
@@ -1,12 +1,25 @@
-<?php namespace CodeIgniter\Format\Exceptions;
+<?php
+
+namespace CodeIgniter\Format\Exceptions;
 
 use CodeIgniter\Exceptions\ExceptionInterface;
+use RuntimeException;
 
-class FormatException extends \RuntimeException implements ExceptionInterface
+class FormatException extends RuntimeException implements ExceptionInterface
 {
+	public static function forInvalidFormatter(string $class)
+	{
+		return new static(lang('Format.invalidFormatter', [$class]));
+	}
+
 	public static function forInvalidJSON(string $error = null)
 	{
 		return new static(lang('Format.invalidJSON', [$error]));
+	}
+
+	public static function forInvalidMime(string $mime)
+	{
+		return new static(lang('Format.invalidMime', [$mime]));
 	}
 
 	/**
