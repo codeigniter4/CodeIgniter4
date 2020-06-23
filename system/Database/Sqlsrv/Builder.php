1e43715a3eb0e307584ac3dadadc1ba221f0123b
Ferenc Kurucz <ferenc.kurucz@kerry.ie>
2020-06-23 09:58:22 +0100

    Update Sqlsrv driver

---
+++ system/Database/Sqlsrv/Builder.php
@@ -0,0 +1,359 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ */
+
+namespace CodeIgniter\Database\Sqlsrv;
+
+use CodeIgniter\Database\BaseBuilder;
+use CodeIgniter\Database\Exceptions\DataException;
+
+/**
+ * Builder for Sqlsrv
+ */
+class Builder extends BaseBuilder {
+
+	/// TODO: auto check for TextCastToInt
+	/// TODO: auto check for InsertIndexValue
+	/// TODO: replace: delete index entries before insert
+
+	/**
+	 * ORDER BY random keyword
+	 *
+	 * @var array
+	 */
+	protected $randomKeyword = [
+		'NEWID()',
+		'RAND(%d)',
+	];
+
+	/**
+	 * Quoted identifier flag
+	 *
+	 * Whether to use SQL-92 standard quoted identifier
+	 * (double quotes) or brackets for identifier escaping.
+	 *
+	 * @var boolean
+	 */
+	protected $_quoted_identifier = true;
+
+	// handle increment/decrement on text
+	public $castTextToInt = true;
+
+	public $keyPermission = false;
+
+	protected function _truncate(string $table) : string
+	{
+		return 'TRUNCATE TABLE ' . $table;
+	}
+
+	/**
+	 * Insert statement
+	 *
+	 * Generates a platform-specific insert string from the supplied data
+	 * auto-enable
+	 *
+	 * @param string $table         The table name
+	 * @param array  $keys          The insert keys
+	 * @param array  $unescapedKeys The insert values
+	 *
+	 * @return string
+	 */
+	protected function _insert(string $table, array $keys, array $unescapedKeys): string
+	{
+		$fullTableName = $this->getFullName($table);
+
+		// check for index key
+		// TODO: implement check for this instad static $insertKeyPermission
+
+		// insert statement
+		$statement = 'INSERT INTO ' . $fullTableName . ' (' . implode(',', $keys) . ') VALUES (' . implode(', ', $unescapedKeys) . ')';
+
+		return $this->keyPermission ? $this->addIdentity($fullTableName, $statement) : $statement;
+	}
+
+	protected function _update(string $table, array $values) : string
+	{
+		$valstr = [];
+
+		foreach ($values as $key => $val)
+		{
+			$valstr[] = $key . ' = ' . $val;
+		}
+
+		$statement = 'UPDATE ' . ( empty($this->QBLimit) ? '' : 'TOP(' . $this->QBLimit . ') ' ) . $table . ' SET '
+			. implode(', ', $valstr) . $this->compileWhereHaving('QBWhere') . $this->compileOrderBy();
+
+		return $this->keyPermission ? $this->addIdentity($this->getFullName($table), $statement) : $statement;
+	}
+
+	public function increment(string $column, int $value = 1)
+	{
+		$column = $this->db->protectIdentifiers($column);
+
+		if ($this->castTextToInt)
+		{
+			$values = [ $column => "CONVERT(VARCHAR(MAX),CONVERT(INT,CONVERT(VARCHAR(MAX), {$column})) + {$value})" ];
+		}
+		else
+		{
+			$values = [ $column => "{$column} + {$value}" ];
+		}
+		$sql = $this->_update($this->QBFrom[0], $values);
+
+		return $this->db->query($sql, $this->binds, false);
+	}
+
+	public function decrement(string $column, int $value = 1)
+	{
+		$column = $this->db->protectIdentifiers($column);
+
+		if ($this->castTextToInt)
+		{
+			$values = [ $column => "CONVERT(VARCHAR(MAX),CONVERT(INT,CONVERT(VARCHAR(MAX), {$column})) - {$value})" ];
+		}
+		else
+		{
+			$values = [ $column => "{$column} + {$value}" ];
+		}
+		$sql = $this->_update($this->QBFrom[0], $values);
+
+		return $this->db->query($sql, $this->binds, false);
+	}
+
+	private function getFullName(string $table): string
+	{
+		// TODO: replace static dbo group with config
+		return '[' . $this->db->getDatabase() . '].[dbo].[' . str_replace('"', '', $table) . ']';
+	}
+
+	/**
+	 * Add permision statements for index value inserts
+	 *
+	 * @param string $fullTable full table name
+	 * @param string $insert    statement
+	 *
+	 * @return string
+	 */
+	private function addIdentity(string $fullTable, string $insert): string
+	{
+		return 'SET IDENTITY_INSERT ' . $fullTable . " ON\n" . $insert . "\nSET IDENTITY_INSERT " . $fullTable . ' OFF';
+	}
+
+	// not used -> custom implementation required
+	protected function _limit(string $sql, bool $offsetIgnore = false): string
+	{
+		// As of SQL Server 2012 (11.0.*) OFFSET is supported
+		if (version_compare($this->version(), '11', '>='))
+		{
+			// SQL Server OFFSET-FETCH can be used only with the ORDER BY clause
+			empty($this->qb_orderby) && $sql .= ' ORDER BY 1';
+
+			return $sql . ' OFFSET ' . (int) $this->qb_offset . ' ROWS FETCH NEXT ' . $this->qb_limit . ' ROWS ONLY';
+		}
+	}
+
+	public function replace(array $set = null)
+	{
+		$keyPermission = $this->keyPermission;
+		// TODO: delete old entry
+		$this->keyPermission = true;
+		$this->insert( $set );
+		$this->keyPermission = $keyPermission;
+	}
+
+	// handle float return value
+	protected function maxMinAvgSum(string $select = '', string $alias = '', string $type = 'MAX')
+	{
+		// int functions can be handled by parent
+		if (! in_array($type, ['AVG']))
+		{
+			return parent::maxMinAvgSum($select, $alias, $type);
+		}
+
+		if ($select === '')
+		{
+			throw DataException::forEmptyInputGiven('Select');
+		}
+
+		if (strpos($select, ',') !== false)
+		{
+			throw DataException::forInvalidArgument('column name not separated by comma');
+		}
+
+		if ($alias === '')
+		{
+			$alias = $this->createAliasFromTable(trim($select));
+		}
+
+		$sql = $type . '( CAST( ' . $this->db->protectIdentifiers(trim($select)) . ' AS FLOAT ) ) AS ' . $this->db->escapeIdentifiers(trim($alias));
+
+		$this->QBSelect[]   = $sql;
+		$this->QBNoEscape[] = null;
+
+		return $this;
+	}
+
+	protected function _delete(string $table): string
+	{
+		return 'DELETE' . (empty($this->QBLimit) ? '' : ' TOP ' . $this->QBLimit . '') . ' FROM ' . $table . $this->compileWhereHaving('QBWhere');
+	}
+
+	protected function compileSelect($select_override = false): string
+	{
+		if (empty($this->QBLimit) || $select_override !== false)
+		{
+			return parent::compileSelect($select_override);
+		}
+
+		$sql = ( ! $this->QBDistinct) ? 'SELECT ' : 'SELECT DISTINCT ';
+
+		if (! empty($this->QBLimit))
+		{
+			$sql .= 'TOP ' . $this->QBLimit . ' ';
+		}
+
+		if (empty($this->QBSelect))
+		{
+			$sql .= '*';
+		}
+		else
+		{
+			// Cycle through the "select" portion of the query and prep each column name.
+			// The reason we protect identifiers here rather than in the select() function
+			// is because until the user calls the from() function we don't know if there are aliases
+			foreach ($this->QBSelect as $key => $val)
+			{
+				$no_escape            = $this->QBNoEscape[$key] ?? null;
+				$this->QBSelect[$key] = $this->db->protectIdentifiers($val, false, $no_escape);
+			}
+
+			$sql .= implode(', ', $this->QBSelect);
+		}
+
+		// Write the "FROM" portion of the query
+		if (! empty($this->QBFrom))
+		{
+			$sql .= "\nFROM " . $this->_fromTables();
+		}
+
+		// Write the "JOIN" portion of the query
+		if (! empty($this->QBJoin))
+		{
+			$sql .= "\n" . implode("\n", $this->QBJoin);
+		}
+
+		$sql .= $this->compileWhereHaving('QBWhere')
+			. $this->compileGroupBy()
+			. $this->compileWhereHaving('QBHaving')
+			. $this->compileOrderBy(); // ORDER BY
+
+			var_dump($sql);
+		return $sql;
+	}
+
+	protected function whereHaving(string $qb_key, $key, $value = null, string $type = 'AND ', bool $escape = null)
+	{
+		if (! is_array($key))
+		{
+			$key = [$key => $value];
+		}
+
+		// If the escape value was not set will base it on the global setting
+		is_bool($escape) || $escape = $this->db->protectIdentifiers;
+
+		foreach ($key as $k => $v)
+		{
+			$prefix = empty($this->$qb_key) ? $this->groupGetType('') : $this->groupGetType($type);
+
+			if ($v !== null)
+			{
+				$op = $this->getOperator($k, true);
+
+				if (! empty($op))
+				{
+					$k = trim($k);
+
+					end($op);
+
+					$op = trim(current($op));
+
+					if (substr($k, -1 * strlen($op)) === $op)
+					{
+						$k = rtrim(strrev(preg_replace(strrev('/' . $op . '/'), strrev(''), strrev($k), 1)));
+					}
+				}
+
+				$bind = $this->setBind($k, $v, $escape);
+
+				if (empty($op))
+				{
+					$k .= ' LIKE';
+				}
+				else
+				{
+					$k .= " $op";
+				}
+
+				if ($v instanceof Closure)
+				{
+					$builder = $this->cleanClone();
+					$v       = '(' . str_replace("\n", ' ', $v($builder)->getCompiledSelect()) . ')';
+				}
+				else
+				{
+					$v = " :$bind:";
+				}
+			}
+			elseif (! $this->hasOperator($k) && $qb_key !== 'QBHaving')
+			{
+				// value appears not to have been set, assign the test to IS NULL
+				$k .= ' IS NULL';
+			}
+			elseif (preg_match('/\s*(!?=|<>|IS(?:\s+NOT)?)\s*$/i', $k, $match, PREG_OFFSET_CAPTURE))
+			{
+				$k = substr($k, 0, $match[0][1]) . ($match[1][0] === '=' ? ' IS NULL' : ' IS NOT NULL');
+			}
+
+			$this->{$qb_key}[] = [
+				'condition' => $prefix . $k . $v,
+				'escape'    => $escape,
+			];
+		}
+
+		return $this;
+	}
+}
---
+++ system/Database/Sqlsrv/Forge.php
@@ -0,0 +1,372 @@
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2014-2019 British Columbia Institute of Technology (https://bcit.ca/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 3.0.0
+ * @filesource
+ */
+
+namespace CodeIgniter\Database\Sqlsrv;
+
+/**
+ * Forge for Sqlsrv
+ */
+class Forge extends \CodeIgniter\Database\Forge {
+
+	/**
+	 * DROP CONSTRAINT statement
+	 *
+	 * @var string
+	 */
+	protected $dropConstraintStr = 'ALTER TABLE %s DROP CONSTRAINT %s';
+
+	/**
+	 * CREATE DATABASE IF statement
+	 *
+	 * @var string
+	 */
+	/// TODO: missing charset, collat & check for existant
+	protected $createDatabaseIfStr = "DECLARE @DBName VARCHAR(255) = '%s'\nDECLARE @SQL VARCHAR(max) = 'IF DB_ID( ''' + @DBName + ''' ) IS NULL CREATE DATABASE ' + @DBName\nEXEC( @SQL )";
+
+	/// TODO: missing charset & collat
+	protected $createDatabaseStr = 'CREATE DATABASE %s ';
+
+	/**
+	 * CHECK DATABASE EXIST statement
+	 *
+	 * @var string
+	 */
+	protected $checkDatabaseExistStr = 'IF DB_ID( %s ) IS NOT NULL SELECT 1';
+
+	/**
+	 * RENAME TABLE statement
+	 *
+	 * While the below statement would work, it returns an error.
+	 * Also MS recommends dropping and dropping and re-creating the table.
+	 * https://docs.microsoft.com/en-us/sql/relational-databases/system-stored-procedures/sp-rename-transact-sql?view=sql-server-2017
+	 * 'EXEC sp_rename %s , %s ;'
+	 *
+	 * @var string
+	 */
+	protected $renameTableStr = 'EXEC sp_rename %s , %s ;';
+
+	/**
+	 * UNSIGNED support
+	 *
+	 * @var array
+	 */
+	protected $unsigned = [
+		'TINYINT'  => 'SMALLINT',
+		'SMALLINT' => 'INT',
+		'INT'      => 'BIGINT',
+		'REAL'     => 'FLOAT',
+	];
+
+	/**
+	 * CREATE TABLE IF statement
+	 *
+	 * @var string
+	 */
+	protected $createTableIfStr = "IF NOT EXISTS (SELECT * FROM sysobjects WHERE ID = object_id(N'%s') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)\nCREATE TABLE";
+
+	/**
+	 * DROP TABLE IF statement
+	 *
+	 * @var string
+	 */
+	protected $_drop_table_if = "IF EXISTS (SELECT * FROM sysobjects WHERE ID = object_id(N'%s') AND OBJECTPROPERTY(id, N'IsUserTable') = 1)\nDROP TABLE";
+
+	protected $createTableStr = "%s %s (%s\n) ";
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * CREATE TABLE attributes
+	 *
+	 * @param  array $attributes Associative array of table attributes
+	 * @return string
+	 */
+	protected function _createTableAttributes(array $attributes): string
+	{
+		return '';
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * ALTER TABLE
+	 *
+	 * @param string $alter_type ALTER type
+	 * @param string $table      Table name
+	 * @param mixed  $field      Column definition
+	 *
+	 * @return string|array
+	 */
+	protected function _alterTable(string $alter_type, string $table, $field)
+	{
+		if (in_array($alter_type, ['DROP', 'ADD'], true))
+		{
+			return parent::_alterTable($alter_type, $table, $field);
+		}
+
+		$sql = 'ALTER TABLE ' . $this->db->escapeIdentifiers($table);
+
+		$sqls = [];
+		foreach ($field as $data)
+		{
+			if ($data['_literal'] !== false)
+			{
+				return false;
+			}
+
+			if (isset($data['type']))
+			{
+				$sqls[] = $sql . ' ALTER COLUMN ' . $this->db->escapeIdentifiers($data['name'])
+					. " {$data['type']}{$data['length']}";
+			}
+
+			if (! empty($data['default']))
+			{
+				$sqls[] = $sql . ' ALTER COLUMN ADD CONSTRAINT ' . $this->db->escapeIdentifiers($data['name']) . '_def'
+					. " DEFAULT {$data['default']} FOR " . $this->db->escapeIdentifiers($data['name']);
+			}
+
+			if (isset($data['null']))
+			{
+				$sqls[] = $sql . ' ALTER COLUMN ' . $this->db->escapeIdentifiers($data['name'])
+					. ($data['null'] === true ? ' DROP' : '') . " {$data['type']}{$data['length']} NOT NULL";
+			}
+
+			if (! empty($data['comment']))
+			{
+				$sqls[] = 'EXEC sys.sp_addextendedproperty '
+					. "@name=N'Caption', @value=N'" . $data['comment'] . "' , "
+					. "@level0type=N'SCHEMA',@level0name=N'dbo', "
+					. "@level1type=N'TABLE',@level1name=N'" . $this->db->escapeIdentifiers($table) . "', "
+					. "@level2type=N'COLUMN',@level2name=N'" . $this->db->escapeIdentifiers($data['name']) . "'";
+			}
+			if (! empty($data['new_name']))
+			{
+				// EXEC sp_rename '[dbo].[db_misc].[value]', 'valueasdasd', 'COLUMN';
+				$sqls[] = "EXEC sp_rename  '[dbo].[" . $table . '].[' . $data['name'] . "]' , '" . $data['new_name'] . "', 'COLUMN';";
+			}
+		}
+
+		return $sqls;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Process column
+	 *
+	 * @param  array $field
+	 * @return string
+	 */
+	protected function _processColumn(array $field): string
+	{
+		return $this->db->escapeIdentifiers($field['name'])
+			. (empty($field['new_name']) ? '' : ' ' . $this->db->escapeIdentifiers($field['new_name']))
+			. ' ' . $field['type'] . $field['length']
+			. $field['default']
+			. $field['null']
+			. $field['auto_increment']
+			. '' // (empty($field['comment']) ? '' : ' COMMENT ' . $field['comment'])
+			. $field['unique'];
+	}
+	//--------------------------------------------------------------------
+	/**
+	 * Process foreign keys
+	 *
+	 * @param string $table Table name
+	 *
+	 * @return string
+	 */
+	protected function _processForeignKeys(string $table): string
+	{
+		$sql = '';
+
+		$allowActions = [
+			'CASCADE',
+			'SET NULL',
+			'NO ACTION',
+			'RESTRICT',
+			'SET DEFAULT',
+		];
+
+		if (count($this->foreignKeys) > 0)
+		{
+			foreach ($this->foreignKeys as $field => $fkey)
+			{
+				/*
+				  ALTER TABLE [dbo].[NewTable]  ADD FOREIGN KEY ([system]) REFERENCES [dbo].[lofasz] ([asdasdasd])
+				 *
+				 * CONSTRAINT FK_TempSales_SalesReason FOREIGN KEY (TempID)  REFERENCES Sales.SalesReason (SalesReasonID)         ON DELETE CASCADE        ON UPDATE CASCADE                    */
+				$name_index = $table . '_' . $field . '_foreign';
+
+				$sql .= ",\n\t CONSTRAINT " . $this->db->escapeIdentifiers($name_index)
+					. ' FOREIGN KEY (' . $this->db->escapeIdentifiers($field) . ') '
+					. ' REFERENCES ' . $this->db->escapeIdentifiers($this->db->getPrefix() . $fkey['table']) . ' (' . $this->db->escapeIdentifiers($fkey['field']) . ')';
+
+				if ($fkey['onDelete'] !== false && in_array($fkey['onDelete'], $allowActions))
+				{
+					$sql .= ' ON DELETE ' . $fkey['onDelete'];
+				}
+
+				if ($fkey['onUpdate'] !== false && in_array($fkey['onUpdate'], $allowActions))
+				{
+					$sql .= ' ON UPDATE ' . $fkey['onUpdate'];
+				}
+			}
+		}
+
+		return $sql;
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Process primary keys
+	 *
+	 * @param string $table Table name
+	 *
+	 * @return string
+	 */
+	protected function _processPrimaryKeys(string $table): string
+	{
+		for ($i = 0, $c = count($this->primaryKeys); $i < $c; $i++)
+		{
+			if (! isset($this->fields[$this->primaryKeys[$i]]))
+			{
+				unset($this->primaryKeys[$i]);
+			}
+		}
+
+		if (count($this->primaryKeys) > 0)
+		{
+			$sql = ",\n\tCONSTRAINT " . $this->db->escapeIdentifiers('pk_' . $table)
+				. ' PRIMARY KEY(' . implode(', ', $this->db->escapeIdentifiers($this->primaryKeys)) . ')';
+		}
+
+		return $sql ?? '';
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Field attribute TYPE
+	 *
+	 * Performs a data type mapping between different databases.
+	 *
+	 * @param array &$attributes
+	 *
+	 * @return void
+	 */
+	protected function _attributeType(array &$attributes)
+	{
+		// Reset field lengths for data types that don't support it
+		if (isset($attributes['CONSTRAINT']) && stripos($attributes['TYPE'], 'int') !== false)
+		{
+			$attributes['CONSTRAINT'] = null;
+		}
+
+		switch (strtoupper($attributes['TYPE'])) {
+			case 'MEDIUMINT':
+				$attributes['TYPE']     = 'INTEGER';
+				$attributes['UNSIGNED'] = false;
+				break;
+			case 'INTEGER':
+				$attributes['TYPE'] = 'INT';
+
+				break;
+			case 'ENUM':
+				$attributes['TYPE']       = 'TEXT';
+				$attributes['CONSTRAINT'] = null;
+
+				break;
+			/*case 'DATETIME':
+				$attributes['TYPE'] = 'TIMESTAMP';
+				break;*/
+			case 'TIMESTAMP':
+				$attributes['TYPE'] = 'DATETIME';
+				break;
+			default:
+				break;
+		}
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Field attribute AUTO_INCREMENT
+	 *
+	 * @param array &$attributes
+	 * @param array &$field
+	 *
+	 * @return void
+	 */
+	protected function _attributeAutoIncrement(array &$attributes, array &$field)
+	{
+		if (! empty($attributes['AUTO_INCREMENT']) && $attributes['AUTO_INCREMENT'] === true && stripos($field['type'], 'INT') !== false)
+		{
+			$field['auto_increment'] = ' IDENTITY(1,1)';
+		}
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Drop Table
+	 *
+	 * Generates a platform-specific DROP TABLE string
+	 *
+	 * @param string  $table     Table name
+	 * @param boolean $if_exists Whether to add an IF EXISTS condition
+	 * @param boolean $cascade
+	 *
+	 * @return string
+	 */
+	protected function _dropTable(string $table, bool $if_exists, bool $cascade): string
+	{
+		$sql = parent::_dropTable($table, $if_exists, $cascade);
+
+		if ($cascade === true)
+		{
+			$sql .= ' CASCADE';
+		}
+
+		return $sql;
+	}
+
+	//--------------------------------------------------------------------
+}
