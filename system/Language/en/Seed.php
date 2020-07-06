13c016e5320415b9b8396624eff5a609fa1c4922
Abilio Posada <abilioposada@gmail.com>
2020-07-06 10:02:10 -0600

    Add seed language
    Add seed language file

---
+++ system/Language/en/Seed.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Seed language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+    // Seed command
+    'nameFile'   => 'Name the seeder file',
+    'badName'    => 'You must provide a seeder file name.',
+    'writeError' => 'Error trying to create {0} file, check if the directory is writable.',
+];
