8e680c76adf828696ea90e47711582a94be8b1d2
Lonnie Ezell <lonnieje@gmail.com>
2020-07-13 23:32:12 -0500

    Finished up tests

--- tests/system/Commands/ClearCacheTest.php
+++ tests/system/Commands/ClearCacheTest.php
@@ -20,29 +20,7 @@ class ClearCacheTest extends CIUnitTestCase
 
 		CITestStreamFilter::$buffer = '';
 		$this->streamFilter         = stream_filter_append(STDOUT, 'CITestStreamFilter');
-
-		$this->env = new \CodeIgniter\Config\DotEnv(ROOTPATH);
-		$this->env->load();
-
-		// Set environment values that would otherwise stop the framework from functioning during tests.
-		if (! isset($_SERVER['app.baseURL']))
-		{
-			$_SERVER['app.baseURL'] = 'http://example.com';
-		}
-
-		$_SERVER['argv'] = [
-			'spark',
-			'list',
-		];
-		$_SERVER['argc'] = 2;
-		CLI::init();
-
-		$this->config   = new MockAppConfig();
-		$this->request  = new \CodeIgniter\HTTP\IncomingRequest($this->config, new \CodeIgniter\HTTP\URI('https://somwhere.com'), null, new UserAgent());
-		$this->response = new \CodeIgniter\HTTP\Response($this->config);
-		$this->logger   = Services::logger();
-		$this->runner   = new CommandRunner();
-		$this->runner->initController($this->request, $this->response, $this->logger);
+		$this->streamFilter         = stream_filter_append(STDERR, 'CITestStreamFilter');
 	}
 
 	public function tearDown(): void
@@ -57,13 +35,23 @@ class ClearCacheTest extends CIUnitTestCase
 
 	public function testClearCacheInvalidHandler()
 	{
-		$config          = config('Cache');
-		$config->handler = 'junk';
-		Config::injectMock('Cache', $config);
-
-		$this->runner->index(['cache:clear']);
+		command('cache:clear junk');
 		$result = CITestStreamFilter::$buffer;
 
 		$this->assertStringContainsString('junk is not a valid cache handler.', $result);
+	}
+
+	public function testClearCacheWorks()
+	{
+		cache()->save('foo', 'bar');
+
+		$this->assertEquals('bar', cache('foo'));
+
+		command('cache:clear');
+		$result = CITestStreamFilter::$buffer;
+
+		$this->assertNull(cache('foo'));
+
+		$this->assertStringContainsString('Done', $result);
 	}
 }
