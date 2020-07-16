7a2179a74a309e1b42664ae08cd9ad32a76b10e7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-07-16 07:51:01 +0700

    by pass $this->dom->documentElement to saveHTML(), the mb_strpos is not needed, use strpos instead

--- system/Test/DOMParser.php
+++ system/Test/DOMParser.php
@@ -143,7 +143,7 @@ class DOMParser
 		if (is_null($element))
 		{
 			$content = $this->dom->saveHTML($this->dom->documentElement);
-			return mb_strpos($content, $search) !== false;
+			return strpos($content, $search) !== false;
 		}
 
 		$result = $this->doXPath($search, $element);
