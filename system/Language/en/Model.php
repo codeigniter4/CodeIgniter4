58e9cd2ff771607442db6fb5e9e29df1e1e0d576
Abilio Posada <abilioposada@gmail.com>
2020-07-29 13:25:58 -0600

    Create and update language files for messages

--- system/Language/en/Model.php
+++ system/Language/en/Model.php
@@ -1,22 +1,21 @@
 <?php
 
 /**
- * CLI language strings.
+ * Seed language strings.
  *
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
  * @copyright  2019-2020 CodeIgniter Foundation
  * @license    https://opensource.org/licenses/MIT	MIT License
  * @link       https://codeigniter.com
+ * @since      Version 4.0.0
  * @filesource
  *
  * @codeCoverageIgnore
  */
 
 return [
-	'tryingToAccessNonExistentProperty' => 'Trying to access non existent property {0} of {1}',
-
-	// Entity command
-	'nameFile'   => 'Name the entity file',
+	// Model command
+	'nameFile'   => 'Name the model file',
 	'writeError' => 'Error trying to create {0} file, check if the directory is writable.',
 ];
