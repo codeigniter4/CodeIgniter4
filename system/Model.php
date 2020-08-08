f0c0933377f5d15854f2ff02b8e6dc4d63034ebd
Mohammad <mds1121@yahoo.com>
2020-08-08 19:48:08 +0430

    Update Model.php
    add setAllowedFields method

--- system/Model.php
+++ system/Model.php
@@ -1578,6 +1578,19 @@ class Model
 
 		return $error['message'] ?? null;
 	}
+	
+	/**
+	 * Allows to set allowed fields.
+	 * It could be used when you have to change default or override current allowed fields.
+	 *
+	 * @param array $allowedFields
+	 *
+	 * @return void
+	 */
+	public function setAllowedFields(array $allowedFields)
+	{
+		$this->$allowedFields = $allowedFields;
+	}
 
 	//--------------------------------------------------------------------
 	//--------------------------------------------------------------------
