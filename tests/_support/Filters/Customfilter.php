2364da566272fbf67a0db01b9f09589ab07d4a24
devorama <33496470+devorama@users.noreply.github.com>
2020-08-18 07:02:27 +0200

    Add Custom filter Testing logic
    Signed-off-by: devorama <33496470+devorama@users.noreply.github.com>

---
+++ tests/_support/Filters/Customfilter.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Support\Filters;
+
+
+class Customfilter implements \CodeIgniter\Filters\FilterInterface
+{
+
+	public function before(RequestInterface $request, $arguments = null)
+	{
+//		
+	}
+
+	public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+	{
+
+	}
+
+	//--------------------------------------------------------------------
+} 
