0822a97449fc9c1e3163c5d0c5bd9b4340d40634
devorama <33496470+devorama@users.noreply.github.com>
2020-08-18 12:06:50 +0200

    Add custom filter test inside route to make sure custom filter was loaded and can be used Call then in CodeIgniter test to make sure the loading of route and filters work till the calling of the controller, thus whole code module loading successs
    Signed-off-by: devorama <33496470+devorama@users.noreply.github.com>

--- tests/_support/Config/Routes.php
+++ tests/_support/Config/Routes.php
@@ -7,4 +7,4 @@
 $routes->add('testing', 'TestController::index', ['as' => 'testing-index']);
 
 /*Will throw error if filter is not found, thus testing the custom filter ability*/
-$routes->add('testingfilter', 'TestController::index',  ['filter' => 'test-customfilter']);
+$routes->add('testingfilter', 'Hello::index',  ['filter' => 'test-customfilter']);
