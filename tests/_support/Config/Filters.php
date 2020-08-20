c613908e38a352d9ba809caa876ff4c6e7467b76
devorama <33496470+devorama@users.noreply.github.com>
2020-08-20 11:03:10 +0200

    Correct class creation in custom filter
    Signed-off-by: devorama <33496470+devorama@users.noreply.github.com>

--- tests/_support/Config/Filters.php
+++ tests/_support/Config/Filters.php
@@ -1,2 +1,4 @@
-<?php namespace Tests\Support\Config\Filters;
-$customfilters->aliases['test-customfilter'] = Tests\Support\Filters\Customfilter::class;
+<?php 
+namespace Tests\Support\Config\Filters;
+
+$customfilters->aliases['test-customfilter'] = \Tests\Support\Filters\Customfilter::class;
