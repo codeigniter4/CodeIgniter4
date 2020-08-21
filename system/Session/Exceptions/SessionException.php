53dbb735d3370cccd4b70bce4c966f9d631c9947
Mattias Sandstrom <1013635+tangix@users.noreply.github.com>
2020-08-21 09:46:41 +0200

    Fixed comment in SessionException

--- system/Session/Exceptions/SessionException.php
+++ system/Session/Exceptions/SessionException.php
@@ -30,13 +30,6 @@ class SessionException extends FrameworkException implements ExceptionInterface
 		return new static(lang('Session.invalidSavePathFormat', [$path]));
 	}
 
-	/**
-	 * For Invalid SameSite attribute setting
-	 *
-	 * @param string $samesite
-	 *
-	 * @return \CodeIgniter\HTTP\Exceptions\HTTPException
-	 */
 	public static function forInvalidSameSiteSetting(string $samesite)
 	{
 		return new static(lang('Session.invalidSameSiteSetting', [$samesite]));
