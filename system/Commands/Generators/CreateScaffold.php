a0e659fe0657e46ed235aaefa3d76da2c8267ae4
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-09-02 19:25:20 +0800

    Allow fallback when make:scaffold's argument is missing

--- system/Commands/Generators/CreateScaffold.php
+++ system/Commands/Generators/CreateScaffold.php
@@ -129,7 +129,7 @@ class CreateScaffold extends BaseCommand
 		];
 
 		// Call those commands!
-		$class = $params[0];
+		$class = $params[0] ?? CLI::getSegment(2);
 		$this->call('make:controller', array_merge([$class], $controllerOpts, $genOptions));
 		$this->call('make:model', array_merge([$class], $modelOpts, $genOptions));
 		$this->call('make:entity', array_merge([$class], $genOptions));
