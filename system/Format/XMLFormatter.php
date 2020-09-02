fd1695539421a45de142a19931442685b91a03f7
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-09-03 01:34:41 +0800

    Add XML tags normaliser

--- system/Format/XMLFormatter.php
+++ system/Format/XMLFormatter.php
@@ -47,7 +47,6 @@ use Config\Format;
  */
 class XMLFormatter implements FormatterInterface
 {
-
 	/**
 	 * Takes the given data and formats it.
 	 *
@@ -77,8 +76,6 @@ class XMLFormatter implements FormatterInterface
 		return $output->asXML();
 	}
 
-	//--------------------------------------------------------------------
-
 	/**
 	 * A recursive method to convert an array into a valid XML string.
 	 *
@@ -95,25 +92,42 @@ class XMLFormatter implements FormatterInterface
 		{
 			if (is_array($value))
 			{
-				if (is_numeric($key))
-				{
-					$key = "item{$key}";
-				}
-
+				$key     = $this->normalizeXMLTag($key);
 				$subnode = $output->addChild("$key");
 				$this->arrayToXML($value, $subnode);
 			}
 			else
 			{
-				if (is_numeric($key))
-				{
-					$key = "item{$key}";
-				}
-
+				$key = $this->normalizeXMLTag($key);
 				$output->addChild("$key", htmlspecialchars("$value"));
 			}
 		}
 	}
 
-	//--------------------------------------------------------------------
+	/**
+	 * Normalizes tags into the allowed by W3C.
+	 * Regex adopted from this StackOverflow answer.
+	 *
+	 * @param string|integer $key
+	 *
+	 * @return string
+	 *
+	 * @see https://stackoverflow.com/questions/60001029/invalid-characters-in-xml-tag-name
+	 */
+	protected function normalizeXMLTag($key)
+	{
+		$startChar = 'A-Z_a-z' .
+			'\\x{C0}-\\x{D6}\\x{D8}-\\x{F6}\\x{F8}-\\x{2FF}\\x{370}-\\x{37D}' .
+			'\\x{37F}-\\x{1FFF}\\x{200C}-\\x{200D}\\x{2070}-\\x{218F}' .
+			'\\x{2C00}-\\x{2FEF}\\x{3001}-\\x{D7FF}\\x{F900}-\\x{FDCF}' .
+			'\\x{FDF0}-\\x{FFFD}\\x{10000}-\\x{EFFFF}';
+		$validName = $startChar . '\\.\\d\\x{B7}\\x{300}-\\x{36F}\\x{203F}-\\x{2040}';
+
+		$key = trim($key);
+		$key = preg_replace("/[^{$validName}-]+/u", '', $key);
+		$key = preg_replace("/^[^{$startChar}]+/u", 'item$0', $key);
+		$key = preg_replace('/^[xml]+/iu', 'item$0', $key);
+
+		return $key;
+	}
 }
