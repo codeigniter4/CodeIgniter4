53bf5363e15f3669fdf03c649e6919cf5c40e8bf
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-09-04 10:34:39 +0200

    Update and rename system/Database/Postgre/Utils.php to system/Database/Drivers/Postgre/Utils.php

--- system/Database/Drivers/Postgre/Utils.php
+++ system/Database/Drivers/Postgre/Utils.php
@@ -36,7 +36,7 @@
  * @filesource
  */
 
-namespace CodeIgniter\Database\Postgre;
+namespace CodeIgniter\Database\Drivers\Postgre;
 
 use CodeIgniter\Database\BaseUtils;
 use CodeIgniter\Database\Exceptions\DatabaseException;
