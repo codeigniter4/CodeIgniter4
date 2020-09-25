4ab0ee46f567ea9f7c298a934b49656c5f845d14
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-09-25 21:53:46 +0200

    Update SessionException.php

--- system/Session/Exceptions/SessionException.php
+++ system/Session/Exceptions/SessionException.php
@@ -1,35 +1,125 @@
-<?php namespace CodeIgniter\Session\Exceptions;
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ * Copyright (c) 2019-2020 CodeIgniter Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT - MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ */
+
+namespace CodeIgniter\Session\Exceptions;
 
 use CodeIgniter\Exceptions\ExceptionInterface;
 use CodeIgniter\Exceptions\FrameworkException;
 
+/**
+ * SessionException
+ */
 class SessionException extends FrameworkException implements ExceptionInterface
 {
+	/**
+	 * Thrown when database table is not found.
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forMissingDatabaseTable()
 	{
 		return new static(lang('Session.missingDatabaseTable'));
 	}
 
+  	//--------------------------------------------------------------------
+
+	/**
+	 * Thrown when session save path is invalid.
+	 *
+	 * @param string|null $path
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forInvalidSavePath(string $path = null)
 	{
 		return new static(lang('Session.invalidSavePath', [$path]));
 	}
 
+  	//--------------------------------------------------------------------
+
+	/**
+	 * Thrown when session save pass is readonly.
+	 *
+	 * @param string|null $path
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forWriteProtectedSavePath(string $path = null)
 	{
 		return new static(lang('Session.writeProtectedSavePath', [$path]));
 	}
 
+  	//--------------------------------------------------------------------
+
+	/**
+	 * Thrown when session save path is empty.
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forEmptySavepath()
 	{
 		return new static(lang('Session.emptySavePath'));
 	}
 
+  	//--------------------------------------------------------------------
+
+	/**
+	 * Thrown when session save path format is invalid.
+	 *
+	 * @param string $path
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forInvalidSavePathFormat(string $path)
 	{
 		return new static(lang('Session.invalidSavePathFormat', [$path]));
 	}
 
+  	//--------------------------------------------------------------------
+
+	/**
+	 * Thrown when samesite setting is invalid.
+	 *
+	 * @param string $samesite
+	 *
+	 * @return \CodeIgniter\Session\Exceptions\SessionException
+	 */
 	public static function forInvalidSameSiteSetting(string $samesite)
 	{
 		return new static(lang('Session.invalidSameSiteSetting', [$samesite]));
