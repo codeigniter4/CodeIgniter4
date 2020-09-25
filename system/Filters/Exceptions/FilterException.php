15016d84ee4512486be3e7e91ee8dc7e713f3b6f
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-09-25 21:11:05 +0200

    Update FilterException.php

--- system/Filters/Exceptions/FilterException.php
+++ system/Filters/Exceptions/FilterException.php
@@ -31,7 +31,7 @@
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
  * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT	MIT License
+ * @license    https://opensource.org/licenses/MIT - MIT License
  * @link       https://codeigniter.com
  * @since      Version 4.0.0
  * @filesource
@@ -48,24 +48,26 @@ use CodeIgniter\Exceptions\ExceptionInterface;
 class FilterException extends ConfigException implements ExceptionInterface
 {
 	/**
-	 * Thrown when the provided alias is not within
-	 * the list of configured filter aliases.
+	 * Thrown when the provided alias is not within the list of
+	 * configured filter aliases.
 	 *
 	 * @param string $alias
 	 *
-	 * @return static
+	 * @return \CodeIgniter\Filters\Exceptions\FilterException
 	 */
 	public static function forNoAlias(string $alias)
 	{
 		return new static(lang('Filters.noFilter', [$alias]));
 	}
 
+  	//--------------------------------------------------------------------
+
 	/**
 	 * Thrown when the filter class does not implement FilterInterface.
 	 *
 	 * @param string $class
 	 *
-	 * @return static
+	 * @return \CodeIgniter\Filters\Exceptions\FilterException
 	 */
 	public static function forIncorrectInterface(string $class)
 	{
