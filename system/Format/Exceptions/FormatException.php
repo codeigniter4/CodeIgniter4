8da2769dc75aba4cdfcb7fd4f3877b48d9f33cf4
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-09-25 21:14:12 +0200

    Update FormatException.php

--- system/Format/Exceptions/FormatException.php
+++ system/Format/Exceptions/FormatException.php
@@ -31,7 +31,7 @@
  * @package    CodeIgniter
  * @author     CodeIgniter Dev Team
  * @copyright  2019-2020 CodeIgniter Foundation
- * @license    https://opensource.org/licenses/MIT	MIT License
+ * @license    https://opensource.org/licenses/MIT - MIT License
  * @link       https://codeigniter.com
  * @since      Version 4.0.0
  * @filesource
@@ -40,12 +40,12 @@
 namespace CodeIgniter\Format\Exceptions;
 
 use CodeIgniter\Exceptions\ExceptionInterface;
-use RuntimeException;
+use CodeIgniter\Exceptions\FrameworkException;
 
 /**
  * FormatException
  */
-class FormatException extends RuntimeException implements ExceptionInterface
+class FormatException extends FrameworkException implements ExceptionInterface
 {
 	/**
 	 * Thrown when the instantiated class does not exist.
@@ -59,39 +59,41 @@ class FormatException extends RuntimeException implements ExceptionInterface
 		return new static(lang('Format.invalidFormatter', [$class]));
 	}
 
+  	//--------------------------------------------------------------------
+
 	/**
-	 * Thrown in JSONFormatter when the json_encode produces
-	 * an error code other than JSON_ERROR_NONE and JSON_ERROR_RECURSION.
+	 * Thrown in JSONFormatter when the json_encode produces an error code
+	 * other than JSON_ERROR_NONE and JSON_ERROR_RECURSION.
 	 *
 	 * @param string $error
 	 *
-	 * @return FormatException
+	 * @return \CodeIgniter\Format\Exceptions\FormatException
 	 */
 	public static function forInvalidJSON(string $error = null)
 	{
 		return new static(lang('Format.invalidJSON', [$error]));
 	}
 
+  	//--------------------------------------------------------------------
+
 	/**
-	 * Thrown when the supplied MIME type has no
-	 * defined Formatter class.
+	 * Thrown when the supplied MIME type has no defined Formatter class.
 	 *
 	 * @param string $mime
 	 *
-	 * @return FormatException
+	 * @return \CodeIgniter\Format\Exceptions\FormatException
 	 */
 	public static function forInvalidMime(string $mime)
 	{
 		return new static(lang('Format.invalidMime', [$mime]));
 	}
 
+  	//--------------------------------------------------------------------
+
 	/**
-	 * Thrown on XMLFormatter when the `simplexml` extension
-	 * is not installed.
+	 * Thrown on XMLFormatter when the `simplexml` extension isn't installed.
 	 *
-	 * @return FormatException
-	 *
-	 * @codeCoverageIgnore
+	 * @return \CodeIgniter\Format\Exceptions\FormatException
 	 */
 	public static function forMissingExtension()
 	{
