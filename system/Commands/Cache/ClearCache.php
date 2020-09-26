c17e0001c6fcb981b660e9ec328f96159ff2e4c1
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-09-26 19:52:33 +0800

    Update cache commands

--- system/Commands/Cache/ClearCache.php
+++ system/Commands/Cache/ClearCache.php
@@ -1,9 +1,51 @@
-<?php namespace CodeIgniter\Commands\Cache;
+<?php
+
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014-2019 British Columbia Institute of Technology
+ * Copyright (c) 2019-2020 CodeIgniter Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ */
+
+namespace CodeIgniter\Commands\Cache;
 
 use CodeIgniter\Cache\CacheFactory;
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
 
+/**
+ * Clears current cache.
+ */
 class ClearCache extends BaseCommand
 {
 	/**
@@ -50,12 +92,13 @@ class ClearCache extends BaseCommand
 	 */
 	public function run(array $params)
 	{
-		$config = config('Cache');
-
+		$config  = config('Cache');
 		$handler = $params[0] ?? $config->handler;
+
 		if (! array_key_exists($handler, $config->validHandlers))
 		{
 			CLI::error($handler . ' is not a valid cache handler.');
+
 			return;
 		}
 
@@ -64,10 +107,13 @@ class ClearCache extends BaseCommand
 
 		if (! $cache->clean())
 		{
+			// @codeCoverageIgnoreStart
 			CLI::error('Error while clearing the cache.');
+
 			return;
+			// @codeCoverageIgnoreEnd
 		}
 
-		CLI::write(CLI::color('Done', 'green'));
+		CLI::write(CLI::color('Cache cleared.', 'green'));
 	}
 }
