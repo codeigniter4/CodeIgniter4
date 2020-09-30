3f72ead6a575aeaa3083b0f71087a8bcc758964a
michalsn <michal@sniatala.pl>
2020-09-30 16:17:04 +0200

    Move DB config to Registrar

--- tests/_support/Config/Registrar.php
+++ tests/_support/Config/Registrar.php
@@ -8,24 +8,85 @@
 
 class Registrar
 {
+	/**
+	 * DB config array for testing purposes.
+	 *
+	 * @var array
+	 */
+	protected $dbConfig = [
+		'MySQLi'  => [
+			'DSN'      => '',
+			'hostname' => '127.0.0.1',
+			'username' => 'root',
+			'password' => '',
+			'database' => 'test',
+			'DBDriver' => 'MySQLi',
+			'DBPrefix' => 'db_',
+			'pConnect' => false,
+			'DBDebug'  => (ENVIRONMENT !== 'production'),
+			'charset'  => 'utf8',
+			'DBCollat' => 'utf8_general_ci',
+			'swapPre'  => '',
+			'encrypt'  => false,
+			'compress' => false,
+			'strictOn' => false,
+			'failover' => [],
+			'port'     => 3306,
+		],
+		'Postgre' => [
+			'DSN'      => '',
+			'hostname' => 'localhost',
+			'username' => 'postgres',
+			'password' => 'postgres',
+			'database' => 'test',
+			'DBDriver' => 'Postgre',
+			'DBPrefix' => 'db_',
+			'pConnect' => false,
+			'DBDebug'  => (ENVIRONMENT !== 'production'),
+			'charset'  => 'utf8',
+			'DBCollat' => 'utf8_general_ci',
+			'swapPre'  => '',
+			'encrypt'  => false,
+			'compress' => false,
+			'strictOn' => false,
+			'failover' => [],
+			'port'     => 5432,
+		],
+		'SQLite3' => [
+			'DSN'      => '',
+			'hostname' => 'localhost',
+			'username' => '',
+			'password' => '',
+			'database' => 'database.db',
+			'DBDriver' => 'SQLite3',
+			'DBPrefix' => 'db_',
+			'pConnect' => false,
+			'DBDebug'  => (ENVIRONMENT !== 'production'),
+			'charset'  => 'utf8',
+			'DBCollat' => 'utf8_general_ci',
+			'swapPre'  => '',
+			'encrypt'  => false,
+			'compress' => false,
+			'strictOn' => false,
+			'failover' => [],
+			'port'     => 3306,
+		],
+	];
 
+	/**
+	 * Override database config
+	 *
+	 * @return array
+	 */
 	public static function Database()
 	{
 		$config = [];
 
 		// Under Github Actions, we can set an ENV var named 'DB'
 		// so that we can test against multiple databases.
-		if ($group = getenv('DB'))
+		if ($group = getenv('DB') && ! empty($this->dbConfig[$group]))
 		{
-			if (is_file(TESTPATH . '_github/Database.php'))
-			{
-				require TESTPATH . '_github/Database.php';
-
-				if (! empty($dbconfig) && array_key_exists($group, $dbconfig))
-				{
-					$config['tests'] = $dbconfig[$group];
-				}
-			}
+			$config['tests'] = $this->dbConfig[$group];
 		}
 
 		return $config;
