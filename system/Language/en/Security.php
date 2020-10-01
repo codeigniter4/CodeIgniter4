13a279977df8d140a525b42b07cca24e323cead4
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-10-01 10:50:45 +0200

    Separate Security Configuration

---
+++ system/Language/en/Security.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Security language strings.
+ *
+ * @package    CodeIgniter
+ * @author     CodeIgniter Dev Team
+ * @copyright  2019-2020 CodeIgniter Foundation
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 4.0.0
+ * @filesource
+ *
+ * @codeCoverageIgnore
+ */
+
+return [
+   'disallowedAction' => 'The action you requested is not allowed.',
+   'invalidSameSite'  => 'The SameSite value must be None, Lax, Strict, or a blank string. Given: {0}',
+];
