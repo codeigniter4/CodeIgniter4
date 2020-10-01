2bc0c4faba6ad7b8df0a6fcdc5f61dcc4ede0930
Mostafa Khudair <59371810+mostafakhudair@users.noreply.github.com>
2020-10-01 15:41:16 +0200

    Separate Security Configuration

--- system/Test/Mock/MockSecurityConfig.php
+++ system/Test/Mock/MockSecurityConfig.php
@@ -6,13 +6,13 @@ use Config\Security as SecurityConfig;
 
 class MockSecurityConfig extends SecurityConfig
 {
-    public $CSRFProtection  = false;
-    public $CSRFTokenName   = 'csrf_test_name';
-    public $CSRFHeaderName  = 'X-CSRF-TOKEN';
-    public $CSRFCookieName  = 'csrf_cookie_name';
-    public $CSRFExpire      = 7200;
-    public $CSRFRegenerate  = true;
-    public $CSRFExcludeURIs = ['http://example.com'];
-    public $CSRFRedirect    = false;
-    public $CSRFSameSite    = 'Lax';
+    public $protection  = false;
+    public $tokenName   = 'csrf_test_name';
+    public $headerName  = 'X-CSRF-TOKEN';
+    public $cookieName  = 'csrf_cookie_name';
+    public $expire      = 7200;
+    public $regenerate  = true;
+    public $excludeURIs = ['http://example.com'];
+    public $redirect    = false;
+    public $samesite    = 'Lax';
 }
