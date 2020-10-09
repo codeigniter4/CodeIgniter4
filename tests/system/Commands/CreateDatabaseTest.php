7b4fab7e1919ca81833d98e945eb7b6cecb17169
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-10-10 01:15:43 +0800

    Fill tests for uncovered lines

--- tests/system/Commands/CreateDatabaseTest.php
+++ tests/system/Commands/CreateDatabaseTest.php
@@ -2,6 +2,7 @@
 
 namespace CodeIgniter\Commands;
 
+use CodeIgniter\Config\Config;
 use CodeIgniter\Test\CIUnitTestCase;
 use CodeIgniter\Test\Filters\CITestStreamFilter;
 use Config\Database;
@@ -23,13 +24,22 @@ class CreateDatabaseTest extends CIUnitTestCase
 	protected function tearDown(): void
 	{
 		stream_filter_remove($this->streamFilter);
-		$file = WRITEPATH . 'foobar.db';
 
-		if (file_exists($file))
+		if (Database::connect()->getPlatform() === 'SQLite3')
 		{
-			unlink($file);
+			$file = WRITEPATH . 'foobar.db';
+
+			if (file_exists($file))
+			{
+				unlink($file);
+			}
+		}
+		else
+		{
+			Database::forge()->dropDatabase('foobar');
 		}
 
+		Config::reset();
 		parent::tearDown();
 	}
 
@@ -56,5 +66,35 @@ class CreateDatabaseTest extends CIUnitTestCase
 
 		command('db:create foobar --ext db');
 		$this->assertStringContainsString('already exists.', $this->getBuffer());
+	}
+
+	public function testOtherDriverDuplicatedDatabaseThrowsDatabaseException()
+	{
+		if (Database::connect()->getPlatform() === 'SQLite3')
+		{
+			$this->markTestSkipped('Needs to run on non-SQLite3 drivers.');
+		}
+
+		command('db:create foobar');
+		CITestStreamFilter::$buffer = '';
+
+		command('db:create foobar');
+		$this->assertStringContainsString('Unable to create the specified database.', $this->getBuffer());
+	}
+
+	public function testOtherDriverDuplicatedDatabaseOnSilent()
+	{
+		if (Database::connect()->getPlatform() === 'SQLite3')
+		{
+			$this->markTestSkipped('Needs to run on non-SQLite3 drivers.');
+		}
+
+		config('Database')->tests['DBDebug'] = false;
+
+		command('db:create foobar');
+		CITestStreamFilter::$buffer = '';
+
+		command('db:create foobar');
+		$this->assertStringContainsString('Database creation failed.', $this->getBuffer());
 	}
 }
