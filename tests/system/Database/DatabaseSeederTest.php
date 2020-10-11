258700d9f4ec2cfb5e1d26250cff243994f886c0
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-10-11 18:42:41 +0800

    Don't instantiate Faker on Seeder constructor

---
+++ tests/system/Database/DatabaseSeederTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CodeIgniter\Database;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use Config\Database;
+use Faker\Generator;
+
+class DatabaseSeederTest extends CIUnitTestCase
+{
+	public function testInstantiateNoSeedPath()
+	{
+		$this->expectException('InvalidArgumentException');
+
+		$config            = new Database();
+		$config->filesPath = '';
+		new Seeder($config);
+	}
+
+	public function testInstantiateNotDirSeedPath()
+	{
+		$this->expectException('InvalidArgumentException');
+
+		$config            = new Database;
+		$config->filesPath = APPPATH . 'Foo';
+		new Seeder($config);
+	}
+
+	public function testFakerGet()
+	{
+		$this->assertInstanceOf(Generator::class, Seeder::faker());
+	}
+
+	public function testCallOnEmptySeeder()
+	{
+		$this->expectException('InvalidArgumentException');
+
+		$seeder = new Seeder(new Database());
+		$seeder->call('');
+	}
+}
