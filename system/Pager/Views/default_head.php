c3dcd05e35de4a3e65fa79c5bd027f5f1fcacc79
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2020-10-24 16:38:41 +0800

    Use short notices for header docblock

--- system/Pager/Views/default_head.php
+++ system/Pager/Views/default_head.php
@@ -1,8 +1,17 @@
 <?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * @var \CodeIgniter\Pager\PagerRenderer $pager
  */
-
 $pager->setSurroundCount(0);
 
 if ($pager->hasPrevious())
--- utils/PHPStan/CheckUseStatementsAfterLicenseRule.php
+++ utils/PHPStan/CheckUseStatementsAfterLicenseRule.php
@@ -18,6 +18,11 @@ final class CheckUseStatementsAfterLicenseRule implements Rule
 	 */
 	private const ERROR_MESSAGE = 'Use statement must be located after license docblock';
 
+	/**
+	 * @var string
+	 */
+	private const COPYRIGHT_REGEX = '/\* \(c\) CodeIgniter Foundation <admin@codeigniter\.com>/m';
+
 	public function getNodeType(): string
 	{
 		return Stmt::class;
@@ -29,6 +34,7 @@ final class CheckUseStatementsAfterLicenseRule implements Rule
 	public function processNode(Node $node, Scope $scope): array
 	{
 		$comments = $node->getAttribute('comments');
+
 		if ($comments === [])
 		{
 			return [];
@@ -41,13 +47,13 @@ final class CheckUseStatementsAfterLicenseRule implements Rule
 				continue;
 			}
 
-			$text = $comment->getText();
-			if (! preg_match('/\* Copyright \(c\) 2019-\d{4,} CodeIgniter Foundation\n/', $text))
+			if (! preg_match(self::COPYRIGHT_REGEX, $comment->getText()))
 			{
 				continue;
 			}
 
 			$previous = $node->getAttribute('previous');
+
 			while ($previous)
 			{
 				if ($previous instanceof Use_)
