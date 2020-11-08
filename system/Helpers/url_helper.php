b36de3e2f885cba31325e1afbeb24087d47eff15
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-11-08 20:35:35 +0700

    add forControllerNotFound() and forInvalidRoute() static method to RouterException

--- system/Helpers/url_helper.php
+++ system/Helpers/url_helper.php
@@ -637,12 +637,10 @@ if (! function_exists('url_to'))
 
 			if (isset($explode[1]))
 			{
-				// @phpstan-ignore-next-line
-				throw new RouterException(lang('HTTP.controllerNotFound', [$explode[0], $explode[1]]));
+				throw RouterException::forControllerNotFound($explode[0], $explode[1]);
 			}
 
-			// @phpstan-ignore-next-line
-			throw new RouterException(lang('HTTP.invalidRoute', [$controller]));
+			throw RouterException::forInvalidRoute($controller);
 		}
 
 		return site_url($route);
