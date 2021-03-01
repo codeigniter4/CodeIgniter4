542ac246934340c4a8efd2a735f3528e1d583f75
kenjis <kenji.uui@gmail.com>
2021-03-01 15:05:19 +0900

    fix: make the constructor to final in DebugTraceableTrait

--- system/Exceptions/DebugTraceableTrait.php
+++ system/Exceptions/DebugTraceableTrait.php
@@ -20,7 +20,7 @@ trait DebugTraceableTrait
 	 * @param integer        $code
 	 * @param Throwable|null $previous
 	 */
-	public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+	final public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
 	{
 		parent::__construct($message, $code, $previous);
 
