e7a085f7df664867adce12d02a3e0edfa87e1f6f
MGatner <mgatner@icloud.com>
2021-05-23 19:09:49 +0000

    Add Publisher

---
+++ system/Language/en/Publisher.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// Publisher language settings
+return [
+	'expectedFile'      => 'Publisher::{0} expects a valid file.',
+	'expectedDirectory' => 'Publisher::{0} expects a valid directory.',
+	'collision'         => 'Publisher encountered an unexpected {0} while copying {1} to {2}.',
+];
