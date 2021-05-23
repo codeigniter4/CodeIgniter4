492b16f867be07a3d59b8d01bb1f1124ef6e6c63
MGatner <mgatner@icloud.com>
2021-05-23 19:09:49 +0000

    Add Publisher

---
+++ system/Publisher/Exceptions/PublisherException.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of the CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Publisher\Exceptions;
+
+use CodeIgniter\Exceptions\FrameworkException;
+
+class PublisherException extends FrameworkException
+{
+	public static function forExpectedDirectory(string $caller)
+	{
+		return new static(lang('Publisher.expectedDirectory', [$caller]));
+	}
+
+	public static function forExpectedFile(string $caller)
+	{
+		return new static(lang('Publisher.expectedFile', [$caller]));
+	}
+
+	public static function forCollision(string $from, string $to)
+	{
+		return new static(lang('Publisher.collision', [filetype($to), $from, $to]));
+	}
+}
