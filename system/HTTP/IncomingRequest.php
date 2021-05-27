821dbb1ebc16a4509c17d88889ae61972baecfc9
Milan Hoško <sosko666@gmail.com>
2021-05-27 13:03:02 +0200

    String to int
    My mistake.

--- system/HTTP/IncomingRequest.php
+++ system/HTTP/IncomingRequest.php
@@ -154,7 +154,7 @@ class IncomingRequest extends Request
 		// Get Maximum avaible memory
 		$memory_limit = preg_split('/\d+\K/', ini_get('memory_limit'));
 		// Translate memory limit from human readible to bytes and substract already used memory
-		$free_memory = $memory_limit[0] * pow(1024, (array_search(strtolower($memory_limit[1]), ["k", "m", "g", "t", "p"]) ?: -1) + 1) - memory_get_peak_usage();
+		$free_memory = ((int) $memory_limit[0]) * pow(1024, (array_search(strtolower($memory_limit[1]), ["k", "m", "g", "t", "p"]) ?: -1) + 1) - memory_get_peak_usage();
 		// Get our body from php://input - if the send content is too big, it wouldn't be load to the memory
 		if ($body === 'php://input' && ((int) $_SERVER['CONTENT_LENGTH'] ?: 0) < $free_memory)
 		{
