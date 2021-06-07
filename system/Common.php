da6d09eb4fa5daa5a11ed2afcd70e486b8ef0bc3
Jérôme Gamez <jerome@gamez.name>
2021-06-07 13:50:42 +0200

    Remove removed `safe_mode` ini Option
    `safe_mode` was removed in PHP 5.4

--- system/Common.php
+++ system/Common.php
@@ -793,8 +793,8 @@ if (! function_exists('is_really_writable'))
      */
     function is_really_writable(string $file): bool
     {
-        // If we're on a Unix server with safe_mode off we call is_writable
-        if (DIRECTORY_SEPARATOR === '/' || ! ini_get('safe_mode'))
+        // If we're on a Unix server we call is_writable
+        if (DIRECTORY_SEPARATOR === '/')
         {
             return is_writable($file);
         }
