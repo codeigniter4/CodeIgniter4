8ea319b746d0ce20d744eb634b95fa512a1b8be2
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2021-06-11 01:30:35 +0800

    Ensure one space in concatenations

--- utils/PhpCsFixer/CodeIgniter4.php
+++ utils/PhpCsFixer/CodeIgniter4.php
@@ -90,6 +90,7 @@ final class CodeIgniter4 extends AbstractRuleset
                 ],
             ],
             'compact_nullable_typehint'             => true,
+            'concat_space'                          => ['spacing' => 'one'],
             'function_to_constant'                  => true,
             'indentation_type'                      => true,
             'line_ending'                           => true,
