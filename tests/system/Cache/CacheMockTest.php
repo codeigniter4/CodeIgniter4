09609d5ab050e89d26dac6e93a465a27aff66254
Lonnie Ezell <lonnieje@gmail.com>
2021-06-13 23:37:10 -0500

    New mock() test helper and expanded MockCache with assertions.

---
+++ tests/system/Cache/CacheMockTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Cache;
+
+use CodeIgniter\Cache\Handlers\BaseHandler;
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\Mock\MockCache;
+
+/**
+ * @internal
+ */
+final class CacheMockTest extends CIUnitTestCase
+{
+    public function testMockReturnsMockCacheClass()
+    {
+        $this->assertInstanceOf(BaseHandler::class, service('cache'));
+
+        $mock = mock(CacheFactory::class);
+        $this->assertInstanceOf(MockCache::class, $mock);
+        $this->assertInstanceOf(MockCache::class, service('cache'));
+    }
+
+    public function testMockCaching()
+    {
+        $mock = mock(CacheFactory::class);
+
+        // Ensure it stores the value normally
+        $mock->save('foo', 'bar');
+        $mock->assertHas('foo');
+        $mock->assertHasValue('foo', 'bar');
+
+        // Try it again with bypass on
+        $mock->bypass();
+        $mock->save('foo', 'bar');
+        $mock->assertMissing('foo');
+    }
+}
