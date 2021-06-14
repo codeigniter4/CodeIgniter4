55966e8fb433d2d1c2252f5224107e785b82fd2a
Lonnie Ezell <lonnieje@gmail.com>
2021-06-14 08:13:52 -0500

    Update system/Helpers/test_helper.php
    Co-authored-by: MGatner <mgatner@icloud.com>

--- system/Helpers/test_helper.php
+++ system/Helpers/test_helper.php
@@ -58,7 +58,7 @@ if (! function_exists('mock'))
 	function mock(string $className)
 	{
 		$mockClass   = $className::$mockClass;
-		$mockService = $className::$mockServiceName;
+		$mockService = $className::$mockServiceName ?? '';
 
 		if (empty($mockClass) || ! class_exists($mockClass))
 		{
