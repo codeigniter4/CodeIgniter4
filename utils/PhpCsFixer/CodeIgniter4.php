bd108340e51bc367a43a74c00c6f525ab7fa3056
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2021-06-25 23:06:47 +0800

    Modernize exponentiation

--- utils/PhpCsFixer/CodeIgniter4.php
+++ utils/PhpCsFixer/CodeIgniter4.php
@@ -236,6 +236,7 @@ final class CodeIgniter4 extends AbstractRuleset
             ],
             'phpdoc_var_annotation_correct_order' => true,
             'phpdoc_var_without_name'             => true,
+            'pow_to_exponentiation'               => true,
             'set_type_to_cast'                    => true,
             'short_scalar_cast'                   => true,
             'standardize_increment'               => true,
