ef3bbb9cd8f82adfa48f20e5d4b8afb9064d96e0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2021-07-11 10:40:00 +0700

    undo Foobar.php _support

--- tests/_support/Commands/Foobar.php
+++ tests/_support/Commands/Foobar.php
@@ -1,11 +1,11 @@
 <?php
 
-use CodeIgniter\CLI\CLI;
 use Config\App;
+use CodeIgniter\CLI\CLI;
 
 return [
-    'foo' => 'The command will use this as foo.',
-    'bar' => 'The command will use this as bar.',
-    'baz' => 'The baz is here.',
-    'bas' => CLI::color('bas', 'green') . (new App())->baseURL,
+   'foo' => 'The command will use this as foo.',
+   'bar' => 'The command will use this as bar.',
+   'baz' => 'The baz is here.',
+   'bas' => CLI::color('bas', 'green') . (new App())->baseURL,
 ];
