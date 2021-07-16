4310865db8395f2f9b276afc365fb81a99c1c619
Paweł Nowacki <pawel.nowacki@teleit.pl>
2021-07-16 21:06:52 +0200

    Update HTTPException.php

--- system/HTTP/Exceptions/HTTPException.php
+++ system/HTTP/Exceptions/HTTPException.php
@@ -231,6 +231,18 @@ class HTTPException extends FrameworkException
         return new static(lang('HTTP.moveFailed', [$source, $target, $error]));
     }
 
+
+    /**
+     * For non scalar value passed to setHeader
+     *
+     * @return HTTPException
+     */
+    public static function forNonScalarHeaderValue()
+    {
+        return new static(lang('HTTP.nonScalarHeaderValue'));
+    }
+
+
     /**
      * For Invalid SameSite attribute setting
      *
