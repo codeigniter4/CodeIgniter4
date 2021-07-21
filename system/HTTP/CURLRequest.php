73af375bef6df29e6c42c2fbeae4db25bb89f88a
Paweł Nowacki <pawel.nowacki@teleit.pl>
2021-07-21 13:51:02 +0200

    Update system/HTTP/CURLRequest.php
    it looks good to me.
    
    Co-authored-by: MGatner <mgatner@icloud.com>

--- system/HTTP/CURLRequest.php
+++ system/HTTP/CURLRequest.php
@@ -309,8 +309,8 @@ class CURLRequest extends Request
 
         if (array_key_exists('headers', $options) && is_array($options['headers'])) {
             foreach ($options['headers'] as $name => $value) {
-                if (is_scalar($value)) {
-                    $this->setHeader($name, $value);
+                if (is_scalar($value) || (is_object($value) && method_exists($value, '__toString')) {
+                    $this->setHeader($name, (string) $value);
                 } else {
 					throw HTTPException::forNonScalarHeaderValue();
 				}
