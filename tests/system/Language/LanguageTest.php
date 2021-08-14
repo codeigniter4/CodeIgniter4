68a9a353a34b6e34b190d6c3db547990b30b3973
Paulo Esteves <pjesteves@outlook.pt>
2021-08-15 00:27:10 +0100

    Fix lang() function is overriding locale

--- tests/system/Language/LanguageTest.php
+++ tests/system/Language/LanguageTest.php
@@ -272,6 +272,25 @@ final class LanguageTest extends CIUnitTestCase
     }
 
     /**
+     * Test if after using lang() with a locale the Language class keep the locale after return the $line
+     */
+    public function testLangKeepLocale()
+    {
+        $this->lang = Services::language('en', true);
+
+        lang('Language.languageGetLineInvalidArgumentException');
+        $this->assertEquals('en', $this->lang->getLocale());
+
+        lang('Language.languageGetLineInvalidArgumentException', [], 'ru');
+        $this->assertEquals('en', $this->lang->getLocale());
+
+        lang('Language.languageGetLineInvalidArgumentException');
+        $this->assertEquals('en', $this->lang->getLocale());
+    }
+
+    //--------------------------------------------------------------------
+
+    /**
      * Testing base locale vs variants, with fallback to English.
      *
      * Key	en	ab	ac-CD
