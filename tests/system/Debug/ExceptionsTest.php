0fc1e377da9ad9668210ecc334d031dac9c30ae6
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2021-09-03 20:42:53 +0800

    Fixes and enhancements to Exceptions

--- tests/system/Debug/ExceptionsTest.php
+++ tests/system/Debug/ExceptionsTest.php
@@ -11,27 +11,64 @@
 
 namespace CodeIgniter\Debug;
 
+use CodeIgniter\Exceptions\PageNotFoundException;
 use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\ReflectionHelper;
+use Config\Exceptions as ExceptionsConfig;
 use Config\Services;
+use RuntimeException;
 
 /**
  * @internal
  */
 final class ExceptionsTest extends CIUnitTestCase
 {
-    public function testNew()
+    use ReflectionHelper;
+
+    /**
+     * @var Exceptions
+     */
+    private $exception;
+
+    protected function setUp(): void
     {
-        $actual = new Exceptions(new \Config\Exceptions(), Services::request(), Services::response());
-        $this->assertInstanceOf(Exceptions::class, $actual);
+        $this->exception = new Exceptions(new ExceptionsConfig(), Services::request(), Services::response());
+    }
+
+    public function testDetermineViews(): void
+    {
+        $determineView = $this->getPrivateMethodInvoker($this->exception, 'determineView');
+
+        $this->assertSame('error_404.php', $determineView(PageNotFoundException::forControllerNotFound('Foo', 'bar'), ''));
+        $this->assertSame('error_exception.php', $determineView(new RuntimeException('Exception'), ''));
+        $this->assertSame('error_404.php', $determineView(new RuntimeException('foo', 404), 'app/Views/errors/cli'));
+    }
+
+    public function testCollectVars(): void
+    {
+        $vars = $this->getPrivateMethodInvoker($this->exception, 'collectVars')(new RuntimeException('This.'), 404);
+
+        $this->assertIsArray($vars);
+        $this->assertCount(7, $vars);
+
+        foreach (['title', 'type', 'code', 'message', 'file', 'line', 'trace'] as $key) {
+            $this->assertArrayHasKey($key, $vars);
+        }
+    }
+
+    public function testDetermineCodes(): void
+    {
+        $determineCodes = $this->getPrivateMethodInvoker($this->exception, 'determineCodes');
+
+        $this->assertSame([500, 9], $determineCodes(new RuntimeException('This.')));
+        $this->assertSame([500, 1], $determineCodes(new RuntimeException('That.', 600)));
+        $this->assertSame([404, 1], $determineCodes(new RuntimeException('There.', 404)));
     }
 
     /**
      * @dataProvider dirtyPathsProvider
-     *
-     * @param mixed $file
-     * @param mixed $expected
      */
-    public function testCleanPaths($file, $expected)
+    public function testCleanPaths(string $file, string $expected): void
     {
         $this->assertSame($expected, Exceptions::cleanPath($file));
     }
@@ -40,7 +77,7 @@ final class ExceptionsTest extends CIUnitTestCase
     {
         $ds = DIRECTORY_SEPARATOR;
 
-        return [
+        yield from [
             [
                 APPPATH . 'Config' . $ds . 'App.php',
                 'APPPATH' . $ds . 'Config' . $ds . 'App.php',
