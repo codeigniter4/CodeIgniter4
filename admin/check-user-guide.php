b4ca8619a3250bfea92554044f037ae5d4162f81
kenjis <kenji.uui@gmail.com>
2021-09-20 09:24:27 +0900

    chore: add checking user guide to pre-commit

---
+++ admin/check-user-guide.php
@@ -0,0 +1,34 @@
+<?php
+
+$checker = new UserGuideCheck();
+$checker->run();
+
+class UserGuideCheck
+{
+    public function run(): void
+    {
+        $files = $this->getIndexedFiles();
+        $this->checkTab($files);
+    }
+
+    private function getIndexedFiles(): array
+    {
+        exec('git diff --cached --name-only --diff-filter=ACMR HEAD | grep \\.rst$', $output);
+
+        return $output;
+    }
+
+    private function checkTab(array $files): void
+    {
+        foreach ($files as $file) {
+            $contents = file_get_contents($file);
+
+            if (strpos($contents, "\t") !== false) {
+                echo "{$file}\n";
+                echo "The user guide contains tab(s). Please replace tabs with spaces before commit.\n";
+
+                exit(1);
+            }
+        }
+    }
+}
