f9f4c080cd000010a7f8ab5a7ba191fc931f8870
kenjis <kenji.uui@gmail.com>
2021-09-22 10:31:36 +0900

    chore: add functionality to check exsiting all RST files
    $ php admin/check-user-guide.php all

--- admin/check-user-guide.php
+++ admin/check-user-guide.php
@@ -1,11 +1,30 @@
 <?php
 
+$target  = $argv[1] ?? 'commit';
 $checker = new UserGuideCheck();
-$checker->run();
+$checker->run($target);
 
 class UserGuideCheck
 {
-    public function run(): void
+    public function run(string $target): void
+    {
+        switch ($target) {
+            case 'commit':
+                $this->checkIndexedFiles();
+                break;
+
+            case 'all':
+                $this->checkAllFiles();
+                break;
+
+            default:
+                echo 'Error: "' . $target . '" is invalid.' . "\n";
+
+                exit(1);
+        }
+    }
+
+    private function checkIndexedFiles(): void
     {
         $files = $this->getIndexedFiles();
         $this->checkTab($files);
@@ -38,5 +57,32 @@ class UserGuideCheck
 
             exit(1);
         }
+    }
+
+    private function checkAllFiles(): void
+    {
+        $files = $this->getAllFiles();
+        $this->checkTab($files);
+    }
+
+    private function getAllFiles(): array
+    {
+        $dir = './user_guide_src/source';
+        $ext = 'rst';
+
+        $iter   = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($dir));
+        $filter = static function (SplFileInfo $file) use ($ext) {
+            return ! $file->isDir() && $file->getExtension() === $ext;
+        };
+        $iterator = new \CallbackFilterIterator($iter, $filter);
+
+        $files = array_map(
+            static function (SplFileInfo $file) {
+                return $file->getPathname();
+            },
+            iterator_to_array($iterator, false)
+        );
+
+        return $files;
     }
 }
