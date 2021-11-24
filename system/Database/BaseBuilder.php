82e27ba2eb20892b0133645a44f9d349f5cc5430
kenjis <kenji.uui@gmail.com>
2021-11-24 09:55:47 +0900

    docs: make comment more detailed

--- system/Database/BaseBuilder.php
+++ system/Database/BaseBuilder.php
@@ -264,7 +264,9 @@ class BaseBuilder
     /**
      * Constructor
      *
-     * @param array|string $tableName
+     * @param array|string $tableName tablename or tablenames with or without aliases
+     *
+     * Examples of $tableName: `mytable`, `jobs j`, `jobs j, users u`, `['jobs j','users u']`
      *
      * @throws DatabaseException
      */
