9c9c4d2e15805ecfa0163469021ac49c13ff14f8
kenjis <kenji.uui@gmail.com>
2021-11-26 09:24:11 +0900

    fix: passing null to parameter of type string
    ErrorException: json_decode(): Passing null to parameter #1 ($json) of type string is deprecated

--- system/HTTP/IncomingRequest.php
+++ system/HTTP/IncomingRequest.php
@@ -520,7 +520,7 @@ class IncomingRequest extends Request
      */
     public function getJSON(bool $assoc = false, int $depth = 512, int $options = 0)
     {
-        return json_decode($this->body, $assoc, $depth, $options);
+        return json_decode($this->body ?? '', $assoc, $depth, $options);
     }
 
     /**
