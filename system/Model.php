a0a55ed84447904cb64e176294daa1d46f04c340
kenjis <kenji.uui@gmail.com>
2021-12-06 17:09:05 +0900

    fix: when useAutoIncrement is disabled, countAllResults() is called

--- system/Model.php
+++ system/Model.php
@@ -584,12 +584,12 @@ class Model extends BaseModel
      */
     protected function shouldUpdate($data): bool
     {
-        // When useAutoIncrement feature is disabled check
+        // When useAutoIncrement feature is disabled, check
         // in the database if given record already exists
         return parent::shouldUpdate($data)
-            && $this->useAutoIncrement
+            && ($this->useAutoIncrement
                 ? true
-                : $this->where($this->primaryKey, $this->getIdValue($data))->countAllResults() === 1;
+                : $this->where($this->primaryKey, $this->getIdValue($data))->countAllResults() === 1);
     }
 
     /**
