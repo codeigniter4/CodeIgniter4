9792a6b25f53640bca4d59715c58389df6bcc578
kenjis <kenji.uui@gmail.com>
2021-12-11 13:03:35 +0900

    fix: $fraction type
    NumberFormatter::setAttribute(): Passing null to parameter #2 ($value) of type int|float is deprecated

--- tests/system/Helpers/NumberHelperTest.php
+++ tests/system/Helpers/NumberHelperTest.php
@@ -126,6 +126,7 @@ final class NumberHelperTest extends CIUnitTestCase
      */
     public function testCurrencyCurrentLocale()
     {
+        $this->assertSame('$1,235', number_to_currency(1234.56, 'USD', 'en_US'));
         $this->assertSame('$1,234.56', number_to_currency(1234.56, 'USD', 'en_US', 2));
         $this->assertSame('£1,234.56', number_to_currency(1234.56, 'GBP', 'en_GB', 2));
         $this->assertSame("1.234,56\u{a0}RSD", number_to_currency(1234.56, 'RSD', 'sr_RS', 2));
