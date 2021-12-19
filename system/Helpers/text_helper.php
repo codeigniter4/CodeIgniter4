41da2c6ceb5cb63d4145405bfb4b44d698ed0967
Chibueze Agwu <53615067+ChibuezeAgwuDennis@users.noreply.github.com>
2021-12-19 17:10:01 +0100

    Update text_helper.php

--- system/Helpers/text_helper.php
+++ system/Helpers/text_helper.php
@@ -687,10 +687,6 @@ if (!function_exists('str_contains')) {
 	 */
     function str_contains(string $haystack, string $needle): bool
     {
-        if ($needle !== '' && strpos($haystack, $needle) !== false) {
-            return true;
-        }
-
-        return false;
+        return empty($needle) || strpos($haystack, $needle) !== false;
     }
 }
