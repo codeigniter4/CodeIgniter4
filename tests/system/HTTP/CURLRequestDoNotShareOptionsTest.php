98927283a9284ed8eee839b43beb681ec738f2cb
kenjis <kenji.uui@gmail.com>
2022-01-19 21:19:45 +0900

    test: change `delay` from 1000 to 100
    To improve test speed.

--- tests/system/HTTP/CURLRequestDoNotShareOptionsTest.php
+++ tests/system/HTTP/CURLRequestDoNotShareOptionsTest.php
@@ -267,13 +267,13 @@ final class CURLRequestDoNotShareOptionsTest extends CIUnitTestCase
 
     public function testOptionsDelay()
     {
+        $request = $this->getRequest();
+        $this->assertSame(0.0, $request->getDelay());
+
         $options = [
             'delay'   => 2000,
             'headers' => ['fruit' => 'apple'],
         ];
-        $request = $this->getRequest();
-        $this->assertSame(0.0, $request->getDelay());
-
         $request = $this->getRequest($options);
         $this->assertSame(2.0, $request->getDelay());
     }
@@ -715,20 +715,20 @@ final class CURLRequestDoNotShareOptionsTest extends CIUnitTestCase
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->get('products');
 
         // we still need to check the code coverage to make sure this was done
-        $this->assertSame(1.0, $request->getDelay());
+        $this->assertSame(0.1, $request->getDelay());
     }
 
     public function testSendContinued()
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->setOutput("HTTP/1.1 100 Continue\x0d\x0a\x0d\x0aHi there");
@@ -743,7 +743,7 @@ final class CURLRequestDoNotShareOptionsTest extends CIUnitTestCase
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $output = "HTTP/1.1 100 Continue
@@ -787,7 +787,7 @@ Transfer-Encoding: chunked\x0d\x0a\x0d\x0a<title>Update success! config</title>"
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->setOutput("Accept: text/html\x0d\x0a\x0d\x0aHi there");
@@ -799,7 +799,7 @@ Transfer-Encoding: chunked\x0d\x0a\x0d\x0a<title>Update success! config</title>"
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->setBody('name=George');
@@ -814,7 +814,7 @@ Transfer-Encoding: chunked\x0d\x0a\x0d\x0a<title>Update success! config</title>"
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->setOutput("HTTP/2.0 234 Ohoh\x0d\x0aAccept: text/html\x0d\x0a\x0d\x0aHi there");
@@ -828,7 +828,7 @@ Transfer-Encoding: chunked\x0d\x0a\x0d\x0a<title>Update success! config</title>"
     {
         $request = $this->getRequest([
             'base_uri' => 'http://www.foo.com/api/v1/',
-            'delay'    => 1000,
+            'delay'    => 100,
         ]);
 
         $request->setOutput("HTTP/2 235 Ohoh\x0d\x0aAccept: text/html\x0d\x0a\x0d\x0aHi there shortie");
