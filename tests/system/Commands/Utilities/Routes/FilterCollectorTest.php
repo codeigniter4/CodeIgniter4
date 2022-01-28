c01a623c66da40313d09435f9305a287b6acf942
kenjis <kenji.uui@gmail.com>
2022-01-28 18:40:39 +0900

    feat: add FilterCollector class

---
+++ tests/system/Commands/Utilities/Routes/FilterCollectorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Commands\Utilities\Routes;
+
+use CodeIgniter\Filters\DebugToolbar;
+use CodeIgniter\Test\CIUnitTestCase;
+
+/**
+ * @internal
+ */
+final class FilterCollectorTest extends CIUnitTestCase
+{
+    public function testGet()
+    {
+        $collector = new FilterCollector();
+
+        $filters = $collector->get('get', '/');
+
+        $expected = [
+            'before' => [
+            ],
+            'after' => [
+                DebugToolbar::class,
+            ],
+        ];
+        $this->assertSame($expected, $filters);
+    }
+}
