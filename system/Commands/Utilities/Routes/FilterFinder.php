204091c049d2aed74fd99dd4cc59a9dc1c6fe885
kenjis <kenji.uui@gmail.com>
2022-01-28 16:51:01 +0900

    feat: add FilterFinder class

---
+++ system/Commands/Utilities/Routes/FilterFinder.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Commands\Utilities\Routes;
+
+use CodeIgniter\Filters\Filters;
+use CodeIgniter\Router\Router;
+use Config\Services;
+
+/**
+ * Finds filters.
+ */
+final class FilterFinder
+{
+    private Router $router;
+    private Filters $filters;
+
+    public function __construct(?Router $router = null, ?Filters $filters = null)
+    {
+        $this->router  = $router ?? Services::router();
+        $this->filters = $filters ?? Services::filters();
+    }
+
+    private function getRouteFilters(string $uri): array
+    {
+        $this->router->handle($uri);
+
+        $multipleFiltersEnabled = config('Feature')->multipleFilters ?? false;
+        if (! $multipleFiltersEnabled) {
+            $filter = $this->router->getFilter();
+
+            return $filter === null ? [] : [$filter];
+        }
+
+        return $this->router->getFilters();
+    }
+
+    /**
+     * @param string $uri URI path to find filters for
+     *
+     * @return array{before: array<class-string>, after: array<class-string>} Filter classnames
+     */
+    public function find(string $uri): array
+    {
+        $this->filters->reset();
+
+        // Add route filters
+        $routeFilters = $this->getRouteFilters($uri);
+        $this->filters->enableFilters($routeFilters, 'before');
+        $this->filters->enableFilters($routeFilters, 'after');
+
+        $this->filters->initialize($uri);
+
+        return $this->filters->getFiltersClass();
+    }
+}
