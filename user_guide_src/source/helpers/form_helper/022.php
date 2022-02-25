74df08539fdc515723316bd09609ee2c289c22d6
Alex Schmitz <40514119+sfadschm@users.noreply.github.com>
2022-02-25 12:43:40 +0100

    Convert comment to HTML.

--- user_guide_src/source/helpers/form_helper/022.php
+++ user_guide_src/source/helpers/form_helper/022.php
@@ -9,10 +9,9 @@ echo form_fieldset('Address Information', $attributes);
 echo "<p>fieldset content here</p>\n";
 echo form_fieldset_close();
 
-// Produces:
-
 ?>
 
+<!-- Produces: --> 
 <fieldset id="address_info" class="address_info">
     <legend>Address Information</legend>
     <p>form content here</p>
