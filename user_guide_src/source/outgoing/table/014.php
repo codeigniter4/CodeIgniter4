a001904062883cc60591f04f40f82a388a34e81b
Alex Schmitz <40514119+sfadschm@users.noreply.github.com>
2022-02-25 12:50:51 +0100

    Convert comment to HTML.

--- user_guide_src/source/outgoing/table/014.php
+++ user_guide_src/source/outgoing/table/014.php
@@ -3,7 +3,7 @@
 $cell = ['data' => 'Blue', 'class' => 'highlight', 'colspan' => 2];
 $table->addRow($cell, 'Red', 'Green');
 
-// generates
 ?>
 
+<!-- Generates: -->
 <td class='highlight' colspan='2'>Blue</td><td>Red</td><td>Green</td>
