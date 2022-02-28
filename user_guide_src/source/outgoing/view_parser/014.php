a1206a62e64c79cf42d65c4a066025150842cda2
Alex Schmitz <alexander.schmitz@uni-due.de>
2022-02-28 22:33:01 +0100

    Fix view_parser examples.

--- user_guide_src/source/outgoing/view_parser/014.php
+++ user_guide_src/source/outgoing/view_parser/014.php
@@ -2,11 +2,7 @@
 
 class View extends BaseView
 {
-
     public $plugins = [
         'foo' => '\Some\Class::methodName',
-        'bar' => function($str, array $params = []) {
-            return $str;
-        },
     ];
 }
