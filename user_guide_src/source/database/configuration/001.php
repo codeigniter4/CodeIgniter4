257413dd810f3f437fcfcf779487a0d1a7b475a7
Alex Schmitz <alexander.schmitz@uni-due.de>
2022-02-28 12:12:33 +0100

    Fix database.

--- user_guide_src/source/database/configuration/001.php
+++ user_guide_src/source/database/configuration/001.php
@@ -1,20 +1,23 @@
 <?php
 
-public $default = [
-    'DSN'      => '',
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'database_name',
-    'DBDriver' => 'MySQLi',
-    'DBPrefix' => '',
-    'pConnect' => true,
-    'DBDebug'  => true,
-    'charset'  => 'utf8',
-    'DBCollat' => 'utf8_general_ci',
-    'swapPre'  => '',
-    'encrypt'  => false,
-    'compress' => false,
-    'strictOn' => false,
-    'failover' => [],
-];
+class Database extends Config
+{
+    public $default = [
+        'DSN'      => '',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'database_name',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => true,
+        'DBDebug'  => true,
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'encrypt'  => false,
+        'compress' => false,
+        'strictOn' => false,
+        'failover' => [],
+    ];
+}
--- user_guide_src/source/database/configuration/006.php
+++ user_guide_src/source/database/configuration/006.php
@@ -1,20 +1,23 @@
 <?php
 
-public $test = [
-    'DSN'      => '',
-    'hostname' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'database' => 'database_name',
-    'DBDriver' => 'MySQLi',
-    'DBPrefix' => '',
-    'pConnect' => true,
-    'DBDebug'  => true,
-    'charset'  => 'utf8',
-    'DBCollat' => 'utf8_general_ci',
-    'swapPre'  => '',
-    'compress' => false,
-    'encrypt'  => false,
-    'strictOn' => false,
-    'failover' => []
-);
+class Database extends Config
+{
+    public $test = [
+        'DSN'      => '',
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'database_name',
+        'DBDriver' => 'MySQLi',
+        'DBPrefix' => '',
+        'pConnect' => true,
+        'DBDebug'  => true,
+        'charset'  => 'utf8',
+        'DBCollat' => 'utf8_general_ci',
+        'swapPre'  => '',
+        'compress' => false,
+        'encrypt'  => false,
+        'strictOn' => false,
+        'failover' => [],
+    ];
+}
