f9ef14a2064c1c6981d0fa95b02ccec9e09e545a
kenjis <kenji.uui@gmail.com>
2022-03-04 11:37:07 +0900

    docs: change comment style

--- user_guide_src/source/cli/cli_library/008.php
+++ user_guide_src/source/cli/cli_library/008.php
@@ -6,9 +6,11 @@ $fruit = CLI::promptByKey(['These are your choices:', 'Which would you like?'], 
     'banana' => 'The ripe banana'
 ]);
 
-//These are your choices:
-//  [apple]   The red apple
-//  [orange]  The plump orange
-//  [banana]  The ripe banana
-//
-//Which would you like? [apple, orange, banana]:
+/*
+These are your choices:
+  [apple]   The red apple
+  [orange]  The plump orange
+  [banana]  The ripe banana
+
+Which would you like? [apple, orange, banana]:
+*/
