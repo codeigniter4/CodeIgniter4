170faa1f8f3491ffeb2bbdcfbc268fbafd5af187
kenjis <kenji.uui@gmail.com>
2022-03-06 08:41:56 +0900

    docs: revert method chaining indentation

--- user_guide_src/source/database/query_builder/074.php
+++ user_guide_src/source/database/query_builder/074.php
@@ -2,11 +2,11 @@
 
 $builder->select('*')->from('my_table')
     ->groupStart()
-    ->where('a', 'a')
-    ->orGroupStart()
-    ->where('b', 'b')
-    ->where('c', 'c')
-    ->groupEnd()
+        ->where('a', 'a')
+        ->orGroupStart()
+            ->where('b', 'b')
+            ->where('c', 'c')
+        ->groupEnd()
     ->groupEnd()
     ->where('d', 'd')
     ->get();
