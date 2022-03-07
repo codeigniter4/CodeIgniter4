8ed1d87668eddccb2c4d53053e198e7965bc276f
kenjis <kenji.uui@gmail.com>
2022-03-07 11:38:23 +0900

    fix: Entity can't handle column named attributes
    Add _set*() method as a setter.

--- system/Entity/Entity.php
+++ system/Entity/Entity.php
@@ -439,12 +439,21 @@ class Entity implements JsonSerializable
 
         $value = $this->castAs($value, $key, 'set');
 
-        // if a set* method exists for this key, use that method to
+        // if a setter method exists for this key, use that method to
         // insert this value. should be outside $isNullable check,
         // so maybe wants to do sth with null value automatically
         $method = 'set' . str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $key)));
 
-        if (method_exists($this, $method)) {
+        // If a "`_set` + $key" method exists, it is a setter.
+        if (method_exists($this, '_' . $method)) {
+            $this->{$method}($value);
+
+            return $this;
+        }
+
+        // If a "`set` + $key" method exists, it is also a setter.
+        // But setAttributes() is not a setter for this.
+        if ($key !== 'attributes' && method_exists($this, $method)) {
             $this->{$method}($value);
 
             return $this;
