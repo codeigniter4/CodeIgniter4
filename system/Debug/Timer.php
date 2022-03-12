929cf6158fbb22657fb71b982e3780d6a546c37a
kenjis <kenji.uui@gmail.com>
2022-03-12 09:22:42 +0900

    docs: fix `@return` description

--- system/Debug/Timer.php
+++ system/Debug/Timer.php
@@ -78,7 +78,7 @@ class Timer
      * @param string $name     The name of the timer.
      * @param int    $decimals Number of decimal places.
      *
-     * @return float|null Returns null if timer exists by that name.
+     * @return float|null Returns null if timer does not exist by that name.
      *                    Returns a float representing the number of
      *                    seconds elapsed while that timer was running.
      */
