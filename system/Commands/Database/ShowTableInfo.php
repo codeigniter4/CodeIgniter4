6a980d511d9e90bd47a6b32ed685d83d1d13b570
Pooya Parsa Dadashi <pooya_parsa_dadashi@yahoo.com>
2022-04-13 17:39:21 +0330

    CLI : Add new command ``db:table``
    I do not know if this has been discussed before.
    However, I sent the request.
    If merged.
    This command is simple, but practical.
    This command displays the information in the table.
    I will add options if it merged.

---
+++ system/Commands/Database/ShowTableInfo.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Commands\Database;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+
+/**
+ * Creates a new database.
+ */
+class ShowTableInfo extends BaseCommand
+{
+    /**
+     * The group the command is lumped under
+     * when listing commands.
+     *
+     * @var string
+     */
+    protected $group = 'Database';
+
+    /**
+     * The Command's name
+     *
+     * @var string
+     */
+    protected $name = 'db:table';
+
+    /**
+     * the Command's short description
+     *
+     * @var string
+     */
+    protected $description = 'Retrieves the selected table information.';
+
+    /**
+     * the Command's usage
+     *
+     * @var string
+     */
+    protected $usage = 'db:table <table_name>';
+
+    /**
+     * The Command's arguments
+     *
+     * @var array<string, string>
+     */
+    protected $arguments = [
+        'table_name' => 'The table name for show info',
+    ];
+
+    /**
+     * The Command's options
+     *
+     * @var array<string, string>
+     */
+    protected $options = [];
+
+    /**
+     * get table info 
+     */
+    public function run(array $params)
+    {
+        
+        // Connect to database
+        $db = \Config\Database::connect();
+        $getTables = $db->listTables();
+
+        if($getTables === []){
+        return CLI::error('Database has no tables!', 'light_gray', 'red');
+        }
+
+        $table_name = array_shift($params);
+        // table name correct.
+        if(in_array($table_name, $getTables)){
+            CLI::write("Data of table $table_name : ", 'black', 'yellow');
+            $thead = $db->getFieldNames($table_name);
+            $tbody = $db->table($table_name)->get()->getResultArray();
+            return CLI::table($tbody, $thead);
+        }else{          
+        $table_key = CLI::promptByKey(['These are your tables List :', 'Which table do you want see info?'],$getTables);
+            $thead = $db->getFieldNames($getTables[$table_key]);
+            $tbody = $db->table($getTables[$table_key])->get()->getResultArray();
+            return CLI::table($tbody, $thead);
+        }
+            
+    }
+}
