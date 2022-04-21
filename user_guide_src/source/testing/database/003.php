373e2d7126900060ac6ad32ff8de1e4b6f7e3812
kenjis <kenji.uui@gmail.com>
2022-04-21 09:54:28 +0900

    docs: add properties in sample code

--- user_guide_src/source/testing/database/003.php
+++ user_guide_src/source/testing/database/003.php
@@ -9,7 +9,16 @@ class MyTests extends CIUnitTestCase
 {
     use DatabaseTestTrait;
 
-    protected $refresh  = true;
+    // For Migrations
+    protected $migrate     = true;
+    protected $migrateOnce = false;
+    protected $refresh     = true;
+    protected $namespace   = 'Tests\Support';
+
+    // For Seeds
+    protected $seedOnce = false;
     protected $seed     = 'TestSeeder';
     protected $basePath = 'path/to/database/files';
+
+    // ...
 }
