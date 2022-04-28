8c6d1efc72f124f5c5d6342d887e225c7c5cb1e9
kenjis <kenji.uui@gmail.com>
2022-04-28 14:11:07 +0900

    docs: fix @param

--- system/CLI/BaseCommand.php
+++ system/CLI/BaseCommand.php
@@ -96,7 +96,7 @@ abstract class BaseCommand
     /**
      * Actually execute a command.
      *
-     * @param array<int|string, string> $params
+     * @param array<int|string, string|null> $params
      */
     abstract public function run(array $params);
 
