a21aac8d265db9f57248d8af562950462eb7cc67
kenjis <kenji.uui@gmail.com>
2022-05-06 14:18:42 +0900

    docs: fix sample code
    save() returns bool.

--- user_guide_src/source/libraries/images/006.php
+++ user_guide_src/source/libraries/images/006.php
@@ -1,6 +1,5 @@
 <?php
 
-$image = \Config\Services::image()
-    ->withFile('/path/to/image/mypic.jpg')
+$image->withFile('/path/to/image/mypic.jpg')
     ->withResource()
     ->save('/path/to/image/my_low_quality_pic.jpg', 10);
