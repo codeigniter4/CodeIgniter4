ed2447e64f1a2f2b1c6b73b33df9b7a9ab6edb51
kenjis <kenji.uui@gmail.com>
2022-05-11 08:33:20 +0900

    docs: fix tricky sample code

--- user_guide_src/source/database/utilities/001.php
+++ user_guide_src/source/database/utilities/001.php
@@ -4,7 +4,7 @@ $model                    = new class () extends \CodeIgniter\Model {
     protected $table      = 'foo';
     protected $primaryKey = 'id';
 };
-$db = \Closure::bind(static fn ($model) => $model->db, null, $model)($model);
 
-$util = (new \CodeIgniter\Database\Database())->loadUtils($db);
+$util = \CodeIgniter\Database\Config::utils();
+
 echo $util->getXMLFromResult($model->get());
