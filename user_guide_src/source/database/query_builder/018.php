d21115993a5c3a761b02bb4d69364f6e8f439dab
Andrey Pyzhikov <5071@mail.ru>
2022-05-17 08:59:43 +0800

    docs fix
    Signed-off-by: Andrey Pyzhikov <5071@mail.ru>

--- user_guide_src/source/database/query_builder/018.php
+++ user_guide_src/source/database/query_builder/018.php
@@ -3,4 +3,4 @@
 $subquery = $db->table('users')->select('id, name');
 $builder  = $db->newQuery()->fromSubquery($subquery, 't');
 $query    = $builder->get();
-// Produces: SELECT * FROM (SELECT `id`, `name` FROM users) AS `t`
+// Produces: SELECT * FROM (SELECT `id`, `name` FROM users) `t`
