1e1f9638681e0fb5d9b6202ac7cb4153df036081
kenjis <action@github.com>
2022-06-03 08:10:00 +0000

    Update User Guide

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -5,7 +5,10 @@ var DOCUMENTATION_OPTIONS = {
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
