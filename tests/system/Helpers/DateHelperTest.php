340c0f9b2365e31b6c444f96fe7a92b6cdfaffa4
Denny Septian Panggabean <97607754+ddevsr@users.noreply.github.com>
2022-06-09 17:26:32 +0700

    Adding testing for later in Date Helper

--- tests/system/Helpers/DateHelperTest.php
+++ tests/system/Helpers/DateHelperTest.php
@@ -34,4 +34,15 @@ final class DateHelperTest extends CIUnitTestCase
         // Chicago should be two hours ahead of Vancouver
         $this->assertCloseEnough(7200, now('America/Chicago') - now('America/Vancouver'));
     }
+    
+    public function testLaterDefault()
+    {
+        $date = date('j-n-Y G:i:s', strtotime('2022-06-09 15:00:00' . '+1 days'));
+
+        sscanf($date, '%d-%d-%d %d:%d:%d', $day, $month, $year, $hour, $minute, $second);
+
+        $laterDate = mktime($hour, $minute, $second, $month, $day, $year);
+
+        $this->assertCloseEnough(later('2022-06-09 15:00:00', 1, 'D', 'Asia/Jakarta'), $laterDate);
+    }
 }
