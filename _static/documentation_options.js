f69b9bb8952cd1c393f20001b056a39d5ce0124e
MGatner <action@github.com>
2022-06-16 13:52:47 +0000

    Update User Guide

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.2.0',
+    VERSION: '4.2.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
