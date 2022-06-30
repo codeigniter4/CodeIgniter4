dd73e491f4176004fd091f4ed61daa332ea3a353
ThibautPV <75485473+ThibautPV@users.noreply.github.com>
2022-06-30 18:17:59 +0200

    Remove unused CSS

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -5,16 +5,6 @@
     <title>404 Page Not Found</title>
 
     <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
-        }
         body {
             height: 100%;
             background: #fafafa;
@@ -40,31 +30,8 @@
             border-radius: 0.5rem;
             position: relative;
         }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
-        }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
-        }
         p {
             margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
         }
     </style>
 </head>
