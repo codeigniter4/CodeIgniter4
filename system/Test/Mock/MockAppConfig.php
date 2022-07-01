a2310baa133a2a7624f8e726a9c09cdcaecb98ea
kenjis <kenji.uui@gmail.com>
2022-07-01 13:49:24 +0900

    refactor: add type for properties

--- system/Test/Mock/MockAppConfig.php
+++ system/Test/Mock/MockAppConfig.php
@@ -15,27 +15,27 @@ use Config\App;
 
 class MockAppConfig extends App
 {
-    public $baseURL          = 'http://example.com/';
-    public $uriProtocol      = 'REQUEST_URI';
-    public $cookiePrefix     = '';
-    public $cookieDomain     = '';
-    public $cookiePath       = '/';
-    public $cookieSecure     = false;
-    public $cookieHTTPOnly   = false;
-    public $cookieSameSite   = 'Lax';
-    public $proxyIPs         = '';
-    public $CSRFTokenName    = 'csrf_test_name';
-    public $CSRFHeaderName   = 'X-CSRF-TOKEN';
-    public $CSRFCookieName   = 'csrf_cookie_name';
-    public $CSRFExpire       = 7200;
-    public $CSRFRegenerate   = true;
-    public $CSRFExcludeURIs  = ['http://example.com'];
-    public $CSRFRedirect     = false;
-    public $CSRFSameSite     = 'Lax';
-    public $CSPEnabled       = false;
-    public $defaultLocale    = 'en';
-    public $negotiateLocale  = false;
-    public $supportedLocales = [
+    public string $baseURL         = 'http://example.com/';
+    public string $uriProtocol     = 'REQUEST_URI';
+    public string $cookiePrefix    = '';
+    public string $cookieDomain    = '';
+    public string $cookiePath      = '/';
+    public bool $cookieSecure      = false;
+    public bool $cookieHTTPOnly    = false;
+    public ?string $cookieSameSite = 'Lax';
+    public $proxyIPs               = '';
+    public string $CSRFTokenName   = 'csrf_test_name';
+    public string $CSRFHeaderName  = 'X-CSRF-TOKEN';
+    public string $CSRFCookieName  = 'csrf_cookie_name';
+    public int $CSRFExpire         = 7200;
+    public bool $CSRFRegenerate    = true;
+    public array $CSRFExcludeURIs  = ['http://example.com'];
+    public bool $CSRFRedirect      = false;
+    public string $CSRFSameSite    = 'Lax';
+    public bool $CSPEnabled        = false;
+    public string $defaultLocale   = 'en';
+    public bool $negotiateLocale   = false;
+    public array $supportedLocales = [
         'en',
         'es',
     ];
