ff0ac551ff7ecf73db7ee58b66cab0f61781a843
kenjis <kenji.uui@gmail.com>
2022-07-02 18:20:24 +0900

    fix: remove mistakenly reverted lines

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,6 @@ namespace Config;
 // Create a new instance of our RouteCollection class.
 $routes = Services::routes();
 
-// Load the system's routing file first, so that the app and ENVIRONMENT
-// can override as needed.
-if (is_file(SYSTEMPATH . 'Config/Routes.php')) {
-    require SYSTEMPATH . 'Config/Routes.php';
-}
-
 /*
  * --------------------------------------------------------------------
  * Router Setup
