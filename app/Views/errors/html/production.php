6ce810f6334934eaa9bcd23aca8cfa1ae47329b9
Stefan Bauer <stefan@databauer.ch>
2022-07-04 10:06:14 +0200

    Codestyle

--- app/Views/errors/html/production.php
+++ app/Views/errors/html/production.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="robots" content="noindex">
 
-    <title><?= lang('Errors.whoops')?></title>
+    <title><?= lang('Errors.whoops') ?></title>
 
     <style type="text/css">
         <?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
@@ -14,9 +14,9 @@
 
     <div class="container text-center">
 
-        <h1 class="headline"><?= lang('Errors.whoops')?></h1>
+        <h1 class="headline"><?= lang('Errors.whoops') ?></h1>
 
-        <p class="lead"><?= lang('Errors.weHitASnag')?></p>
+        <p class="lead"><?= lang('Errors.weHitASnag') ?></p>
 
     </div>
 
