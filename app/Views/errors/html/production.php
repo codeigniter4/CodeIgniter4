ea17779800d177cad3862456cd86a593eecf0f84
Stefan Bauer <stefan@databauer.ch>
2022-07-06 12:18:08 +0200

    Update production.php
    remove obsolete type="text/css" attribute

--- app/Views/errors/html/production.php
+++ app/Views/errors/html/production.php
@@ -6,7 +6,7 @@
 
     <title><?= lang('Errors.whoops') ?></title>
 
-    <style type="text/css">
+    <style>
         <?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
     </style>
 </head>
