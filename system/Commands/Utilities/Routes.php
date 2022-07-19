938a57608ea6454355c67d5b9764d8c1c96c2ff1
kenjis <kenji.uui@gmail.com>
2022-07-19 18:05:09 +0900

    fix: spark routes show 404 error when using regex

--- system/Commands/Utilities/Routes.php
+++ system/Commands/Utilities/Routes.php
@@ -18,6 +18,7 @@ use CodeIgniter\Commands\Utilities\Routes\AutoRouteCollector;
 use CodeIgniter\Commands\Utilities\Routes\AutoRouterImproved\AutoRouteCollector as AutoRouteCollectorImproved;
 use CodeIgniter\Commands\Utilities\Routes\FilterCollector;
 use CodeIgniter\Commands\Utilities\Routes\SampleURIGenerator;
+use CodeIgniter\Exceptions\PageNotFoundException;
 use Config\Services;
 
 /**
@@ -99,7 +100,15 @@ class Routes extends BaseCommand
             foreach ($routes as $route => $handler) {
                 if (is_string($handler) || $handler instanceof Closure) {
                     $sampleUri = $uriGenerator->get($route);
-                    $filters   = $filterCollector->get($method, $sampleUri);
+
+                    try {
+                        $filters = $filterCollector->get($method, $sampleUri);
+                    } catch (PageNotFoundException $e) {
+                        $filters = [
+                            'before' => ['<unknown>'],
+                            'after'  => ['<unknown>'],
+                        ];
+                    }
 
                     $tbody[] = [
                         strtoupper($method),
