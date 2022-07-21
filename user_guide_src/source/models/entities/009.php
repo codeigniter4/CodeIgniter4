abe670ef583515a0f812f7e65468ab8f9aed382f
kenjis <kenji.uui@gmail.com>
2022-07-21 15:14:33 +0900

    docs: improve code comments

--- user_guide_src/source/models/entities/009.php
+++ user_guide_src/source/models/entities/009.php
@@ -8,7 +8,7 @@ class User extends Entity
 {
     protected $attributes = [
         'id'         => null,
-        'full_name'  => null, // In the $attributes, the key is the column name
+        'full_name'  => null, // In the $attributes, the key is the db column name
         'email'      => null,
         'password'   => null,
         'created_at' => null,
@@ -16,6 +16,7 @@ class User extends Entity
     ];
 
     protected $datamap = [
+        // property_name => db_column_name
         'name' => 'full_name',
     ];
 }
