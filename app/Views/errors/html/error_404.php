d6eb12fb820aeeed5d275c2830f2e8cb220f915e
Stefan Bauer <stefan@databauer.ch>
2022-07-23 15:30:42 +0200

    make error_404.php design same as production.php
    use styles and css classes fromm debug.css as it is in production.php

--- app/Views/errors/html/error_404.php
+++ app/Views/errors/html/error_404.php
@@ -5,74 +5,15 @@
     <title><?= lang('Errors.404pageNotFound') ?></title>
 
     <style>
-        div.logo {
-            height: 200px;
-            width: 155px;
-            display: inline-block;
-            opacity: 0.08;
-            position: absolute;
-            top: 2rem;
-            left: 50%;
-            margin-left: -73px;
-        }
-        body {
-            height: 100%;
-            background: #fafafa;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-            color: #777;
-            font-weight: 300;
-        }
-        h1 {
-            font-weight: lighter;
-            letter-spacing: normal;
-            font-size: 3rem;
-            margin-top: 0;
-            margin-bottom: 0;
-            color: #222;
-        }
-        .wrap {
-            max-width: 1024px;
-            margin: 5rem auto;
-            padding: 2rem;
-            background: #fff;
-            text-align: center;
-            border: 1px solid #efefef;
-            border-radius: 0.5rem;
-            position: relative;
-        }
-        pre {
-            white-space: normal;
-            margin-top: 1.5rem;
-        }
-        code {
-            background: #fafafa;
-            border: 1px solid #efefef;
-            padding: 0.5rem 1rem;
-            border-radius: 5px;
-            display: block;
-        }
-        p {
-            margin-top: 1.5rem;
-        }
-        .footer {
-            margin-top: 2rem;
-            border-top: 1px solid #efefef;
-            padding: 1em 2em 0 2em;
-            font-size: 85%;
-            color: #999;
-        }
-        a:active,
-        a:link,
-        a:visited {
-            color: #dd4814;
-        }
+        <?= preg_replace('#[\r\n\t ]+#', ' ', file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'debug.css')) ?>
     </style>
 </head>
 <body>
-    <div class="wrap">
-        <h1><?= lang('Errors.404pageNotFound') ?></h1>
+    <div class="container text-center">
 
-        <p>
+        <h1 class="headline"><?= lang('Errors.404pageNotFound') ?></h1>
+
+        <p class="lead">
             <?php if (! empty($message) && $message !== '(null)') : ?>
                 <?= nl2br(esc($message)) ?>
             <?php else : ?>
