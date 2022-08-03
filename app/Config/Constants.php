1cca975d90737d8317a2aeff4ab8e8e14aec9493
alfstylecat <66421004+alfstylecat@users.noreply.github.com>
2022-08-03 10:53:51 +0300

    PHP Parse error:  syntax error, unexpected '_592_000' get error please check and fix

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -38,9 +38,9 @@ defined('MINUTE') || define('MINUTE', 60);
 defined('HOUR')   || define('HOUR', 3600);
 defined('DAY')    || define('DAY', 86400);
 defined('WEEK')   || define('WEEK', 604800);
-defined('MONTH')  || define('MONTH', 2_592_000);
-defined('YEAR')   || define('YEAR', 31_536_000);
-defined('DECADE') || define('DECADE', 315_360_000);
+defined('MONTH')  || define('MONTH', 2592000);
+defined('YEAR')   || define('YEAR', 31536000);
+defined('DECADE') || define('DECADE', 315360000);
 
 /*
  | --------------------------------------------------------------------------
