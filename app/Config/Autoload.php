84da1d5623167b229ac8fb71aecbf0c4552bb5c3
kenjis <kenji.uui@gmail.com>
2022-08-21 10:07:41 +0900

    docs: remove '```' in doc comments

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -31,12 +31,10 @@ class Autoload extends AutoloadConfig
      * else you will need to modify all of those classes for this to work.
      *
      * Prototype:
-     * ```
      *   $psr4 = [
      *       'CodeIgniter' => SYSTEMPATH,
-     *       'App'	       => APPPATH
+     *       'App'         => APPPATH
      *   ];
-     * ```
      *
      * @var array<string, string>
      */
@@ -56,11 +54,9 @@ class Autoload extends AutoloadConfig
      * were being autoloaded through a namespace.
      *
      * Prototype:
-     * ```
      *   $classmap = [
      *       'MyClass'   => '/path/to/class/file.php'
      *   ];
-     * ```
      *
      * @var array<string, string>
      */
@@ -75,11 +71,9 @@ class Autoload extends AutoloadConfig
      * or for loading functions.
      *
      * Prototype:
-     * ```
-     *  $files = [
-     *      '/path/to/my/file.php',
-     *  ];
-     * ```
+     *   $files = [
+     *       '/path/to/my/file.php',
+     *   ];
      *
      * @var string[]
      * @phpstan-var list<string>
@@ -91,11 +85,9 @@ class Autoload extends AutoloadConfig
      * Helpers
      * -------------------------------------------------------------------
      * Prototype:
-     * ```
      *   $helpers = [
      *       'form',
      *   ];
-     * ```
      *
      * @var string[]
      * @phpstan-var list<string>
