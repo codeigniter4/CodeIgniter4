9b68863a65e1cdfff00b02f5a8e4685317c1df11
Denny Septian Panggabean <xamidimura@gmail.com>
2022-09-17 21:24:44 +0700

    docs: replace doctype Parser and Filter

--- system/View/Filters.php
+++ system/View/Filters.php
@@ -30,7 +30,7 @@ class Filters
     /**
      * Formats a date into the given $format.
      *
-     * @param mixed $value
+     * @param int|string|null $value
      */
     public static function date($value, string $format): string
     {
@@ -48,7 +48,7 @@ class Filters
      * Example:
      *      my_date|date_modify(+1 day)
      *
-     * @param string $value
+     * @param int|string|null $value
      *
      * @return false|int
      */
@@ -62,7 +62,7 @@ class Filters
     /**
      * Returns the given default value if $value is empty or undefined.
      *
-     * @param mixed $value
+     * @param int|string|null $value
      */
     public static function default($value, string $default): string
     {
@@ -208,12 +208,13 @@ class Filters
      *  - ceil      always rounds up
      *  - floor     always rounds down
      *
-     * @param mixed $precision
+     * @param float|int|string $precision
      *
      * @return float|string
      */
     public static function round(string $value, $precision = 2, string $type = 'common')
     {
+        $testGatau = 1337.0;
         if (! is_numeric($precision)) {
             $type      = $precision;
             $precision = 2;
