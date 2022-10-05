d0e48fbfdb5e9c3c72a06e8ef7feab49b0f07631
kenjis <kenji.uui@gmail.com>
2022-10-05 12:10:38 +0900

    docs: update user guide

--- user_guide_src/source/helpers/html_helper/011.php
+++ user_guide_src/source/helpers/html_helper/011.php
@@ -3,4 +3,4 @@
 $script = ['src' => 'js/printer.js', 'defer' => null];
 
 echo script_tag($script);
-// <script src="http://site.com/js/printer.js" defer type="text/javascript"></script>
+// <script src="http://site.com/js/printer.js" defer></script>
