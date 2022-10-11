0ef3f7e0ac86a55b048b41ff76765cbe00bf6598
kenjis <kenji.uui@gmail.com>
2022-10-11 11:50:01 +0900

    docs: make IncomingRequest::$uri deprecated

--- system/HTTP/IncomingRequest.php
+++ system/HTTP/IncomingRequest.php
@@ -64,7 +64,7 @@ class IncomingRequest extends Request
      * AFTER the script name. So, if hosted in a sub-folder this will
      * appear different than actual URL. If you need that use getPath().
      *
-     * @TODO should be protected. Use getUri() instead.
+     * @deprecated Will be protected. Use getUri() instead.
      *
      * @var URI
      */
