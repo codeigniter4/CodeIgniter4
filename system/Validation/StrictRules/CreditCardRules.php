542099d33a42b0753e63d9e585217a4f90f872db
ping-yee <611077101@mail.nknu.edu.tw>
2022-10-12 16:01:19 +0800

    docs: changed mixed to array|bool|float|int|object|string|null in CreditCardRules.php

--- system/Validation/StrictRules/CreditCardRules.php
+++ system/Validation/StrictRules/CreditCardRules.php
@@ -41,7 +41,7 @@ class CreditCardRules
      *      'cc_num' => 'valid_cc_number[visa]'
      *  ];
      *
-     * @param mixed $ccNumber
+     * @param array|bool|float|int|object|string|null $ccNumber
      */
     public function valid_cc_number($ccNumber, string $type): bool
     {
