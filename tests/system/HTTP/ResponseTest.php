1624e7377ea23ac1f87384377e7e653a572cc6cd
Dyno <dyno@aldabase.com>
2022-10-14 12:17:45 +0200

    Bug: ResponseTest::testSetLastModifiedWithDateTimeObject depends on time
    Fix #5428

--- tests/system/HTTP/ResponseTest.php
+++ tests/system/HTTP/ResponseTest.php
@@ -243,9 +243,10 @@ final class ResponseTest extends CIUnitTestCase
     {
         $response = new Response(new App());
 
-        $response->setLastModified(DateTime::createFromFormat('Y-m-d', '2000-03-10'));
+        $datetime = DateTime::createFromFormat('Y-m-d', '2000-03-10');
+        $response->setLastModified($datetime);
 
-        $date = DateTime::createFromFormat('Y-m-d', '2000-03-10');
+        $date = clone $datetime;
         $date->setTimezone(new DateTimeZone('UTC'));
 
         $header = $response->getHeaderLine('Last-Modified');
