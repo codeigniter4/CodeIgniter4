d0a0b10b10d33734321e00861ccb5b5d67bed3bb
sclubricants <vaughnb@scoil.com>
2022-10-14 20:49:29 -0700

    Change example to use fromQuery()

--- user_guide_src/source/database/query_builder/117.php
+++ user_guide_src/source/database/query_builder/117.php
@@ -5,7 +5,7 @@ $query = "SELECT user2.name, user2.email, user2.country
           LEFT JOIN user ON user.email = user2.email
           WHERE user.email IS NULL";
 
-$sql = $builder->ignore(true)->insertBatch($query);
+$sql = $builder->ignore(true)->fromQuery($query)->insertBatch();
 /* MySQLi produces:
     INSERT IGNORE INTO `db_user` (`name`, `country`, `email`)
     SELECT user2.name, user2.email, user2.country
