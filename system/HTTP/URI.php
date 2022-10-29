9d1ac4a7c86f379619db460c8fcab4676081c28a
kenjis <kenji.uui@gmail.com>
2022-10-29 12:43:03 +0900

    docs: add note

--- system/HTTP/URI.php
+++ system/HTTP/URI.php
@@ -83,6 +83,11 @@ class URI
     /**
      * URI path.
      *
+     * Note: The constructor of the IncomingRequest class changes the path of
+     *      the URI object held by the IncomingRequest class to a path relative
+     *      to the SCRIPT_NAME. If the baseURL contains subfolders, this value
+     *      will be different from the current URI path.
+     *
      * @var string
      */
     protected $path;
