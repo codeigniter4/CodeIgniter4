cde6d08a7a41c870f0c5cd30df8f29a62c9e0564
ping-yee <611077101@mail.nknu.edu.tw>
2022-11-13 23:40:26 +0800

    change the place of use statement.

--- system/Helpers/date_helper.php
+++ system/Helpers/date_helper.php
@@ -1,16 +1,4 @@
 <?php
-
-/**
- * This file is part of CodeIgniter 4 framework.
- *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
-use CodeIgniter\I18n\Time;
-
 /**
  * This file is part of CodeIgniter 4 framework.
  *
@@ -21,6 +9,8 @@ use CodeIgniter\I18n\Time;
  */
 
 // CodeIgniter Date Helpers
+
+use CodeIgniter\I18n\Time;
 
 if (! function_exists('now')) {
     /**
