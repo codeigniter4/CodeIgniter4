da068cf182307e0ff5b5be80784da081d9b1092f
kenjis <kenji.uui@gmail.com>
2022-12-01 10:08:52 +0900

    docs: add "// ..." in sample code

--- user_guide_src/source/libraries/sessions/041.php
+++ user_guide_src/source/libraries/sessions/041.php
@@ -6,7 +6,9 @@ use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    public $sessionDiver    = 'CodeIgniter\Session\Handlers\RedisHandler';
+    // ...
+    public $sessionDiver = 'CodeIgniter\Session\Handlers\RedisHandler';
+    // ...
     public $sessionSavePath = 'tcp://localhost:6379';
     // ...
 }
