699c88c61b6720d726b0de430080f8954ce6a049
kenjis <kenji.uui@gmail.com>
2022-12-09 09:47:04 +0900

    doc: add missing properties

--- user_guide_src/source/models/model/005.php
+++ user_guide_src/source/models/model/005.php
@@ -16,12 +16,16 @@ class UserModel extends Model
 
     protected $allowedFields = ['name', 'email'];
 
+    // Dates
     protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
 
+    // Validation
     protected $validationRules    = [];
     protected $validationMessages = [];
     protected $skipValidation     = false;
+    protected $cleanValidationRules = true;
 }
