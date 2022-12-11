9f71f23f4fe572d6637deac327b6e05d3deaac3e
kenjis <kenji.uui@gmail.com>
2022-12-11 11:16:30 +0900

    docs: fix and update the sample codes

--- user_guide_src/source/concepts/services/006.php
+++ user_guide_src/source/concepts/services/006.php
@@ -4,7 +4,7 @@ namespace App\Router;
 
 use CodeIgniter\Router\RouteCollectionInterface;
 
-class MyRouter implements RouteCollectionInterface
+class MyRouteCollection implements RouteCollectionInterface
 {
     // Implement required methods here.
 }
