c6708289c2b9c9e0e35b325508e6a147d7a07ae2
kenjis <kenji.uui@gmail.com>
2022-12-16 17:04:37 +0900

    docs: refactor sample code
    Array is easier to read than long sting.

--- user_guide_src/source/libraries/uploaded_files/002.php
+++ user_guide_src/source/libraries/uploaded_files/002.php
@@ -18,11 +18,13 @@ class Upload extends BaseController
         $validationRule = [
             'userfile' => [
                 'label' => 'Image File',
-                'rules' => 'uploaded[userfile]'
-                    . '|is_image[userfile]'
-                    . '|mime_in[userfile,image/jpg,image/jpeg,image/gif,image/png,image/webp]'
-                    . '|max_size[userfile,100]'
-                    . '|max_dims[userfile,1024,768]',
+                'rules' => [
+                    'uploaded[userfile]',
+                    'is_image[userfile]',
+                    'mime_in[userfile,image/jpg,image/jpeg,image/gif,image/png,image/webp]',
+                    'max_size[userfile,100]',
+                    'max_dims[userfile,1024,768]',
+                ],
             ],
         ];
         if (! $this->validate($validationRule)) {
@@ -40,6 +42,7 @@ class Upload extends BaseController
 
             return view('upload_success', $data);
         }
+
         $data = ['errors' => 'The file has already been moved.'];
 
         return view('upload_form', $data);
