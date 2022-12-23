ac2f64f787259a4f50e3a9d4bdff28d14b450bd1
ping-yee <611077101@mail.nknu.edu.tw>
2022-12-23 15:45:33 +0800

    docs: fix typo and docs of uploaded_files.

--- user_guide_src/source/libraries/uploaded_files/002.php
+++ user_guide_src/source/libraries/uploaded_files/002.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace App\Controllers;
 
 use CodeIgniter\Files\File;
@@ -18,11 +27,13 @@ class Upload extends BaseController
         $validationRule = [
             'userfile' => [
                 'label' => 'Image File',
-                'rules' => 'uploaded[userfile]'
-                    . '|is_image[userfile]'
-                    . '|mime_in[userfile,image/jpg,image/jpeg,image/gif,image/png,image/webp]'
-                    . '|max_size[userfile,100]'
-                    . '|max_dims[userfile,1024,768]',
+                'rules' => [
+                    'uploaded[userfile]',
+                    'is_image[userfile]',
+                    'mime_in[userfile,image/jpg,image/jpeg,image/gif,image/png,image/webp]',
+                    'max_size[userfile,100]',
+                    'max_dims[userfile,1024,768]',
+                ],
             ],
         ];
         if (! $this->validate($validationRule)) {
@@ -36,10 +47,11 @@ class Upload extends BaseController
         if (! $img->hasMoved()) {
             $filepath = WRITEPATH . 'uploads/' . $img->store();
 
-            $data = ['uploaded_flleinfo' => new File($filepath)];
+            $data = ['uploaded_fileinfo' => new File($filepath)];
 
             return view('upload_success', $data);
         }
+
         $data = ['errors' => 'The file has already been moved.'];
 
         return view('upload_form', $data);
