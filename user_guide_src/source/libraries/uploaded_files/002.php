067289252a9d2c8ea4fd24c21cd3adc9db58a8e8
ping-yee <611077101@mail.nknu.edu.tw>
2022-12-23 16:50:19 +0800

    Revert the html tag and remove the comment out.

--- user_guide_src/source/libraries/uploaded_files/002.php
+++ user_guide_src/source/libraries/uploaded_files/002.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of CodeIgniter 4 framework.
- *
- * (c) CodeIgniter Foundation <admin@codeigniter.com>
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
-
 namespace App\Controllers;
 
 use CodeIgniter\Files\File;
