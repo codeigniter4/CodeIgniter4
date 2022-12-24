cb7ccc60b604de29aefcd34272888f33f0ce44b9
kenjis <kenji.uui@gmail.com>
2022-12-24 09:44:48 +0900

    docs: align comments in sample code

--- user_guide_src/source/incoming/request/003.php
+++ user_guide_src/source/incoming/request/003.php
@@ -1,5 +1,5 @@
 <?php
 
-echo $request->getMethod(true); // Outputs: POST
+echo $request->getMethod(true);  // Outputs: POST
 echo $request->getMethod(false); // Outputs: post
-echo $request->getMethod(); // Outputs: post
+echo $request->getMethod();      // Outputs: post
