a2c47fa5cf5b3e6f2297356fe210f022a4ab16e6
Dominic Rego <dominic@nerdyreef.com>
2022-12-24 09:56:05 -0500

    Update app/Controllers/BaseController.php
    Co-authored-by: Abdul Malik Ikhsan <samsonasik@gmail.com>

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -38,7 +38,7 @@ abstract class BaseController extends Controller
     protected $helpers = [];
 
     /**
-     * Be sure to declare properties for anything you assign.
+     * Be sure to declare properties for any property fetch you initialized.
      * The creation of dynamic property is deprecated in PHP 8.2.
      */
     // protected $session;
