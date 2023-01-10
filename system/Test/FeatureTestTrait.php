ce60e23d0b941d55df4fc083e841655f133b1457
baycik <baycikplus@gmail.com>
2023-01-10 16:59:31 +0300

    Update FeatureTestTrait.php

--- system/Test/FeatureTestTrait.php
+++ system/Test/FeatureTestTrait.php
@@ -286,7 +286,7 @@ trait FeatureTestTrait
     {
         $path    = URI::removeDotSegments($path);
         $config  = config(App::class);
-        $request = Services::request($config, true);
+        $request = Services::request($config, false);
 
         // $path may have a query in it
         $parts                   = explode('?', $path);
@@ -365,7 +365,6 @@ trait FeatureTestTrait
             return $request;
         }
 
-        $request->removeHeader('Content-Type');
         if (isset($this->bodyFormat) && $this->bodyFormat !== '') {
             if (empty($params)) {
                 $params = $request->fetchGlobal('request');
