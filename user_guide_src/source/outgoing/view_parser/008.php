e8e5fac33ee8d5cdfb3aa74bc258cac7f592a524
kenjis <kenji.uui@gmail.com>
2023-01-13 09:30:33 +0900

    docs: fix incorrect sample code

--- user_guide_src/source/outgoing/view_parser/008.php
+++ user_guide_src/source/outgoing/view_parser/008.php
@@ -4,8 +4,8 @@ $data = [
     'blog_title'   => 'My Blog Title',
     'blog_heading' => 'My Blog Heading',
     'blog_entry'   => [
-        'title' => 'Title 1',
-        'body'  => 'Body 1',
+        ['title' => 'Title 1'],
+        ['body' => 'Body 1'],
     ],
 ];
 
