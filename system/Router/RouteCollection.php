7c125b90cf2b2614ae24d1c93b2686c398dcb9fc
kenjis <kenji.uui@gmail.com>
2023-01-25 14:59:58 +0900

    perf: change RouteCollection::$routes structure

--- system/Router/RouteCollection.php
+++ system/Router/RouteCollection.php
@@ -116,17 +116,44 @@ class RouteCollection implements RouteCollectionInterface
      *
      * [
      *     verb => [
-     *         routeName => [
-     *             'route' => [
-     *                 routeKey(regex) => handler,
-     *                 or routeKey(regex)(from) => [routeKey(regex)(to) => handler], // redirect
-     *             ],
+     *         routeKey(regex) => [
+     *             'name'     => routeName
+     *             'handler'  => handler,
+     *         ],
+     *         // redirect route
+     *         or routeKey(regex)(from) => [
+     *             'name'     => routeName
+     *             'handler'  => [routeKey(regex)(to) => handler],
      *             'redirect' => statusCode,
-     *         ]
+     *         ],
      *     ],
      * ]
      */
     protected $routes = [
+        '*'       => [],
+        'options' => [],
+        'get'     => [],
+        'head'    => [],
+        'post'    => [],
+        'put'     => [],
+        'delete'  => [],
+        'trace'   => [],
+        'connect' => [],
+        'cli'     => [],
+    ];
+
+    /**
+     * Array of routes names
+     *
+     * @var array
+     *
+     * [
+     *     verb => [
+     *         routeName => routeKey(regex)
+     *     ],
+     * ]
+     */
+    protected $routesNames = [
         '*'       => [],
         'options' => [],
         'get'     => [],
@@ -538,9 +565,8 @@ class RouteCollection implements RouteCollectionInterface
             // before any of the generic, "add" routes.
             $collection = $this->routes[$verb] + ($this->routes['*'] ?? []);
 
-            foreach ($collection as $r) {
-                $key          = key($r['route']);
-                $routes[$key] = $r['route'][$key];
+            foreach ($collection as $routeKey => $r) {
+                $routes[$routeKey] = $r['handler'];
             }
         }
 
@@ -639,11 +665,16 @@ class RouteCollection implements RouteCollectionInterface
     public function addRedirect(string $from, string $to, int $status = 302)
     {
         // Use the named route's pattern if this is a named route.
-        if (array_key_exists($to, $this->routes['*'])) {
-            $redirectTo = $this->routes['*'][$to]['route'];
-        } elseif (array_key_exists($to, $this->routes['get'])) {
-            $redirectTo = $this->routes['get'][$to]['route'];
+        if (array_key_exists($to, $this->routesNames['*'])) {
+            $routeName  = $to;
+            $routeKey   = $this->routesNames['*'][$routeName];
+            $redirectTo = [$routeKey => $this->routes['*'][$routeKey]['handler']];
+        } elseif (array_key_exists($to, $this->routesNames['get'])) {
+            $routeName  = $to;
+            $routeKey   = $this->routesNames['get'][$routeName];
+            $redirectTo = [$routeKey => $this->routes['get'][$routeKey]['handler']];
         } else {
+            // The named route is not found.
             $redirectTo = $to;
         }
 
@@ -659,11 +690,16 @@ class RouteCollection implements RouteCollectionInterface
      */
     public function isRedirect(string $routeKey): bool
     {
-        foreach ($this->routes['*'] as $name => $route) {
-            // Named route?
-            if ($name === $routeKey || key($route['route']) === $routeKey) {
-                return isset($route['redirect']) && is_numeric($route['redirect']);
-            }
+        if (isset($this->routes['*'][$routeKey]['redirect'])) {
+            return true;
+        }
+
+        // This logic is not used. Should be deprecated?
+        $routeName = $this->routes['*'][$routeKey]['name'] ?? null;
+        if ($routeName === $routeKey) {
+            $routeKey = $this->routesNames['*'][$routeName];
+
+            return isset($this->routes['*'][$routeKey]['redirect']);
         }
 
         return false;
@@ -676,11 +712,16 @@ class RouteCollection implements RouteCollectionInterface
      */
     public function getRedirectCode(string $routeKey): int
     {
-        foreach ($this->routes['*'] as $name => $route) {
-            // Named route?
-            if ($name === $routeKey || key($route['route']) === $routeKey) {
-                return $route['redirect'] ?? 0;
-            }
+        if (isset($this->routes['*'][$routeKey]['redirect'])) {
+            return $this->routes['*'][$routeKey]['redirect'];
+        }
+
+        // This logic is not used. Should be deprecated?
+        $routeName = $this->routes['*'][$routeKey]['name'] ?? null;
+        if ($routeName === $routeKey) {
+            $routeKey = $this->routesNames['*'][$routeName];
+
+            return $this->routes['*'][$routeKey]['redirect'];
         }
 
         return 0;
@@ -1095,9 +1136,11 @@ class RouteCollection implements RouteCollectionInterface
     public function reverseRoute(string $search, ...$params)
     {
         // Named routes get higher priority.
-        foreach ($this->routes as $collection) {
+        foreach ($this->routesNames as $collection) {
             if (array_key_exists($search, $collection)) {
-                return $this->buildReverseRoute(key($collection[$search]['route']), $params);
+                $routeKey = $collection[$search];
+
+                return $this->buildReverseRoute($routeKey, $params);
             }
         }
 
@@ -1113,9 +1156,8 @@ class RouteCollection implements RouteCollectionInterface
         // If it's not a named route, then loop over
         // all routes to find a match.
         foreach ($this->routes as $collection) {
-            foreach ($collection as $route) {
-                $routeKey = key($route['route']);
-                $to       = $route['route'][$routeKey];
+            foreach ($collection as $routeKey => $route) {
+                $to = $route['handler'];
 
                 // ignore closures
                 if (! is_string($to)) {
@@ -1340,7 +1382,7 @@ class RouteCollection implements RouteCollectionInterface
         }
 
         // When redirecting to named route, $to is an array like `['zombies' => '\Zombies::index']`.
-        if (is_array($to) && count($to) === 2) {
+        if (is_array($to) && isset($to[0])) {
             $to = $this->processArrayCallableSyntax($from, $to);
         }
 
@@ -1420,20 +1462,21 @@ class RouteCollection implements RouteCollectionInterface
         // routes should always be the "source of truth".
         // this works only because discovered routes are added just prior
         // to attempting to route the request.
-        $fromExists = dot_array_search('*.route.' . $routeKey, $this->routes[$verb] ?? []) !== null;
-        if ((isset($this->routes[$verb][$name]) || $fromExists) && ! $overwrite) {
+        $fromExists = isset($this->routes[$verb][$routeKey]);
+        if ((isset($this->routesNames[$verb][$name]) || $fromExists) && ! $overwrite) {
             return;
         }
 
-        $this->routes[$verb][$name] = [
-            'route' => [$routeKey => $to],
+        $this->routes[$verb][$routeKey] = [
+            'name'    => $name,
+            'handler' => $to,
         ];
-
         $this->routesOptions[$verb][$routeKey] = $options;
+        $this->routesNames[$verb][$name]       = $routeKey;
 
         // Is this a redirect?
         if (isset($options['redirect']) && is_numeric($options['redirect'])) {
-            $this->routes['*'][$name]['redirect'] = $options['redirect'];
+            $this->routes['*'][$routeKey]['redirect'] = $options['redirect'];
         }
     }
 
@@ -1574,11 +1617,14 @@ class RouteCollection implements RouteCollectionInterface
      */
     public function resetRoutes()
     {
-        $this->routes = ['*' => []];
+        $this->routes = $this->routesNames = ['*' => []];
 
         foreach ($this->defaultHTTPMethods as $verb) {
-            $this->routes[$verb] = [];
+            $this->routes[$verb]      = [];
+            $this->routesNames[$verb] = [];
         }
+
+        $this->routesOptions = [];
 
         $this->prioritizeDetected = false;
         $this->didDiscover        = false;
@@ -1645,9 +1691,8 @@ class RouteCollection implements RouteCollectionInterface
 
         if ($verb === '*') {
             foreach ($this->defaultHTTPMethods as $tmpVerb) {
-                foreach ($this->routes[$tmpVerb] as $route) {
-                    $routeKey   = key($route['route']);
-                    $controller = $this->getControllerName($route['route'][$routeKey]);
+                foreach ($this->routes[$tmpVerb] as $routeKey => $route) {
+                    $controller = $this->getControllerName($route['handler']);
                     if ($controller !== null) {
                         $controllers[] = $controller;
                     }
