921dbbe37ee5125668001a1eca6146eae575269f
kenjis <kenji.uui@gmail.com>
2023-01-26 13:08:36 +0900

    docs: add other samle code

--- user_guide_src/source/models/model/001.php
+++ user_guide_src/source/models/model/001.php
@@ -3,11 +3,15 @@
 // Create a new class manually.
 $userModel = new \App\Models\UserModel();
 
-// Create a new class with the model() function.
-$userModel = model('App\Models\UserModel', false);
-
 // Create a shared instance of the model.
+$userModel = model('UserModel');
+// or
 $userModel = model('App\Models\UserModel');
+// or
+$userModel = model(App\Models\UserModel::class);
+
+// Create a new class with the model() function.
+$userModel = model('UserModel', false);
 
 // Create shared instance with a supplied database connection.
 // When no namespace is given, it will search through all namespaces
