636b38cbf644bfc295d578672e8b1f9df101fd31
kenjis <kenji.uui@gmail.com>
2023-01-29 21:20:47 +0900

    test: remove unneeded code

--- tests/system/HTTP/IncomingRequestDetectingTest.php
+++ tests/system/HTTP/IncomingRequestDetectingTest.php
@@ -31,127 +31,139 @@ final class IncomingRequestDetectingTest extends CIUnitTestCase
 
         $_POST = $_GET = $_SERVER = $_REQUEST = $_ENV = $_COOKIE = $_SESSION = [];
 
-        $origin = 'http://www.example.com/index.php/woot?code=good#pos';
-
+        // The URI object is not used in detectPath().
+        $origin        = 'http://www.example.com/index.php/woot?code=good#pos';
         $this->request = new IncomingRequest(new App(), new URI($origin), null, new UserAgent());
     }
 
     public function testPathDefault()
     {
-        $this->request->uri     = '/index.php/woot?code=good#pos';
+        // /index.php/woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/index.php/woot';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath());
     }
 
     public function testPathEmpty()
     {
-        $this->request->uri     = '/';
+        // /
         $_SERVER['REQUEST_URI'] = '/';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = '/';
+
+        $expected = '/';
         $this->assertSame($expected, $this->request->detectPath());
     }
 
     public function testPathRequestURI()
     {
-        $this->request->uri     = '/index.php/woot?code=good#pos';
+        // /index.php/woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/index.php/woot';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURINested()
     {
-        $this->request->uri     = '/ci/index.php/woot?code=good#pos';
+        // /ci/index.php/woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/index.php/woot';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURISubfolder()
     {
-        $this->request->uri     = '/ci/index.php/popcorn/woot?code=good#pos';
+        // /ci/index.php/popcorn/woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/ci/index.php/popcorn/woot';
         $_SERVER['SCRIPT_NAME'] = '/ci/index.php';
-        $expected               = 'popcorn/woot';
+
+        $expected = 'popcorn/woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURINoIndex()
     {
-        $this->request->uri     = '/sub/example';
+        // /sub/example
         $_SERVER['REQUEST_URI'] = '/sub/example';
         $_SERVER['SCRIPT_NAME'] = '/sub/index.php';
-        $expected               = 'example';
+
+        $expected = 'example';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURINginx()
     {
-        $this->request->uri     = '/ci/index.php/woot?code=good#pos';
+        // /ci/index.php/woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/index.php/woot?code=good';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURINginxRedirecting()
     {
-        $this->request->uri     = '/?/ci/index.php/woot';
+        // /?/ci/index.php/woot
         $_SERVER['REQUEST_URI'] = '/?/ci/woot';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'ci/woot';
+
+        $expected = 'ci/woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathRequestURISuppressed()
     {
-        $this->request->uri     = '/woot?code=good#pos';
+        // /woot?code=good#pos
         $_SERVER['REQUEST_URI'] = '/woot';
         $_SERVER['SCRIPT_NAME'] = '/';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath('REQUEST_URI'));
     }
 
     public function testPathQueryString()
     {
-        $this->request->uri      = '/?/ci/index.php/woot';
+        // /?/ci/index.php/woot
         $_SERVER['REQUEST_URI']  = '/?/ci/woot';
         $_SERVER['QUERY_STRING'] = '/ci/woot';
         $_SERVER['SCRIPT_NAME']  = '/index.php';
-        $expected                = 'ci/woot';
+
+        $expected = 'ci/woot';
         $this->assertSame($expected, $this->request->detectPath('QUERY_STRING'));
     }
 
     public function testPathQueryStringEmpty()
     {
-        $this->request->uri      = '/?/ci/index.php/woot';
+        // /?/ci/index.php/woot
         $_SERVER['REQUEST_URI']  = '/?/ci/woot';
         $_SERVER['QUERY_STRING'] = '';
         $_SERVER['SCRIPT_NAME']  = '/index.php';
-        $expected                = '';
+
+        $expected = '';
         $this->assertSame($expected, $this->request->detectPath('QUERY_STRING'));
     }
 
     public function testPathPathInfo()
     {
-        $this->request->uri = '/index.php/woot?code=good#pos';
+        // /index.php/woot?code=good#pos
         $this->request->setGlobal('server', [
             'PATH_INFO' => null,
         ]);
         $_SERVER['REQUEST_URI'] = '/index.php/woot';
         $_SERVER['SCRIPT_NAME'] = '/index.php';
-        $expected               = 'woot';
+
+        $expected = 'woot';
         $this->assertSame($expected, $this->request->detectPath('PATH_INFO'));
     }
 
     public function testPathPathInfoGlobal()
     {
-        $this->request->uri = '/index.php/woot?code=good#pos';
+        // /index.php/woot?code=good#pos
         $this->request->setGlobal('server', [
             'PATH_INFO' => 'silliness',
         ]);
