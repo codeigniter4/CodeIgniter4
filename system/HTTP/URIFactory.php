84193901daf4016d869502bc88f39192ade40dd6
kenjis <kenji.uui@gmail.com>
2023-02-10 11:46:33 +0900

    feat: make URIFactory work

--- system/HTTP/URIFactory.php
+++ system/HTTP/URIFactory.php
@@ -51,61 +51,68 @@ class URIFactory
     }
 
     /**
-     * Detects the relative path based on
-     * the URIProtocol Config setting.
+     * Detects the current URI path relative to baseURL based on the URIProtocol
+     * Config setting.
+     *
+     * @param string $protocol URIProtocol
+     *
+     * @return string The route path
      */
-    public function detectPath(string $protocol = ''): string
+    public function detectRoutePath(string $protocol = ''): string
     {
-        if (empty($protocol)) {
-            $protocol = 'REQUEST_URI';
+        if ($protocol === '') {
+            $protocol = $this->appConfig->uriProtocol;
         }
 
         switch ($protocol) {
             case 'REQUEST_URI':
-                $this->path = $this->parseRequestURI();
+                $routePath = $this->parseRequestURI();
                 break;
 
             case 'QUERY_STRING':
-                $this->path = $this->parseQueryString();
+                $routePath = $this->parseQueryString();
                 break;
 
             case 'PATH_INFO':
             default:
-                $this->path = $this->fetchGlobal('server', $protocol) ?? $this->parseRequestURI();
+                $routePath = $this->server[$protocol] ?? $this->parseRequestURI();
                 break;
         }
 
-        return $this->path;
+        return $routePath;
     }
 
     /**
      * Will parse the REQUEST_URI and automatically detect the URI from it,
      * fixing the query string if necessary.
      *
-     * @return string The URI it found.
+     * This method updates superglobal $_SERVER and $_GET.
+     *
+     * @return string The route path.
      */
-    protected function parseRequestURI(): string
+    private function parseRequestURI(): string
     {
-        if (! isset($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME'])) {
+        if (! isset($this->server['REQUEST_URI'], $this->server['SCRIPT_NAME'])) {
             return '';
         }
 
-        // parse_url() returns false if no host is present, but the path or query string
-        // contains a colon followed by a number. So we attach a dummy host since
-        // REQUEST_URI does not include the host. This allows us to parse out the query string and path.
-        $parts = parse_url('http://dummy' . $_SERVER['REQUEST_URI']);
+        // parse_url() returns false if no host is present, but the path or query
+        // string contains a colon followed by a number. So we attach a dummy
+        // host since REQUEST_URI does not include the host. This allows us to
+        // parse out the query string and path.
+        $parts = parse_url('http://dummy' . $this->server['REQUEST_URI']);
         $query = $parts['query'] ?? '';
         $uri   = $parts['path'] ?? '';
 
         // Strip the SCRIPT_NAME path from the URI
         if (
-            $uri !== '' && isset($_SERVER['SCRIPT_NAME'][0])
-            && pathinfo($_SERVER['SCRIPT_NAME'], PATHINFO_EXTENSION) === 'php'
+            $uri !== '' && isset($this->server['SCRIPT_NAME'][0])
+            && pathinfo($this->server['SCRIPT_NAME'], PATHINFO_EXTENSION) === 'php'
         ) {
             // Compare each segment, dropping them until there is no match
             $segments = $keep = explode('/', $uri);
 
-            foreach (explode('/', $_SERVER['SCRIPT_NAME']) as $i => $segment) {
+            foreach (explode('/', $this->server['SCRIPT_NAME']) as $i => $segment) {
                 // If these segments are not the same then we're done
                 if (! isset($segments[$i]) || $segment !== $segments[$i]) {
                     break;
@@ -117,20 +124,19 @@ class URIFactory
             $uri = implode('/', $keep);
         }
 
-        // This section ensures that even on servers that require the URI to contain the query string (Nginx) a correct
-        // URI is found, and also fixes the QUERY_STRING Server var and $_GET array.
+        // This section ensures that even on servers that require the URI to
+        // contain the query string (Nginx) a correct URI is found, and also
+        // fixes the QUERY_STRING Server var and $_GET array.
         if (trim($uri, '/') === '' && strncmp($query, '/', 1) === 0) {
-            $query                   = explode('?', $query, 2);
-            $uri                     = $query[0];
-            $_SERVER['QUERY_STRING'] = $query[1] ?? '';
+            $query                        = explode('?', $query, 2);
+            $uri                          = $query[0];
+            $this->server['QUERY_STRING'] = $query[1] ?? '';
         } else {
-            $_SERVER['QUERY_STRING'] = $query;
+            $this->server['QUERY_STRING'] = $query;
         }
 
-        // Update our globals for values likely to been have changed
-        parse_str($_SERVER['QUERY_STRING'], $_GET);
-        $this->populateGlobals('server');
-        $this->populateGlobals('get');
+        // Update our globals for values likely to have been changed
+        parse_str($this->server['QUERY_STRING'], $this->get);
 
         $uri = URI::removeDotSegments($uri);
 
@@ -138,28 +144,28 @@ class URIFactory
     }
 
     /**
-     * Parse QUERY_STRING
-     *
      * Will parse QUERY_STRING and automatically detect the URI from it.
+     *
+     * This method updates superglobal $_SERVER and $_GET.
+     *
+     * @return string The route path.
      */
-    protected function parseQueryString(): string
+    private function parseQueryString(): string
     {
-        $uri = $_SERVER['QUERY_STRING'] ?? @getenv('QUERY_STRING');
+        $uri = $this->server['QUERY_STRING'] ?? @getenv('QUERY_STRING');
 
         if (trim($uri, '/') === '') {
             return '/';
         }
 
         if (strncmp($uri, '/', 1) === 0) {
-            $uri                     = explode('?', $uri, 2);
-            $_SERVER['QUERY_STRING'] = $uri[1] ?? '';
-            $uri                     = $uri[0];
+            $uri                          = explode('?', $uri, 2);
+            $this->server['QUERY_STRING'] = $uri[1] ?? '';
+            $uri                          = $uri[0];
         }
 
-        // Update our globals for values likely to been have changed
-        parse_str($_SERVER['QUERY_STRING'], $_GET);
-        $this->populateGlobals('server');
-        $this->populateGlobals('get');
+        // Update our globals for values likely to have been changed
+        parse_str($this->server['QUERY_STRING'], $this->get);
 
         $uri = URI::removeDotSegments($uri);
 
@@ -167,53 +173,49 @@ class URIFactory
     }
 
     /**
-     * Sets the relative path and updates the URI object.
+     * Create current URI object.
      *
-     * Note: Since current_url() accesses the shared request
-     * instance, this can be used to change the "current URL"
-     * for testing.
-     *
-     * @param string   $path   URI path relative to baseURL
-     * @param App|null $config Optional alternate config to use
-     *
-     * @return $this
+     * @param string $routePath URI path relative to baseURL
      */
-    public function setPath(string $path, ?App $config = null)
+    private function createURIFromRoutePath(string $routePath): URI
     {
-        $this->path = $path;
-
-        // @TODO remove this. The path of the URI object should be a full URI path,
-        //      not a URI path relative to baseURL.
-        $this->uri->setPath($path);
-
-        $config ??= $this->config;
+        $config = $this->appConfig;
 
         // It's possible the user forgot a trailing slash on their
         // baseURL, so let's help them out.
-        $baseURL = ($config->baseURL === '') ? $config->baseURL : rtrim($config->baseURL, '/ ') . '/';
+        $baseURL = ($config->baseURL === '')
+            ? $config->baseURL
+            : rtrim($config->baseURL, '/ ') . '/';
 
         // Based on our baseURL and allowedHostnames provided by the developer
         // and HTTP_HOST, set our current domain name, scheme.
         if ($baseURL !== '') {
-            $host = $this->determineHost($config, $baseURL);
+            $host = $this->determineHost($baseURL);
 
             // Set URI::$baseURL
             $uri            = new URI($baseURL);
             $currentBaseURL = (string) $uri->setHost($host);
-            $this->uri->setBaseURL($currentBaseURL);
+            $uri->setBaseURL($currentBaseURL);
 
-            $this->uri->setScheme(parse_url($baseURL, PHP_URL_SCHEME));
-            $this->uri->setHost($host);
-            $this->uri->setPort(parse_url($baseURL, PHP_URL_PORT));
+            $uri->setPath($routePath);
+
+            $uri->setRoutePath($routePath);
+
+            $uri->setScheme(parse_url($baseURL, PHP_URL_SCHEME));
+            $uri->setHost($host);
+            $uri->setPort(parse_url($baseURL, PHP_URL_PORT));
 
             // Ensure we have any query vars
-            $this->uri->setQuery($_SERVER['QUERY_STRING'] ?? '');
+            $uri->setQuery($this->server['QUERY_STRING'] ?? '');
 
             // Check if the scheme needs to be coerced into its secure version
-            if ($config->forceGlobalSecureRequests && $this->uri->getScheme() === 'http') {
-                $this->uri->setScheme('https');
+            if ($config->forceGlobalSecureRequests && $uri->getScheme() === 'http') {
+                $uri->setScheme('https');
             }
-        } elseif (! is_cli()) {
+
+            return $uri;
+        }
+        if (! is_cli()) {
             // Do not change exit() to exception; Request is initialized before
             // setting the exception handler, so if an exception is raised, an
             // error will be displayed even if in the production environment.
@@ -222,23 +224,26 @@ class URIFactory
             // @codeCoverageIgnoreEnd
         }
 
-        return $this;
+        return new URI();
     }
 
-    private function determineHost(App $config, string $baseURL): string
+    /**
+     * @return string The current hostname.
+     */
+    private function determineHost(string $baseURL): string
     {
         $host = parse_url($baseURL, PHP_URL_HOST);
 
-        if (empty($config->allowedHostnames)) {
+        if (empty($this->appConfig->allowedHostnames)) {
             return $host;
         }
 
         // Update host if it is valid.
-        $httpHostPort = $this->getServer('HTTP_HOST');
+        $httpHostPort = $this->server['HTTP_HOST'] ?? null;
         if ($httpHostPort !== null) {
             [$httpHost] = explode(':', $httpHostPort, 2);
 
-            if (in_array($httpHost, $config->allowedHostnames, true)) {
+            if (in_array($httpHost, $this->appConfig->allowedHostnames, true)) {
                 $host = $httpHost;
             }
         }
