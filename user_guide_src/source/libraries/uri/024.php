4cebcd76a59c888a1129860095c9d6bb197424b1
kenjis <kenji.uui@gmail.com>
2023-02-14 11:58:17 +0900

    docs: update URI page

--- user_guide_src/source/libraries/uri/024.php
+++ user_guide_src/source/libraries/uri/024.php
@@ -2,13 +2,17 @@
 
 // URI = http://example.com/users/15/profile
 
+echo $uri->getSegment(1, 'foo');
+// will print 'users'
+
+echo $uri->getSegment(3, 'bar');
 // will print 'profile'
-echo $uri->getSegment(3, 'foo');
-// will print 'bar'
-echo $uri->getSegment(4, 'bar');
+
+echo $uri->getSegment(4, 'baz');
 // will throw an exception
-echo $uri->getSegment(5, 'baz');
+
+echo $uri->setSilent()->getSegment(4, 'baz');
 // will print 'baz'
-echo $uri->setSilent()->getSegment(5, 'baz');
+
+echo $uri->setSilent()->getSegment(4);
 // will print '' (empty string)
-echo $uri->setSilent()->getSegment(5);
