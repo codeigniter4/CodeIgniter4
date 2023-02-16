d44812d91d9fbec9c9d742ad759fa08cd405bd57
蔡銘凱 <z85385637@gmail.com>
2023-02-16 23:50:34 +0800

    Enhance: [MySQLi] use MYSQLI_OPT_INT_AND_FLOAT_NATIVE.

--- system/Database/MySQLi/Connection.php
+++ system/Database/MySQLi/Connection.php
@@ -98,6 +98,7 @@ class Connection extends BaseConnection
         mysqli_report(MYSQLI_REPORT_ALL & ~MYSQLI_REPORT_INDEX);
 
         $this->mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 10);
+        $this->mysqli->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
 
         if (isset($this->strictOn)) {
             if ($this->strictOn) {
