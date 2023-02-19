bb5fe28d56b08e836fa5c2077d910628da556e03
objecttothis <17935339+objecttothis@users.noreply.github.com>
2023-02-19 16:39:41 +0400

    Match capitalization required by php 8.2 unit-test

--- user_guide_src/source/libraries/encryption/013.php
+++ user_guide_src/source/libraries/encryption/013.php
@@ -5,7 +5,7 @@ use Config\Services;
 
 $config         = new Encryption();
 $config->driver = 'OpenSSL';
-$config->cipher = 'aes-128-cbc';
+$config->cipher = 'AES-128-CBC';
 // Your CI3's encryption_key
 $config->key            = hex2bin('64c70b0b8d45b80b9eba60b8b3c8a34d0193223d20fea46f8644b848bf7ce67f');
 $config->rawData        = false;
