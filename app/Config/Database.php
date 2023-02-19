0a8c9711597c7bc88de429e72b30a5285ecbc4e5
蔡銘凱 <z85385637@gmail.com>
2023-02-19 14:41:18 +0800

    fix:Modify changelogs/v4.4.0.rst description and remove the numberNative attribute from the $test array in app/Config/Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -50,26 +50,25 @@ class Database extends Config
      * running PHPUnit database tests.
      */
     public array $tests = [
-        'DSN'          => '',
-        'hostname'     => '127.0.0.1',
-        'username'     => '',
-        'password'     => '',
-        'database'     => ':memory:',
-        'DBDriver'     => 'SQLite3',
-        'DBPrefix'     => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8',
-        'DBCollat'     => 'utf8_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306,
-        'foreignKeys'  => true,
-        'busyTimeout'  => 1000,
-        'numberNative' => false,
+        'DSN'         => '',
+        'hostname'    => '127.0.0.1',
+        'username'    => '',
+        'password'    => '',
+        'database'    => ':memory:',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => 'db_',  // Needed to ensure we're working correctly with prefixes live. DO NOT REMOVE FOR CI DEVS
+        'pConnect'    => false,
+        'DBDebug'     => true,
+        'charset'     => 'utf8',
+        'DBCollat'    => 'utf8_general_ci',
+        'swapPre'     => '',
+        'encrypt'     => false,
+        'compress'    => false,
+        'strictOn'    => false,
+        'failover'    => [],
+        'port'        => 3306,
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
     ];
 
     public function __construct()
