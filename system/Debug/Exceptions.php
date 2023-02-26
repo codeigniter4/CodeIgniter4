4cc9259fbea9e61502da0f0194738aa7248773c1
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-02-27 00:32:01 +0800

    Fix handling of null bytes in `Exceptions::renderBacktrace()`

--- system/Debug/Exceptions.php
+++ system/Debug/Exceptions.php
@@ -531,9 +531,6 @@ class Exceptions
                     case is_resource($value):
                         return sprintf('resource (%s)', get_resource_type($value));
 
-                    case is_string($value):
-                        return var_export(clean_path($value), true);
-
                     default:
                         return var_export($value, true);
                 }
