a6f146c96ad2159b1ae277ecab0b8c05a6a54b04
Abdul Malik Ikhsan <samsonasik@gmail.com>
2023-03-01 21:50:37 +0700

    allow int at @param filter

--- system/HTTP/RequestInterface.php
+++ system/HTTP/RequestInterface.php
@@ -40,8 +40,8 @@ interface RequestInterface extends OutgoingRequestInterface
      * Fetch an item from the $_SERVER array.
      * Supplied by RequestTrait.
      *
-     * @param string   $index  Index for item to be fetched from $_SERVER
-     * @param int|null $filter A filter name to be applied
+     * @param string          $index  Index for item to be fetched from $_SERVER
+     * @param int|string|null $filter A filter name to be applied
      *
      * @return mixed
      */
