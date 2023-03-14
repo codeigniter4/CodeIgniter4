1afa9dadac7071cc58c8ae8412d77d9ae0dc8494
totoprayogo1916 <totoprayogo1916@gmail.com>
2023-03-14 12:00:52 +0700

    doc: change logo (svg)
    Signed-off-by: totoprayogo1916 <totoprayogo1916@gmail.com>

--- user_guide_src/source/conf.py
+++ user_guide_src/source/conf.py
@@ -89,7 +89,7 @@ html_last_updated_fmt = '%b %d, %Y'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/ci-logo-text.png'
+html_logo = '_static/ci-logo-text.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs. This file should be a Windows icon file (.ico) being 16x16 or 32x32
