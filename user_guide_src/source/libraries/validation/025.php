042697630767cfb4611a11e01f3926ddda59a9ca
kenjis <kenji.uui@gmail.com>
2023-04-12 14:12:20 +0900

    docs: change coding style

--- user_guide_src/source/libraries/validation/025.php
+++ user_guide_src/source/libraries/validation/025.php
@@ -1,20 +1,18 @@
 <?php
 
-$validation->setRules(
-    [
-        'username' => [
-            'label'  => 'Rules.username',
-            'rules'  => 'required|is_unique[users.username]',
-            'errors' => [
-                'required' => 'Rules.username.required',
-            ],
+$validation->setRules([
+    'username' => [
+        'label'  => 'Rules.username',
+        'rules'  => 'required|is_unique[users.username]',
+        'errors' => [
+            'required' => 'Rules.username.required',
         ],
-        'password' => [
-            'label'  => 'Rules.password',
-            'rules'  => 'required|min_length[10]',
-            'errors' => [
-                'min_length' => 'Rules.password.min_length',
-            ],
+    ],
+    'password' => [
+        'label'  => 'Rules.password',
+        'rules'  => 'required|min_length[10]',
+        'errors' => [
+            'min_length' => 'Rules.password.min_length',
         ],
-    ]
-);
+    ],
+]);
