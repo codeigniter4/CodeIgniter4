1c28935ba022c34c1be670aa099aaba07128b29a
Marco <marco.guerrini@outlook.com>
2023-04-28 18:12:39 +0200

    Controller name error in Controllers and Routing => URI Routing => For Site Root URI
    Signed-off-by: Marco <marco.guerrini@outlook.com>

--- user_guide_src/source/incoming/routing/047.php
+++ user_guide_src/source/incoming/routing/047.php
@@ -1,4 +1,4 @@
 <?php
 
-// example.com routes to app/Controllers/Welcome.php
-$routes->setDefaultController('Welcome');
+// example.com routes to app/Controllers/Home.php
+$routes->setDefaultController('Home');
