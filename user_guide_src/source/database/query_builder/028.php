da67d3de6153a0d8740175b7cc943d9b1bf08184
Andrey Pyzhikov <5071@mail.ru>
2023-05-28 01:40:31 +0800

    fix: class without import. scrolling in examples.

--- user_guide_src/source/database/query_builder/028.php
+++ user_guide_src/source/database/query_builder/028.php
@@ -1,7 +1,11 @@
 <?php
 
 // With closure
-$builder->where('advance_amount <', static fn (BaseBuilder $builder) => $builder->select('MAX(advance_amount)', false)->from('orders')->where('id >', 2));
+use CodeIgniter\Database\BaseBuilder;
+
+$builder->where('advance_amount <', static function (BaseBuilder $builder) {
+    $builder->select('MAX(advance_amount)', false)->from('orders')->where('id >', 2);
+});
 // Produces: WHERE "advance_amount" < (SELECT MAX(advance_amount) FROM "orders" WHERE "id" > 2)
 
 // With builder directly
--- user_guide_src/source/database/query_builder/037.php
+++ user_guide_src/source/database/query_builder/037.php
@@ -1,7 +1,11 @@
 <?php
 
 // With closure
-$builder->orWhereNotIn('id', static fn (BaseBuilder $builder) => $builder->select('job_id')->from('users_jobs')->where('user_id', 3));
+use CodeIgniter\Database\BaseBuilder;
+
+$builder->orWhereNotIn('id', static function (BaseBuilder $builder) {
+    $builder->select('job_id')->from('users_jobs')->where('user_id', 3);
+});
 // Produces: OR "id" NOT IN (SELECT "job_id" FROM "users_jobs" WHERE "user_id" = 3)
 
 // With builder directly
