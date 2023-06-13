9185aa87d78d040c8f71212dd1af7b2c94798b7b
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-06-13 17:37:53 +0800

    Fix asserted Model class

--- tests/_support/PHPStan/Type/model.php
+++ tests/_support/PHPStan/Type/model.php
@@ -9,8 +9,6 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-use Tests\Support\Models\EntityModel;
-
 use function PHPStan\Testing\assertType;
 
 $class = (static fn (): string => mt_rand(0, 10) > 5 ? stdClass::class : 'Foo')();
@@ -18,7 +16,9 @@ $class = (static fn (): string => mt_rand(0, 10) > 5 ? stdClass::class : 'Foo')(
 assertType('null', model('foo'));
 assertType('stdClass', model(stdClass::class));
 assertType('Closure', model(Closure::class));
-assertType('Tests\Support\Models\EntityModel', model(EntityModel::class));
-assertType('Tests\Support\Models\EntityModel', model('EntityModel'));
 assertType('null', model('App'));
 assertType('object|null', model($class));
+
+// don't use test models used in other tests, as MemoizingReflectionProvider
+// messes autoload causing unknown class errors
+assertType('Tests\Support\Models\BarModel', model('BarModel'));
