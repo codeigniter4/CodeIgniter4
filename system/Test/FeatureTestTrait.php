903fee87f6e5a0784700f460c13bce6557362b2d
kenjis <kenji.uui@gmail.com>
2023-06-21 17:14:15 +0900

    refactor: add ()

--- system/Test/FeatureTestTrait.php
+++ system/Test/FeatureTestTrait.php
@@ -339,7 +339,7 @@ trait FeatureTestTrait
     {
         // $params should set the query vars if present,
         // otherwise set it from the URL.
-        $get = ! empty($params) && $method === 'get'
+        $get = (! empty($params) && $method === 'get')
             ? $params
             : $this->getPrivateProperty($request->getUri(), 'query');
 
