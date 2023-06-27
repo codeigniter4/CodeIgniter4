7a7e98089a63f42dfb227d3b17e66cecc2077763
kenjis <kenji.uui@gmail.com>
2023-06-27 15:38:16 +0900

    refactor: remove CSRF properties in MockAppConfig

--- system/Test/Mock/MockAppConfig.php
+++ system/Test/Mock/MockAppConfig.php
@@ -18,14 +18,6 @@ class MockAppConfig extends App
     public string $baseURL         = 'http://example.com/';
     public string $uriProtocol     = 'REQUEST_URI';
     public array $proxyIPs         = [];
-    public string $CSRFTokenName   = 'csrf_test_name';
-    public string $CSRFHeaderName  = 'X-CSRF-TOKEN';
-    public string $CSRFCookieName  = 'csrf_cookie_name';
-    public int $CSRFExpire         = 7200;
-    public bool $CSRFRegenerate    = true;
-    public array $CSRFExcludeURIs  = ['http://example.com'];
-    public bool $CSRFRedirect      = false;
-    public string $CSRFSameSite    = 'Lax';
     public bool $CSPEnabled        = false;
     public string $defaultLocale   = 'en';
     public bool $negotiateLocale   = false;
