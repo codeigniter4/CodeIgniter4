d88d751976704c35b2d1ee5a6ab48665931c0e6a
ping-yee <611077101@mail.nknu.edu.tw>
2023-07-03 13:39:11 +0800

    fix: changed by rector.

--- system/Helpers/number_helper.php
+++ system/Helpers/number_helper.php
@@ -77,7 +77,7 @@ if (! function_exists('number_to_amount')) {
     {
         // Strip any formatting & ensure numeric input
         try {
-            $num = 0 + (int) str_replace(',', '', $num);
+            $num = (int) str_replace(',', '', $num);
         } catch (ErrorException $ee) {
             return false;
         }
