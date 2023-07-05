7131946ee491916e4342ac5ca7b1ba8f5c7c53f7
kenjis <kenji.uui@gmail.com>
2023-07-05 10:49:14 +0900

    docs: improve sample code
    If a dev copy and paste the constructor, it won't work
    becaseu `parent::__construct();` is missing.

--- user_guide_src/source/database/configuration/008.php
+++ user_guide_src/source/database/configuration/008.php
@@ -4,14 +4,20 @@ namespace Config;
 
 use CodeIgniter\Database\Config;
 
+/**
+ * Database Configuration
+ */
 class Database extends Config
 {
+    // ...
     public $development = [/* ... */];
     public $test        = [/* ... */];
     public $production  = [/* ... */];
 
     public function __construct()
     {
+        // ...
+
         $this->defaultGroup = ENVIRONMENT;
     }
 }
