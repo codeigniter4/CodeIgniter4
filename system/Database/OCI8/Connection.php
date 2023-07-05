ab19d07cfab235bb77bbf7b4079392397b81618e
kenjis <kenji.uui@gmail.com>
2023-07-05 16:19:19 +0900

    docs: fix typo in OCI8\Connection

--- system/Database/OCI8/Connection.php
+++ system/Database/OCI8/Connection.php
@@ -98,7 +98,7 @@ class Connection extends BaseConnection
     public $lastInsertedTableName;
 
     /**
-     * confirm DNS format.
+     * confirm DSN format.
      */
     private function isValidDSN(): bool
     {
