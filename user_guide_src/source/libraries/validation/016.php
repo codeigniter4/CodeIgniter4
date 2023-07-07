39406f0972d02a0a9fd2e5e707b0649799f68173
kenjis <kenji.uui@gmail.com>
2023-07-07 11:52:32 +0900

    docs: add max_length to sample code
    It is good practice to check max length.

--- user_guide_src/source/libraries/validation/016.php
+++ user_guide_src/source/libraries/validation/016.php
@@ -6,13 +6,13 @@ class Validation
 {
     public $signup = [
         'username' => [
-            'rules'  => 'required',
+            'rules'  => 'required|max_length[30]',
             'errors' => [
                 'required' => 'You must choose a Username.',
             ],
         ],
         'email' => [
-            'rules'  => 'required|valid_email',
+            'rules'  => 'required|max_length[254]|valid_email',
             'errors' => [
                 'valid_email' => 'Please check the Email field. It does not appear to be valid.',
             ],
