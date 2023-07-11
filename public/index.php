db612fab7c62cb0db7305d92f5ac6d0c22f6cb45
kenjis <kenji.uui@gmail.com>
2023-07-11 17:59:58 +0900

    refactor: move definition of ENVIRONMENT to index.php/spark
    For Config caching.

--- public/index.php
+++ public/index.php
@@ -43,6 +43,11 @@ require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstra
 require_once SYSTEMPATH . 'Config/DotEnv.php';
 (new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
 
+// Define ENVIRONMENT
+if (! defined('ENVIRONMENT')) {
+    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
+}
+
 /*
  * ---------------------------------------------------------------
  * GRAB OUR CODEIGNITER INSTANCE
