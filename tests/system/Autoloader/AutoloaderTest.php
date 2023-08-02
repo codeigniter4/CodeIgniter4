c7135801417c924917e645740ba4a12a4ca6f2f8
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2023-08-02 10:30:50 +0800

    Add Closure signature

--- tests/system/Autoloader/AutoloaderTest.php
+++ tests/system/Autoloader/AutoloaderTest.php
@@ -20,7 +20,6 @@ use Config\Autoload;
 use Config\Modules;
 use Config\Services;
 use InvalidArgumentException;
-use PHPUnit\Framework\MockObject\MockObject;
 use RuntimeException;
 use UnnamespacedClass;
 
@@ -34,6 +33,10 @@ final class AutoloaderTest extends CIUnitTestCase
     use ReflectionHelper;
 
     private Autoloader $loader;
+
+    /**
+     * @phpstan-var Closure(string): (false|string)
+     */
     private Closure $classLoader;
 
     protected function setUp(): void
