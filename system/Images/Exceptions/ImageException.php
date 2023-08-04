eac79d83b4cddb8ac3ec76faa714e327cca8d128
ping-yee <611077101@mail.nknu.edu.tw>
2023-08-04 13:23:19 +0800

    Dev: change the return type for the exception.

--- system/Images/Exceptions/ImageException.php
+++ system/Images/Exceptions/ImageException.php
@@ -16,52 +16,52 @@ use CodeIgniter\Exceptions\FrameworkException;
 
 class ImageException extends FrameworkException implements ExceptionInterface
 {
-    public static function forMissingImage(): ImageException
+    public static function forMissingImage(): self
     {
         return new static(lang('Images.sourceImageRequired'));
     }
 
-    public static function forFileNotSupported(): ImageException
+    public static function forFileNotSupported(): self
     {
         return new static(lang('Images.fileNotSupported'));
     }
 
-    public static function forMissingAngle(): ImageException
+    public static function forMissingAngle(): self
     {
         return new static(lang('Images.rotationAngleRequired'));
     }
 
-    public static function forInvalidDirection(?string $dir = null): ImageException
+    public static function forInvalidDirection(?string $dir = null): self
     {
         return new static(lang('Images.invalidDirection', [$dir]));
     }
 
-    public static function forInvalidPath(): ImageException
+    public static function forInvalidPath(): self
     {
         return new static(lang('Images.invalidPath'));
     }
 
-    public static function forEXIFUnsupported(): ImageException
+    public static function forEXIFUnsupported(): self
     {
         return new static(lang('Images.exifNotSupported'));
     }
 
-    public static function forInvalidImageCreate(?string $extra = null): ImageException
+    public static function forInvalidImageCreate(?string $extra = null): self
     {
         return new static(lang('Images.unsupportedImageCreate') . ' ' . $extra);
     }
 
-    public static function forSaveFailed(): ImageException
+    public static function forSaveFailed(): self
     {
         return new static(lang('Images.saveFailed'));
     }
 
-    public static function forInvalidImageLibraryPath(?string $path = null): ImageException
+    public static function forInvalidImageLibraryPath(?string $path = null): self
     {
         return new static(lang('Images.libPathInvalid', [$path]));
     }
 
-    public static function forImageProcessFailed(): ImageException
+    public static function forImageProcessFailed(): self
     {
         return new static(lang('Images.imageProcessFailed'));
     }
