ed833192d3daced6ee444dfbf1b3b31b9ccde271
kenjis <kenji.uui@gmail.com>
2023-08-04 15:44:45 +0900

    docs: use max-age instead of expires

--- user_guide_src/source/libraries/cookies/017.php
+++ user_guide_src/source/libraries/cookies/017.php
@@ -2,7 +2,6 @@
 
 use CodeIgniter\Cookie\Cookie;
 use Config\Services;
-use DateTime;
 
 $response = Services::response();
 
@@ -10,7 +9,7 @@ $cookie = new Cookie(
     'remember_token',
     'f699c7fd18a8e082d0228932f3acd40e1ef5ef92efcedda32842a211d62f0aa6',
     [
-        'expires' => new DateTime('+2 hours'),
+        'max-age' => 3600 * 2, // Expires in 2 hours
     ]
 );
 
--- user_guide_src/source/libraries/cookies/018.php
+++ user_guide_src/source/libraries/cookies/018.php
@@ -1,7 +1,6 @@
 <?php
 
 use CodeIgniter\Cookie\Cookie;
-use DateTime;
 
 helper('cookie');
 
@@ -9,7 +8,7 @@ $cookie = new Cookie(
     'remember_token',
     'f699c7fd18a8e082d0228932f3acd40e1ef5ef92efcedda32842a211d62f0aa6',
     [
-        'expires' => new DateTime('+2 hours'),
+        'max-age' => 3600 * 2, // Expires in 2 hours
     ]
 );
 
