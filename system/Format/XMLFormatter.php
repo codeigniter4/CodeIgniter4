5c6298d7f7493017cce77d73362d110f9a6a23f7
蔡銘凱 <z85385637@gmail.com>
2023-08-08 14:21:25 +0800

    Fix: Remove the system-Format ignore errors.

--- system/Format/XMLFormatter.php
+++ system/Format/XMLFormatter.php
@@ -53,6 +53,8 @@ class XMLFormatter implements FormatterInterface
      * @see http://www.codexworld.com/convert-array-to-xml-in-php/
      *
      * @param SimpleXMLElement $output
+     *
+     * @return void
      */
     protected function arrayToXML(array $data, &$output)
     {
