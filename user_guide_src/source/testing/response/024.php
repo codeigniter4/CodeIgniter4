71b5a95cab29ade4b164347a47353140ac2c2ce0
kenjis <kenji.uui@gmail.com>
2023-08-10 05:40:15 +0900

    docs: improve comments in sample code

--- user_guide_src/source/testing/response/024.php
+++ user_guide_src/source/testing/response/024.php
@@ -1,10 +1,13 @@
 <?php
 
-// Check that "Hello World" is on the page
+// Verify that "Hello World" is on the page
 $result->assertSee('Hello World');
-// Check that "Hello World" is within an h1 tag
+
+// Verify that "Hello World" is within an h1 tag
 $result->assertSee('Hello World', 'h1');
-// Check that "Hello World" is within an element with the "notice" class
+
+// Verify that "Hello World" is within an element with the "notice" class
 $result->assertSee('Hello World', '.notice');
-// Check that "Hello World" is within an element with id of "title"
+
+// Verify that "Hello World" is within an element with id of "title"
 $result->assertSee('Hello World', '#title');
