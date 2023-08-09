054fa727ce285427696598994be632a6b55c64d8
kenjis <kenji.uui@gmail.com>
2023-08-10 05:39:52 +0900

    docs: fix incorrect sample code

--- user_guide_src/source/testing/response/025.php
+++ user_guide_src/source/testing/response/025.php
@@ -1,6 +1,7 @@
 <?php
 
-// Checks that "Hello World" does NOT exist on the page
-$results->dontSee('Hello World');
-// Checks that "Hello World" does NOT exist within any h1 tag
-$results->dontSee('Hello World', 'h1');
+// Verify that "Hello World" does NOT exist on the page
+$results->assertDontSee('Hello World');
+
+// Verify that "Hello World" does NOT exist within any h1 tag
+$results->assertDontSee('Hello World', 'h1');
--- user_guide_src/source/testing/response/028.php
+++ user_guide_src/source/testing/response/028.php
@@ -1,6 +1,7 @@
 <?php
 
-// Check that a link exists with 'Upgrade Account' as the text::
-$results->seeLink('Upgrade Account');
-// Check that a link exists with 'Upgrade Account' as the text, AND a class of 'upsell'
-$results->seeLink('Upgrade Account', '.upsell');
+// Verify that a link exists with 'Upgrade Account' as the text::
+$results->assertSeeLink('Upgrade Account');
+
+// Verify that a link exists with 'Upgrade Account' as the text, AND a class of 'upsell'
+$results->assertSeeLink('Upgrade Account', '.upsell');
