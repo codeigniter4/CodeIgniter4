68d21ebbf33feb72d786e23c5669908903f1656d
kenjis <kenji.uui@gmail.com>
2023-08-15 06:56:59 +0900

    style: composer cs-fix

--- system/Helpers/number_helper.php
+++ system/Helpers/number_helper.php
@@ -15,8 +15,7 @@ if (! function_exists('number_to_size')) {
     /**
      * Formats a numbers as bytes, based on size, and adds the appropriate suffix
      *
-     * @param int|string $num    Will be cast as int
-     * @param string     $locale
+     * @param int|string $num Will be cast as int
      *
      * @return bool|string
      */
