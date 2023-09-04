b1001a86f893979b80f7481f0f0d42f40751640c
neznaika0 <ozornick.ks@gmail.com>
2023-09-04 21:23:26 +0300

    test: Fix namespace in tests

--- tests/_support/Services/Translation/TranslationTwo.php
+++ tests/_support/Services/Translation/TranslationTwo.php
@@ -9,7 +9,7 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace Tests\Support\Controllers\Translation;
+namespace Tests\Support\Services\Translation;
 
 class TranslationTwo
 {
