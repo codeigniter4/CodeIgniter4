034f9ddf6efc20ea9b290db50c5908283d687164
neznaika0 <ozornick.ks@gmail.com>
2023-09-04 22:55:24 +0300

    docs: Example code with command

---
+++ user_guide_src/source/outgoing/localization/019.php
@@ -0,0 +1,13 @@
+<?php
+
+// Controllers/Translation/Lang.php
+$message  = lang('Text.info.success');
+$message2 = lang('Text.paragraph');
+
+// Save in Language/en/Text.php
+return [
+    'info' => [
+        'success' => 'Text.info.success',
+    ],
+    'paragraph' => 'Text.paragraph',
+];
