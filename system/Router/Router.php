eca7c8271c2a32d3364abb3bc506c43f714fea55
kenjis <kenji.uui@gmail.com>
2023-09-06 11:45:44 +0900

    refactor: remove Router::$filterInfo and getFilter()

--- system/Router/Router.php
+++ system/Router/Router.php
@@ -103,16 +103,6 @@ class Router implements RouterInterface
      * The filter info from Route Collection
      * if the matched route should be filtered.
      *
-     * @var string|null
-     *
-     * @deprecated Use $filtersInfo
-     */
-    protected $filterInfo;
-
-    /**
-     * The filter info from Route Collection
-     * if the matched route should be filtered.
-     *
      * @var string[]
      */
     protected $filtersInfo = [];
@@ -179,7 +169,6 @@ class Router implements RouterInterface
         $uri = urldecode($uri);
 
         // Restart filterInfo
-        $this->filterInfo  = null;
         $this->filtersInfo = [];
 
         // Checks defined routes
@@ -204,18 +193,6 @@ class Router implements RouterInterface
         $this->autoRoute($uri);
 
         return $this->controllerName();
-    }
-
-    /**
-     * Returns the filter info for the matched route, if any.
-     *
-     * @return string|null
-     *
-     * @deprecated Use getFilters()
-     */
-    public function getFilter()
-    {
-        return $this->filterInfo;
     }
 
     /**
