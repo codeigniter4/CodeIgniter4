f75e33d57b6e4742e909cc207aa7af6177ddfc8d
kenjis <kenji.uui@gmail.com>
2023-09-07 15:51:15 +0900

    chore: change PHPUnitLevelSetList::UP_TO_PHPUNIT_100 to PHPUnitSetList::PHPUNIT_100
    It does not work for us.
    See https://github.com/codeigniter4/CodeIgniter4/pull/7904#discussion_r1318145952

--- rector.php
+++ rector.php
@@ -40,7 +40,6 @@ use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php73\Rector\FuncCall\StringifyStrNeedlesRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\ConstructClassMethodToSetUpTestCaseRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\YieldDataProviderRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
@@ -54,8 +53,8 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->sets([
         SetList::DEAD_CODE,
         LevelSetList::UP_TO_PHP_74,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
         PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_100,
     ]);
 
     $rectorConfig->parallel(240, 8, 1);
