ecad35478d978bbc41d3ece20f127624cc4d0dd2
neznaika0 <ozornick.ks@gmail.com>
2023-09-08 21:08:25 +0300

    fix: Add array helper as separate class

---
+++ system/Commands/Translation/LocalizationFinder/ArrayHelper.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Commands\Translation\LocalizationFinder;
+
+class ArrayHelper
+{
+    /**
+     * Compare recursive two associative arrays and return new array (difference)
+     * Returns keys that exist in $original but not in $compareWith
+     */
+    public static function recursiveDiff(array $original, array $compareWith): array
+    {
+        $difference = [];
+
+        if ([] === $original) {
+            return [];
+        }
+
+        if ([] === $compareWith) {
+            return $original;
+        }
+
+        foreach ($original as $originalKey => $originalValue) {
+            if ([] === $originalValue) {
+                continue;
+            }
+
+            if (is_array($originalValue)) {
+                $diffArrays = [];
+
+                if (isset($compareWith[$originalKey]) && is_array($compareWith[$originalKey])) {
+                    $diffArrays = self::recursiveDiff($originalValue, $compareWith[$originalKey]);
+                } else {
+                    $difference[$originalKey] = $originalValue;
+                }
+
+                if ([] !== $diffArrays) {
+                    $difference[$originalKey] = $diffArrays;
+                }
+            } elseif (is_string($originalValue) && ! array_key_exists($originalKey, $compareWith)) {
+                $difference[$originalKey] = $originalValue;
+            }
+        }
+
+        return $difference;
+    }
+
+    /**
+     * Count all keys (with nested keys)
+     */
+    public static function recursiveCount(array $array, int $counter = 0): int
+    {
+        foreach ($array as $value) {
+            if (is_array($value)) {
+                $counter = self::recursiveCount($value, $counter);
+            }
+
+            $counter++;
+        }
+
+        return $counter;
+    }
+}
