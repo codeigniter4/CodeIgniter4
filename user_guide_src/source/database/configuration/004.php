995da81a3be565b47f92767ab86922b6111d28d4
kenjis <kenji.uui@gmail.com>
2023-09-11 12:06:13 +0900

    docs: update utf8 to utf8mb4

--- user_guide_src/source/database/configuration/004.php
+++ user_guide_src/source/database/configuration/004.php
@@ -10,7 +10,7 @@ class Database extends Config
 
     // MySQLi
     public array $default = [
-        'DSN' => 'MySQLi://username:password@hostname:3306/database?charset=utf8&DBCollat=utf8_general_ci',
+        'DSN' => 'MySQLi://username:password@hostname:3306/database?charset=utf8mb4&DBCollat=utf8mb4_general_ci',
         // ...
     ];
 
