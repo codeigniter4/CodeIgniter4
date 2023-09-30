3ebc1a0aa9d5d659a278f17c823d97549f789f76
kenjis <kenji.uui@gmail.com>
2023-09-30 10:09:24 +0900

    fix!: add toDatabase() and fromDatabase() to CastInterface

--- system/Entity/Cast/BaseCast.php
+++ system/Entity/Cast/BaseCast.php
@@ -17,7 +17,8 @@ namespace CodeIgniter\Entity\Cast;
 abstract class BaseCast implements CastInterface
 {
     /**
-     * Get
+     * Returns value when getting the Entity property.
+     * This method is normally returns the value as it is.
      *
      * @param array|bool|float|int|object|string|null $value  Data
      * @param array                                   $params Additional param
@@ -30,7 +31,7 @@ abstract class BaseCast implements CastInterface
     }
 
     /**
-     * Set
+     * Returns value for Entity property when setting the Entity property.
      *
      * @param array|bool|float|int|object|string|null $value  Data
      * @param array                                   $params Additional param
@@ -38,6 +39,32 @@ abstract class BaseCast implements CastInterface
      * @return array|bool|float|int|object|string|null
      */
     public static function set($value, array $params = [])
+    {
+        return $value;
+    }
+
+    /**
+     * Takes the Entity property value, returns its value for database.
+     *
+     * @param array|bool|float|int|object|string|null $value  Data
+     * @param array                                   $params Additional param
+     *
+     * @return bool|float|int|string|null
+     */
+    public static function toDatabase($value, array $params = [])
+    {
+        return $value;
+    }
+
+    /**
+     * Takes value from database, returns its value for the Entity property.
+     *
+     * @param bool|float|int|string|null $value  Data
+     * @param array                      $params Additional param
+     *
+     * @return array|bool|float|int|object|string|null
+     */
+    public static function fromDatabase($value, array $params = [])
     {
         return $value;
     }
--- system/Entity/Cast/CastInterface.php
+++ system/Entity/Cast/CastInterface.php
@@ -13,11 +13,15 @@ namespace CodeIgniter\Entity\Cast;
 
 /**
  * Interface CastInterface
+ *
+ * [App Code] --- set() --> [Entity] --- toDatabase() ---> [Database]
+ * [App Code] <-- get() --- [Entity] <-- fromDatabase() -- [Database]
  */
 interface CastInterface
 {
     /**
-     * Get
+     * Returns value when getting the Entity property.
+     * This method is normally returns the value as it is.
      *
      * @param array|bool|float|int|object|string|null $value  Data
      * @param array                                   $params Additional param
@@ -27,7 +31,7 @@ interface CastInterface
     public static function get($value, array $params = []);
 
     /**
-     * Set
+     * Returns value for the Entity property when setting the Entity property.
      *
      * @param array|bool|float|int|object|string|null $value  Data
      * @param array                                   $params Additional param
@@ -35,4 +39,24 @@ interface CastInterface
      * @return array|bool|float|int|object|string|null
      */
     public static function set($value, array $params = []);
+
+    /**
+     * Takes the Entity property value, returns its value for database.
+     *
+     * @param array|bool|float|int|object|string|null $value  Data
+     * @param array                                   $params Additional param
+     *
+     * @return bool|float|int|string|null
+     */
+    public static function toDatabase($value, array $params = []);
+
+    /**
+     * Takes value from database, returns its value for the Entity property.
+     *
+     * @param bool|float|int|string|null $value  Data
+     * @param array                      $params Additional param
+     *
+     * @return array|bool|float|int|object|string|null
+     */
+    public static function fromDatabase($value, array $params = []);
 }
