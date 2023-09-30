c089d1db8140dcc3270b675da2f0ead702b3c2ae
kenjis <kenji.uui@gmail.com>
2023-09-30 10:36:12 +0900

    test: add Entity Live tests

---
+++ tests/_support/Entity/Database/Migrations/2023-09-29-103000_CreateUsersTable.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests\Support\Entity\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up(): void
+    {
+        $this->forge->addField([
+            'id'         => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'username'   => ['type' => 'varchar', 'constraint' => 30, 'null' => true],
+            'active'     => ['type' => 'tinyint', 'constraint' => 1, 'null' => 0, 'default' => 0],
+            'memo'       => ['type' => 'text', 'null' => true],
+            'created_at' => ['type' => 'datetime', 'null' => true],
+            'updated_at' => ['type' => 'datetime', 'null' => true],
+            'deleted_at' => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->addUniqueKey('username');
+        $this->forge->createTable('users');
+    }
+
+    public function down(): void
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ tests/system/Entity/EntityLiveTest.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Entity;
+
+use CodeIgniter\Database\Exceptions\DatabaseException;
+use CodeIgniter\I18n\Time;
+use CodeIgniter\Model;
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\DatabaseTestTrait;
+use Config\Services;
+
+/**
+ * @internal
+ *
+ * @group DatabaseLive
+ */
+final class EntityLiveTest extends CIUnitTestCase
+{
+    use DatabaseTestTrait;
+
+    protected $namespace = 'Tests\Support\Entity';
+
+    protected function setUp(): void
+    {
+        $this->setUpMethods[] = 'setUpAddNamespace';
+
+        parent::setUp();
+    }
+
+    protected function setUpAddNamespace(): void
+    {
+        Services::autoloader()->addNamespace(
+            'Tests\Support\Entity',
+            SUPPORTPATH . 'Entity'
+        );
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->regressDatabase();
+    }
+
+    public function testEntityReturnsValuesWithCorrectTypes()
+    {
+        $entity = new class () extends Entity {
+            protected $casts = [
+                'id'     => 'int',
+                'active' => 'int-bool',
+                'memo'   => 'json',
+            ];
+        };
+        $model = new class () extends Model {
+            protected $table         = 'users';
+            protected $allowedFields = [
+                'username', 'active', 'memo',
+            ];
+            protected $useTimestamps = true;
+        };
+        $entity->fill(['username' => 'johnsmith', 'active' => false, 'memo' => ['foo', 'bar']]);
+        $model->save($entity);
+
+        $user = $model->asObject(get_class($entity))->find(1);
+
+        $this->assertSame(1, $user->id);
+        $this->assertSame('johnsmith', $user->username);
+        $this->assertFalse($user->active);
+        $this->assertSame(['foo', 'bar'], $user->memo);
+        $this->assertInstanceOf(Time::class, $user->created_at);
+        $this->assertInstanceOf(Time::class, $user->updated_at);
+    }
+
+    /**
+     * @TODO Fix the object cast handler implementation.
+     */
+    public function testCastObject(): void
+    {
+        $this->expectException(DatabaseException::class);
+
+        $entity = new class () extends Entity {
+            protected $casts = [
+                'id'     => 'int',
+                'active' => 'int-bool',
+                'memo'   => 'object',
+            ];
+        };
+        $model = new class () extends Model {
+            protected $table         = 'users';
+            protected $allowedFields = [
+                'username', 'active', 'memo',
+            ];
+            protected $useTimestamps = true;
+        };
+        $entity->fill(['username' => 'johnsmith', 'active' => false, 'memo' => ['foo', 'bar']]);
+        $model->save($entity);
+    }
+}
