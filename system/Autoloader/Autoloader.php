092989f68be3d8978cc3579b88517f090fc3da9d
kenjis <kenji.uui@gmail.com>
2023-10-04 20:06:00 +0900

    perf: use Composer autoloader first
    - When we use Composer, we use Composer autoloader first.
      So no need to load Composer classmap to CI4 autoloader.
    - Add `App` and `Config` namespaces to composer.json.
      So we can use composer dump-autoload for app classes.

--- system/Autoloader/Autoloader.php
+++ system/Autoloader/Autoloader.php
@@ -137,8 +137,6 @@ class Autoloader
          */
         $composer = include COMPOSER_PATH;
 
-        $this->loadComposerClassmap($composer);
-
         // Should we load through Composer's namespaces, also?
         if ($modules->discoverInComposer) {
             // @phpstan-ignore-next-line
@@ -155,11 +153,11 @@ class Autoloader
      */
     public function register()
     {
-        // Prepend the PSR4  autoloader for maximum performance.
-        spl_autoload_register([$this, 'loadClass'], true, true);
+        // Register classmap loader for the files in our class map.
+        spl_autoload_register([$this, 'loadClassmap'], true);
 
-        // Now prepend another loader for the files in our class map.
-        spl_autoload_register([$this, 'loadClassmap'], true, true);
+        // Register the PSR-4 autoloader.
+        spl_autoload_register([$this, 'loadClass'], true);
 
         // Load our non-class files
         foreach ($this->files as $file) {
@@ -362,9 +360,13 @@ class Autoloader
     {
         $namespacePaths = $composer->getPrefixesPsr4();
 
-        // Get rid of CodeIgniter so we don't have duplicates
-        if (isset($namespacePaths['CodeIgniter\\'])) {
-            unset($namespacePaths['CodeIgniter\\']);
+        // Get rid of duplicated namespaces.
+        $duplicatedNamespaces = ['CodeIgniter', APP_NAMESPACE, 'Config'];
+
+        foreach ($duplicatedNamespaces as $ns) {
+            if (isset($namespacePaths[$ns . '\\'])) {
+                unset($namespacePaths[$ns . '\\']);
+            }
         }
 
         if (! method_exists(InstalledVersions::class, 'getAllRawData')) {
