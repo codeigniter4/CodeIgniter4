e5d7d3f38ed2a9dc79fe34ecc30d0344e03fad3b
kenjis <kenji.uui@gmail.com>
2023-10-05 08:45:30 +0900

    docs: update sample code

--- user_guide_src/source/concepts/autoloader/001.php
+++ user_guide_src/source/concepts/autoloader/001.php
@@ -8,8 +8,7 @@ class Autoload extends AutoloadConfig
 {
     // ...
     public $psr4 = [
-        APP_NAMESPACE => APPPATH, // For custom app namespace
-        'Config'      => APPPATH . 'Config',
+        APP_NAMESPACE => APPPATH,
     ];
 
     // ...
