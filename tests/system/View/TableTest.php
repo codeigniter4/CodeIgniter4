4c0eab55ef6da3235c23b6a32dda8ab02ac876bb
kenjis <kenji.uui@gmail.com>
2023-10-27 05:32:27 +0900

    test: remove unneeded table template

--- tests/system/View/TableTest.php
+++ tests/system/View/TableTest.php
@@ -822,42 +822,7 @@ final class TableTest extends CIUnitTestCase
      */
     public function testGenerateTableWithHeadingContainFieldNamedData(): void
     {
-        $tableTemplate = [
-            'table_open' => '<table border="1" cellpadding="2" cellspacing="1">',
-
-            'thead_open'  => '<thead>',
-            'thead_close' => '</thead>',
-
-            'heading_row_start'  => '<tr>',
-            'heading_row_end'    => '</tr>',
-            'heading_cell_start' => '<th>',
-            'heading_cell_end'   => '</th>',
-
-            'tfoot_open'  => '<tfoot>',
-            'tfoot_close' => '</tfoot>',
-
-            'footing_row_start'  => '<tr>',
-            'footing_row_end'    => '</tr>',
-            'footing_cell_start' => '<td>',
-            'footing_cell_end'   => '</td>',
-
-            'tbody_open'  => '<tbody>',
-            'tbody_close' => '</tbody>',
-
-            'row_start'  => '<tr>',
-            'row_end'    => '</tr>',
-            'cell_start' => '<td>',
-            'cell_end'   => '</td>',
-
-            'row_alt_start'  => '<tr>',
-            'row_alt_end'    => '</tr>',
-            'cell_alt_start' => '<td>',
-            'cell_alt_end'   => '</td>',
-
-            'table_close' => '</table>',
-        ];
-
-        $table = new Table($tableTemplate);
+        $table = new Table();
         $table->setHeading([
             'codigo'         => 'Codigo Orçamento',
             'data'           => 'Data do Orçamento',
