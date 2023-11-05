75907cff0152c47201f4cdd16d8f0a0f9732b7e0
kenjis <kenji.uui@gmail.com>
2023-11-06 08:04:05 +0900

    docs: fix comment in sample code

--- user_guide_src/source/libraries/validation/008.php
+++ user_guide_src/source/libraries/validation/008.php
@@ -4,7 +4,8 @@ $validation = \Config\Services::validation();
 $request    = \Config\Services::request();
 
 if ($validation->withRequest($request)->run()) {
-    // If you want to get the validated data.
+    // If you use the input data, you should get it from the getValidated() method.
+    // Otherwise you may create a vulnerability.
     $validData = $validation->getValidated();
 
     // ...
