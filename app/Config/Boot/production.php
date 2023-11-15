d7f566c29b25d7a78f67b62e7c3048a1befa2f66
kenjis <kenji.uui@gmail.com>
2023-11-15 10:30:23 +0900

    config: change error_reporting to -1 in production
    In production in many cases -1 is also appropriate.

--- app/Config/Boot/production.php
+++ app/Config/Boot/production.php
@@ -9,8 +9,8 @@
  |
  | If you set 'display_errors' to '1', CI4's detailed error report will show.
  */
+error_reporting(-1);
 ini_set('display_errors', '0');
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 
 /*
  |--------------------------------------------------------------------------
