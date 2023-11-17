63fc2c04c9cfea49d13aa7b3de8fd95f5c4e204c
kenjis <kenji.uui@gmail.com>
2023-11-18 07:06:54 +0900

    docs: align comments

--- user_guide_src/source/database/results/007.php
+++ user_guide_src/source/database/results/007.php
@@ -3,5 +3,5 @@
 $query = $db->query('SELECT * FROM users LIMIT 1;');
 $row   = $query->getRow(0, \App\Entities\User::class);
 
-echo $row->name; // access attributes
+echo $row->name;           // access attributes
 echo $row->reverse_name(); // or methods defined on the 'User' class
