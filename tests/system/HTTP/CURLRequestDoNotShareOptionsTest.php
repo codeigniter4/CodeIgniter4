9e6be65899e1caa182303bdea82315cf53482242
kenjis <kenji.uui@gmail.com>
2023-11-21 13:58:43 +0900

    test: add test for multiple headers

--- tests/system/HTTP/CURLRequestDoNotShareOptionsTest.php
+++ tests/system/HTTP/CURLRequestDoNotShareOptionsTest.php
@@ -852,6 +852,38 @@ Transfer-Encoding: chunked\x0d\x0a\x0d\x0a<title>Hello2</title>";
         $this->assertSame(200, $response->getStatusCode());
     }
 
+    public function testResponseHeadersWithMultipleSetCookies(): void
+    {
+        $request = $this->getRequest([
+            'base_uri' => 'https://github.com/',
+        ]);
+
+        $output = "HTTP/2 200
+server: GitHub.com
+date: Sat, 11 Nov 2023 02:26:55 GMT
+content-type: text/html; charset=utf-8
+set-cookie: _gh_sess=PlRlha1YumlLhLuo5MuNbIWJRO9RRuR%2FHfYsWRh5B0mkalFIZstlAbTmSstl8q%2FAC57IsWMVuFHWQc6L4qDHQJrwhuYVO5ZaigPCUjAStnhh%2FieZQVqIf92Al7vusuzx2o8XH%2Fv6nd9qzMTAWc2%2FkRsl8jxPQYGNaWeuUBY2w3%2FDORSikN4c0vHOyedhU7Xcv3Ryz5xD3DNxK9R8xKNZ6OSXLJ6bjX8iIT6LxvroVIf2HjvowW9cQsq0kN08mS6KtTnH0mD3ANWqsVVWeMzFNA%3D%3D--Jx830Q9Nmkfz9OGA--kEcPtNphvjNMopYqFDxUbw%3D%3D; Path=/; HttpOnly; Secure; SameSite=Lax
+set-cookie: _octo=GH1.1.599292127.1699669625; Path=/; Domain=github.com; Expires=Mon, 11 Nov 2024 02:27:05 GMT; Secure; SameSite=Lax
+set-cookie: logged_in=no; Path=/; Domain=github.com; Expires=Mon, 11 Nov 2024 02:27:05 GMT; HttpOnly; Secure; SameSite=Lax
+accept-ranges: bytes\x0d\x0a\x0d\x0a";
+        $request->setOutput($output);
+
+        $response = $request->get('/');
+
+        $setCookieHeaders = $response->header('set-cookie');
+
+        $this->assertCount(3, $setCookieHeaders);
+        $this->assertSame(
+            'logged_in=no; Path=/; Domain=github.com; Expires=Mon, 11 Nov 2024 02:27:05 GMT; HttpOnly; Secure; SameSite=Lax',
+            $setCookieHeaders[2]->getValue()
+        );
+
+        $this->assertSame(
+            '_octo=GH1.1.599292127.1699669625; Path=/; Domain=github.com; Expires=Mon, 11 Nov 2024 02:27:05 GMT; Secure; SameSite=Lax',
+            $setCookieHeaders[1]->getValueLine()
+        );
+    }
+
     public function testSplitResponse(): void
     {
         $request = $this->getRequest([
