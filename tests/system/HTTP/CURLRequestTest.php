5c4a23bdbd6d4be20f89518bc4c656815ae058bc
Nicolae Iotu <nicolae.g.iotu@gmail.com>
2023-11-26 04:36:03 +0000

    Skip hostname checks if CURLRequest options 'verify' is set to false.
    When CURLRequest options 'verify' is set to false, some CURLOPT_SSL_... options should be disabled in such a way as to allow requests to pass through in case the destination is for example on private networks.

--- tests/system/HTTP/CURLRequestTest.php
+++ tests/system/HTTP/CURLRequestTest.php
@@ -502,6 +502,9 @@ final class CURLRequestTest extends CIUnitTestCase
 
         $this->assertArrayHasKey(CURLOPT_SSL_VERIFYPEER, $options);
         $this->assertSame(1, $options[CURLOPT_SSL_VERIFYPEER]);
+
+        $this->assertArrayHasKey(CURLOPT_SSL_VERIFYHOST, $options);
+        $this->assertSame(2, $options[CURLOPT_SSL_VERIFYHOST]);
     }
 
     public function testSSLWithBadKey()
