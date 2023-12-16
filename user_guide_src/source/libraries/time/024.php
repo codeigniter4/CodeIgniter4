73053d3a270bb26d4a09e6141f445e49e60152ae
kenjis <kenji.uui@gmail.com>
2023-12-16 10:39:12 +0900

    docs: fix incorrect sample code

--- user_guide_src/source/libraries/time/024.php
+++ user_guide_src/source/libraries/time/024.php
@@ -2,5 +2,5 @@
 
 use CodeIgniter\I18n\Time;
 
-echo Time::now()->getLocal();    // true
-echo Time::now('Europe/London'); // false
+echo Time::now()->getLocal();           // true
+echo Time::now('Europe/London')->local; // false
