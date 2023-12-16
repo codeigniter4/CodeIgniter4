0a94fea4a22a430e9e02e3999c7bc32650c8d238
kenjis <kenji.uui@gmail.com>
2023-12-16 10:29:39 +0900

    docs: fix commnets in sample code

--- user_guide_src/source/libraries/time/020.php
+++ user_guide_src/source/libraries/time/020.php
@@ -4,16 +4,17 @@ use CodeIgniter\I18n\Time;
 
 $time = Time::parse('August 12, 2016 4:15:23pm');
 
-echo $time->getYear();   // 2016
-echo $time->getMonth();  // 8
-echo $time->getDay();    // 12
-echo $time->getHour();   // 16
-echo $time->getMinute(); // 15
-echo $time->getSecond(); // 23
+// The output may vary based on locale.
+echo $time->getYear();   // '2016'
+echo $time->getMonth();  // '8'
+echo $time->getDay();    // '12'
+echo $time->getHour();   // '16'
+echo $time->getMinute(); // '15'
+echo $time->getSecond(); // '23'
 
-echo $time->year;   // 2016
-echo $time->month;  // 8
-echo $time->day;    // 12
-echo $time->hour;   // 16
-echo $time->minute; // 15
-echo $time->second; // 23
+echo $time->year;   // '2016'
+echo $time->month;  // '8'
+echo $time->day;    // '12'
+echo $time->hour;   // '16'
+echo $time->minute; // '15'
+echo $time->second; // '23'
--- user_guide_src/source/libraries/time/021.php
+++ user_guide_src/source/libraries/time/021.php
@@ -4,16 +4,17 @@ use CodeIgniter\I18n\Time;
 
 $time = Time::parse('August 12, 2016 4:15:23pm');
 
-echo $time->getDayOfWeek();   // 6 - but may vary based on locale's starting day of the week
-echo $time->getDayOfYear();   // 225
-echo $time->getWeekOfMonth(); // 2
-echo $time->getWeekOfYear();  // 33
-echo $time->getTimestamp();   // 1471018523 - UNIX timestamp
-echo $time->getQuarter();     // 3
+// The output may vary based on locale.
+echo $time->getDayOfWeek();   // '6'
+echo $time->getDayOfYear();   // '225'
+echo $time->getWeekOfMonth(); // '2'
+echo $time->getWeekOfYear();  // '33'
+echo $time->getTimestamp();   // 1471018523 - UNIX timestamp (locale independent)
+echo $time->getQuarter();     // '3'
 
-echo $time->dayOfWeek;   // 6
-echo $time->dayOfYear;   // 225
-echo $time->weekOfMonth; // 2
-echo $time->weekOfYear;  // 33
+echo $time->dayOfWeek;   // '6'
+echo $time->dayOfYear;   // '225'
+echo $time->weekOfMonth; // '2'
+echo $time->weekOfYear;  // '33'
 echo $time->timestamp;   // 1471018523
-echo $time->quarter;     // 3
+echo $time->quarter;     // '3'
