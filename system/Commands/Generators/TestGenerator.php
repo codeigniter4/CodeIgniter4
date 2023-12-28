9ab2708b19a0f577194fd633bb116e23ca589690
kenjis <kenji.uui@gmail.com>
2023-12-28 11:08:16 +0900

    feat: make:test creates test files in /tests/ directories

--- system/Commands/Generators/TestGenerator.php
+++ system/Commands/Generators/TestGenerator.php
@@ -14,7 +14,9 @@ declare(strict_types=1);
 namespace CodeIgniter\Commands\Generators;
 
 use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
 use CodeIgniter\CLI\GeneratorTrait;
+use Config\Services;
 
 /**
  * Generates a skeleton command file.
@@ -66,7 +68,8 @@ class TestGenerator extends BaseCommand
      * @var array<string, string>
      */
     protected $options = [
-        '--force' => 'Force overwrite existing file.',
+        '--namespace' => 'Set root namespace. Default: "Tests".',
+        '--force'     => 'Force overwrite existing file.',
     ];
 
     /**
@@ -76,9 +79,110 @@ class TestGenerator extends BaseCommand
     {
         $this->component = 'Test';
         $this->template  = 'test.tpl.php';
-        $this->namespace = 'Tests';
 
         $this->classNameLang = 'CLI.generator.className.test';
         $this->generateClass($params);
+    }
+
+    /**
+     * Gets the namespace from input or the default namespace.
+     */
+    protected function getNamespace(): string
+    {
+        if ($this->namespace !== null) {
+            return $this->namespace;
+        }
+
+        if ($this->getOption('namespace') !== null) {
+            return trim(
+                str_replace(
+                    '/',
+                    '\\',
+                    $this->getOption('namespace')
+                ),
+                '\\'
+            );
+        }
+
+        $class      = $this->normalizeInputClassName();
+        $classPaths = explode('\\', $class);
+
+        $namespaces = Services::autoloader()->getNamespace();
+
+        while ($classPaths !== []) {
+            array_pop($classPaths);
+            $namespace = implode('\\', $classPaths);
+
+            foreach (array_keys($namespaces) as $prefix) {
+                if ($prefix === $namespace) {
+                    // The input classname is FQCN, and use the namespace.
+                    return $namespace;
+                }
+            }
+        }
+
+        return 'Tests';
+    }
+
+    /**
+     * Builds the test file path from the class name.
+     *
+     * @param string $class namespaced classname.
+     */
+    protected function buildPath(string $class): string
+    {
+        $namespace = $this->getNamespace();
+
+        $base = $this->searchTestFilePath($namespace);
+
+        if ($base === null) {
+            CLI::error(
+                lang('CLI.namespaceNotDefined', [$namespace]),
+                'light_gray',
+                'red'
+            );
+            CLI::newLine();
+
+            return '';
+        }
+
+        $realpath = realpath($base);
+        $base     = ($realpath !== false) ? $realpath : $base;
+
+        $file = $base . DIRECTORY_SEPARATOR
+            . str_replace(
+                '\\',
+                DIRECTORY_SEPARATOR,
+                trim(str_replace($namespace . '\\', '', $class), '\\')
+            ) . '.php';
+
+        return implode(
+            DIRECTORY_SEPARATOR,
+            array_slice(
+                explode(DIRECTORY_SEPARATOR, $file),
+                0,
+                -1
+            )
+        ) . DIRECTORY_SEPARATOR . $this->basename($file);
+    }
+
+    /**
+     * Returns test file path for the namespace.
+     */
+    private function searchTestFilePath(string $namespace): ?string
+    {
+        $bases = Services::autoloader()->getNamespace($namespace);
+
+        $base = null;
+
+        foreach ($bases as $candidate) {
+            if (str_contains($candidate, '/tests/')) {
+                $base = $candidate;
+
+                break;
+            }
+        }
+
+        return $base;
     }
 }
