c5e72668326265faf74e58f2a3ee6794720b23ed
kenjis <kenji.uui@gmail.com>
2023-12-28 17:27:17 +0900

    Prep for 4.4.4 release

--- system/CodeIgniter.php
+++ system/CodeIgniter.php
@@ -54,7 +54,7 @@ class CodeIgniter
     /**
      * The current version of CodeIgniter Framework
      */
-    public const CI_VERSION = '4.4.3';
+    public const CI_VERSION = '4.4.4';
 
     /**
      * App startup time.
