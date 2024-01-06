cf10edd78bc37ef37110cdae79c2d04525957bce
michalsn <michal@sniatala.pl>
2024-01-06 14:55:17 +0100

    remove pre-commit

--- admin/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Install a pre-commit hook that
-# automatically runs php-cs-fixer to lint code
-mkdir -p .git/hooks
-cp admin/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
