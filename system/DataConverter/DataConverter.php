ac9c5367cfa85da1cef3bea34c31f5f2320c01e2
kenjis <kenji.uui@gmail.com>
2024-01-23 16:06:35 +0900

    refactor: use DataCaster in DataConverter

--- system/DataConverter/DataConverter.php
+++ system/DataConverter/DataConverter.php
@@ -13,68 +13,27 @@ declare(strict_types=1);
 
 namespace CodeIgniter\DataConverter;
 
-use CodeIgniter\DataCaster\Cast\ArrayCast;
-use CodeIgniter\DataCaster\Cast\BooleanCast;
-use CodeIgniter\DataCaster\Cast\CastInterface;
-use CodeIgniter\DataCaster\Cast\CSVCast;
-use CodeIgniter\DataCaster\Cast\DatetimeCast;
-use CodeIgniter\DataCaster\Cast\FloatCast;
-use CodeIgniter\DataCaster\Cast\IntBoolCast;
-use CodeIgniter\DataCaster\Cast\IntegerCast;
-use CodeIgniter\DataCaster\Cast\JsonCast;
-use CodeIgniter\DataCaster\Cast\TimestampCast;
-use CodeIgniter\DataCaster\Cast\URICast;
-use InvalidArgumentException;
-use TypeError;
+use CodeIgniter\DataCaster\DataCaster;
 
 /**
  * PHP data <==> DataSource data converter
  *
  * @see \CodeIgniter\DataConverter\DataConverterTest
  */
-class DataConverter
+final class DataConverter
 {
     /**
-     * Custom convert handlers
-     *
-     * @var array<string, class-string<CastInterface>> [type => classname]
+     * The data caster.
      */
-    protected $castHandlers = [];
+    private DataCaster $dataCaster;
 
     /**
-     * Default convert handlers
-     *
-     * @var array<string, class-string<CastInterface>> [type => classname]
+     * @param array<string, string> $types        [column => type]
+     * @param array                 $castHandlers Custom convert handlers
      */
-    private array $defaultCastHandlers = [
-        'array'     => ArrayCast::class,
-        'bool'      => BooleanCast::class,
-        'boolean'   => BooleanCast::class,
-        'csv'       => CSVCast::class,
-        'datetime'  => DatetimeCast::class,
-        'double'    => FloatCast::class,
-        'float'     => FloatCast::class,
-        'int'       => IntegerCast::class,
-        'integer'   => IntegerCast::class,
-        'int-bool'  => IntBoolCast::class,
-        'json'      => JsonCast::class,
-        'timestamp' => TimestampCast::class,
-        'uri'       => URICast::class,
-    ];
-
-    /**
-     * @param array $castHandlers Custom convert handlers
-     */
-    public function __construct(
-        /**
-         * @var array<string, string> [column => type]
-         */
-        private array $types,
-        array $castHandlers = []
-    ) {
-        if ($castHandlers !== []) {
-            $this->castHandlers = $castHandlers;
-        }
+    public function __construct(array $types, array $castHandlers = [])
+    {
+        $this->dataCaster = new DataCaster($castHandlers, $types);
     }
 
     /**
@@ -87,7 +46,7 @@ class DataConverter
         $output = [];
 
         foreach ($data as $field => $value) {
-            $output[$field] = $this->castAs($value, $field, 'get');
+            $output[$field] = $this->dataCaster->castAs($value, $field, 'get');
         }
 
         return $output;
@@ -103,81 +62,9 @@ class DataConverter
         $output = [];
 
         foreach ($phpData as $field => $value) {
-            $output[$field] = $this->castAs($value, $field, 'set');
+            $output[$field] = $this->dataCaster->castAs($value, $field, 'set');
         }
 
         return $output;
-    }
-
-    /**
-     * Provides the ability to cast an item as a specific data type.
-     * Add ? at the beginning of $type  (i.e. ?string) to get `null`
-     * instead of casting $value if ($value === null).
-     *
-     * @param mixed  $value  The value to convert
-     * @param string $field  The field name
-     * @param string $method Allowed to "get" and "set"
-     * @phpstan-param 'get'|'set' $method
-     */
-    protected function castAs($value, string $field, string $method = 'get'): mixed
-    {
-        // If the type is not defined, return as it is.
-        if (! isset($this->types[$field])) {
-            return $value;
-        }
-
-        $type = $this->types[$field];
-
-        $isNullable = false;
-
-        // Is nullable?
-        if (str_starts_with($type, '?')) {
-            $isNullable = true;
-
-            if ($value === null) {
-                return null;
-            }
-
-            $type = substr($type, 1);
-        } elseif ($value === null) {
-            $message = 'Field "' . $field . '" is not nullable, but null was passed.';
-
-            throw new TypeError($message);
-        }
-
-        // In order not to create a separate handler for the
-        // json-array type, we transform the required one.
-        $type = ($type === 'json-array') ? 'json[array]' : $type;
-
-        $params = [];
-
-        // Attempt to retrieve additional parameters if specified
-        // type[param, param2,param3]
-        if (preg_match('/\A(.+)\[(.+)\]\z/', $type, $matches)) {
-            $type   = $matches[1];
-            $params = array_map('trim', explode(',', $matches[2]));
-        }
-
-        if ($isNullable) {
-            $params[] = 'nullable';
-        }
-
-        $type = trim($type, '[]');
-
-        $handlers = array_merge($this->defaultCastHandlers, $this->castHandlers);
-
-        if (! isset($handlers[$type])) {
-            throw new InvalidArgumentException('No such handler for "' . $field . '". Invalid type: ' . $type);
-        }
-
-        $handler = $handlers[$type];
-
-        if (! is_subclass_of($handler, CastInterface::class)) {
-            throw new InvalidArgumentException(
-                'Invalid class type. It must implement CastInterface. class: ' . $handler
-            );
-        }
-
-        return $handler::$method($value, $params);
     }
 }
