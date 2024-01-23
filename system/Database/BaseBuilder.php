cd644df4de7bfa0b9380d443ecb0804e6dca9358
kenjis <kenji.uui@gmail.com>
2024-01-23 11:24:38 +0900

    docs: add @param

--- system/Database/BaseBuilder.php
+++ system/Database/BaseBuilder.php
@@ -1759,6 +1759,8 @@ class BaseBuilder
     /**
      * Compiles batch insert/update/upsert strings and runs the queries
      *
+     * @param '_deleteBatch'|'_insertBatch'|'_updateBatch'|'_upsertBatch' $renderMethod
+     *
      * @return false|int|string[] Number of rows inserted or FALSE on failure, SQL array when testMode
      *
      * @throws DatabaseException
