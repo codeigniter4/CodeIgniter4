c12aa141bba417f7ef2e88a33f88a8a681771947
kenjis <kenji.uui@gmail.com>
2024-01-26 16:37:36 +0900

    test: sort field data

--- tests/system/Database/Live/OCI8/GetFieldDataTest.php
+++ tests/system/Database/Live/OCI8/GetFieldDataTest.php
@@ -45,66 +45,71 @@ final class GetFieldDataTest extends AbstractGetFieldDataTest
         $idDefault = $data['id']->default;
         $this->assertMatchesRegularExpression('/"ORACLE"."ISEQ\$\$_[0-9]+".nextval/', $idDefault);
 
-        $this->assertJsonStringEqualsJsonString(
-            json_encode([
-                (object) [
-                    'name'       => 'id',
-                    'type'       => 'NUMBER',
-                    'max_length' => '11',
-                    'default'    => $idDefault, // The default value is not defined.
-                    // 'primary_key' => 1,
-                    'nullable' => false,
-                ],
-                (object) [
-                    'name'       => 'text_not_null',
-                    'type'       => 'VARCHAR2',
-                    'max_length' => '64',
-                    'default'    => null, // The default value is not defined.
-                    // 'primary_key' => 0,
-                    'nullable' => false,
-                ],
-                (object) [
-                    'name'       => 'text_null',
-                    'type'       => 'VARCHAR2',
-                    'max_length' => '64',
-                    'default'    => null, // The default value is not defined.
-                    // 'primary_key' => 0,
-                    'nullable' => true,
-                ],
-                (object) [
-                    'name'       => 'int_default_0',
-                    'type'       => 'NUMBER',
-                    'max_length' => '11',
-                    'default'    => '0 ', // int 0
-                    // 'primary_key' => 0,
-                    'nullable' => false,
-                ],
-                (object) [
-                    'name'       => 'text_default_null',
-                    'type'       => 'VARCHAR2',
-                    'max_length' => '64',
-                    'default'    => 'NULL ', // NULL value
-                    // 'primary_key' => 0,
-                    'nullable' => true,
-                ],
-                (object) [
-                    'name'       => 'text_default_text_null',
-                    'type'       => 'VARCHAR2',
-                    'max_length' => '64',
-                    'default'    => "'null' ", // string "null"
-                    // 'primary_key' => 0,
-                    'nullable' => false,
-                ],
-                (object) [
-                    'name'       => 'text_default_abc',
-                    'type'       => 'VARCHAR2',
-                    'max_length' => '64',
-                    'default'    => "'abc' ", // string "abc"
-                    // 'primary_key' => 0,
-                    'nullable' => false,
-                ],
-            ]),
-            json_encode($fields)
-        );
+        $expected = json_decode(json_encode([
+            (object) [
+                'name'       => 'id',
+                'type'       => 'NUMBER',
+                'max_length' => '11',
+                'default'    => $idDefault, // The default value is not defined.
+                // 'primary_key' => 1,
+                'nullable' => false,
+            ],
+            (object) [
+                'name'       => 'text_not_null',
+                'type'       => 'VARCHAR2',
+                'max_length' => '64',
+                'default'    => null, // The default value is not defined.
+                // 'primary_key' => 0,
+                'nullable' => false,
+            ],
+            (object) [
+                'name'       => 'text_null',
+                'type'       => 'VARCHAR2',
+                'max_length' => '64',
+                'default'    => null, // The default value is not defined.
+                // 'primary_key' => 0,
+                'nullable' => true,
+            ],
+            (object) [
+                'name'       => 'int_default_0',
+                'type'       => 'NUMBER',
+                'max_length' => '11',
+                'default'    => '0 ', // int 0
+                // 'primary_key' => 0,
+                'nullable' => false,
+            ],
+            (object) [
+                'name'       => 'text_default_null',
+                'type'       => 'VARCHAR2',
+                'max_length' => '64',
+                'default'    => 'NULL ', // NULL value
+                // 'primary_key' => 0,
+                'nullable' => true,
+            ],
+            (object) [
+                'name'       => 'text_default_text_null',
+                'type'       => 'VARCHAR2',
+                'max_length' => '64',
+                'default'    => "'null' ", // string "null"
+                // 'primary_key' => 0,
+                'nullable' => false,
+            ],
+            (object) [
+                'name'       => 'text_default_abc',
+                'type'       => 'VARCHAR2',
+                'max_length' => '64',
+                'default'    => "'abc' ", // string "abc"
+                // 'primary_key' => 0,
+                'nullable' => false,
+            ],
+        ]), true);
+        $names = array_column($expected, 'name');
+        array_multisort($names, SORT_ASC, $expected);
+
+        $fields = json_decode(json_encode($fields), true);
+        $names  = array_column($fields, 'name');
+        array_multisort($names, SORT_ASC, $fields);
+
+        $this->assertSame($expected, $fields);
     }
 }
