69d8a58d70585519321bf1fd5962f133b3d6129a
kenjis <kenji.uui@gmail.com>
2024-01-27 18:41:45 +0900

    feat: add base RuntimeException and LogicException

---
+++ system/Exceptions/RuntimeException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Exceptions;
+
+/**
+ * Exception thrown if an error which can only be found on runtime occurs.
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+}
