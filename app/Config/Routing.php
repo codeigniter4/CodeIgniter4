60042fba17267e56a8879e80498825c20ed30c87
kenjis <kenji.uui@gmail.com>
2024-01-31 20:47:33 +0900

    docs: fix doc comments
    I think there is no way to set 404 closure in the constructor.

--- app/Config/Routing.php
+++ app/Config/Routing.php
@@ -63,13 +63,12 @@ class Routing extends BaseRouting
 
     /**
      * Sets the class/method that should be called if routing doesn't
-     * find a match. It can be either a closure or the controller/method
-     * name exactly like a route is defined: Users::index
+     * find a match. It can be the controller/method name like: Users::index
      *
      * This setting is passed to the Router class and handled there.
      *
      * If you want to use a closure, you will have to set it in the
-     * class constructor or the routes file by calling:
+     * routes file by calling:
      *
      * $routes->set404Override(function() {
      *    // Do something here
