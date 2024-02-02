1cd91a991a6179c363a3a8fd8260e451da010f7c
kenjis <kenji.uui@gmail.com>
2024-02-02 09:46:31 +0900

    docs: fix test classname

--- user_guide_src/source/testing/controllers/002.php
+++ user_guide_src/source/testing/controllers/002.php
@@ -6,7 +6,7 @@ use CodeIgniter\Test\CIUnitTestCase;
 use CodeIgniter\Test\ControllerTestTrait;
 use CodeIgniter\Test\DatabaseTestTrait;
 
-class FooControllerTest extends CIUnitTestCase
+class ForumControllerTest extends CIUnitTestCase
 {
     use ControllerTestTrait;
     use DatabaseTestTrait;
