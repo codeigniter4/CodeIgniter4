cf7ab9dc1ef4e4bc6a21799f01a35f2abc7db769
kenjis <kenji.uui@gmail.com>
2024-02-10 11:03:27 +0900

    docs: add sample code to get URI segments

--- user_guide_src/source/incoming/routing/069.php
+++ user_guide_src/source/incoming/routing/069.php
@@ -4,7 +4,10 @@
 // Would execute the show404 method of the App\Errors class
 $routes->set404Override('App\Errors::show404');
 
-// Will display a custom view
+// Will display a custom view.
 $routes->set404Override(static function () {
-    echo view('my_errors/not_found.html');
+    // If you want to get the URI segments.
+    $segments = request()->getUri()->getSegments();
+
+    return view('my_errors/not_found.html');
 });
