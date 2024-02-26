29717da4f63047d07a81393fb55427ee7f564ad4
kenjis <kenji.uui@gmail.com>
2024-02-26 13:46:23 +0900

    docs: add @internal

--- system/HTTP/Response.php
+++ system/HTTP/Response.php
@@ -176,6 +176,7 @@ class Response extends Message implements ResponseInterface
      *
      * @return $this
      *
+     * @internal For testing purposes only.
      * @testTag only available to test code
      */
     public function pretend(bool $pretend = true)
