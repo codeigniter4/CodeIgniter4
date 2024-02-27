78edddf62c1c0aeea84a76cc5d8195aa37cfa9cd
Melroy van den Berg <melroy@melroy.org>
2024-02-27 02:48:06 +0100

    docs: Fix typo in Toolbar.php

--- app/Config/Toolbar.php
+++ app/Config/Toolbar.php
@@ -49,7 +49,7 @@ class Toolbar extends BaseConfig
      * Collect Var Data
      * --------------------------------------------------------------------------
      *
-     * If set to false var data from the views will not be colleted. Useful to
+     * If set to false var data from the views will not be collected. Useful to
      * avoid high memory usage when there are lots of data passed to the view.
      *
      * @var bool
