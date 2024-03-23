fa6cb5489eab7c662451953ea43aec210bd3af7e
kenjis <kenji.uui@gmail.com>
2024-03-23 17:14:35 +0900

    docs: improve readability of comments

--- user_guide_src/source/libraries/curlrequest/014.php
+++ user_guide_src/source/libraries/curlrequest/014.php
@@ -3,7 +3,7 @@
 $client->request('GET', 'http://example.com', ['allow_redirects' => true]);
 /*
  * Sets the following defaults:
- * 'max'       => 5, // Maximum number of redirects to follow before stopping
- * 'strict'    => true, // Ensure POST requests stay POST requests through redirects
- * 'protocols' => ['http', 'https'] // Restrict redirects to one or more protocols
+ *   'max'       => 5,                // Maximum number of redirects to follow before stopping
+ *   'strict'    => true,             // Ensure POST requests stay POST requests through redirects
+ *   'protocols' => ['http', 'https'] // Restrict redirects to one or more protocols
  */
