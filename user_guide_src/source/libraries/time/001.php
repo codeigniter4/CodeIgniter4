db187b2b5c0b6effaaabf2cd96df792dade31c36
kenjis <kenji.uui@gmail.com>
2024-03-28 08:13:59 +0900

    docs: add more sample code

--- user_guide_src/source/libraries/time/001.php
+++ user_guide_src/source/libraries/time/001.php
@@ -2,5 +2,7 @@
 
 use CodeIgniter\I18n\Time;
 
-$myTime = new Time('+3 week');
+$myTime = new Time('2024-01-01');
+$myTime = new Time('2024-01-01 12:00:00');
 $myTime = new Time('now');
+$myTime = new Time('+3 week');
