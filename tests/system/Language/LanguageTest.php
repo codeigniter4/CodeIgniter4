f4054e9749cf0fe809b9e5b76a7759a0aa96bfaf
kenjis <kenji.uui@gmail.com>
2024-03-30 17:42:37 +0900

    refactor: remove unneeded use statement

--- tests/system/Language/LanguageTest.php
+++ tests/system/Language/LanguageTest.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace CodeIgniter\Language;
 
-use CodeIgniter\Exceptions\InvalidArgumentException;
 use CodeIgniter\Test\CIUnitTestCase;
 use CodeIgniter\Test\Mock\MockLanguage;
 use Config\Services;
