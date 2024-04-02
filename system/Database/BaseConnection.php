4f4e805e2e99071942705a60644bf0d016a63aa4
kenjis <kenji.uui@gmail.com>
2024-04-02 09:06:16 +0900

    feat: escapeIdentifier() handles empty string

--- system/Database/BaseConnection.php
+++ system/Database/BaseConnection.php
@@ -1197,6 +1197,10 @@ abstract class BaseConnection implements ConnectionInterface
      */
     public function escapeIdentifier(string $item): string
     {
+        if ($item === '') {
+            return '';
+        }
+
         return $this->escapeChar
             . str_replace(
                 $this->escapeChar,
