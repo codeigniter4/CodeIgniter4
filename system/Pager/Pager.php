98ce544fb246a379ad1539c053fee9f9c2fe1357
kenjis <kenji.uui@gmail.com>
2024-04-03 19:03:55 +0900

    fix: Pager::only([]) does not work

--- system/Pager/Pager.php
+++ system/Pager/Pager.php
@@ -60,9 +60,9 @@ class Pager implements PagerInterface
     /**
      * List of only permitted queries
      *
-     * @var array
+     * @var list<string>|null
      */
-    protected $only = [];
+    protected $only;
 
     /**
      * Constructor.
@@ -276,7 +276,7 @@ class Pager implements PagerInterface
             $uri->addQuery($this->groups[$group]['pageSelector'], $page);
         }
 
-        if ($this->only) {
+        if ($this->only !== null) {
             $query = array_intersect_key($_GET, array_flip($this->only));
 
             if (! $segment) {
