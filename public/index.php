986c78cc94e8b2ca37d0a0990479ffda17e4a91b
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-04-14 04:19:26 +0700

    update comment in index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  *---------------------------------------------------------------
  */
 
-$minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
+$minPhpVersion = '8.1'; // If you update this, don't forget to update `system/spark.php`.
 if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
     $message = sprintf(
         'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
