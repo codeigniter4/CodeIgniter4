1434af4a6434b5ec050ffaaadec42494231b8895
ping-yee <611077101@mail.nknu.edu.tw>
2024-04-17 15:27:15 +0800

    feat: extend the code to implement the scenario without database.

--- system/Database/SQLSRV/Builder.php
+++ system/Database/SQLSRV/Builder.php
@@ -295,10 +295,15 @@ class Builder extends BaseBuilder
                 $database = $this->db->getDatabase();
                 $table    = $dbInfo[0];
 
-                if (isset($dbInfo[2])) {
-                    $database  = str_replace('"', '', $dbInfo[0]);
-                    $schema    = str_replace('"', '', $dbInfo[1]);
-                    $tableName = str_replace('"', '', $dbInfo[2]);
+                if (count($dbInfo) >= 2) {
+                    if (count($dbInfo) === 3) {
+                        $database  = str_replace('"', '', $dbInfo[0]);
+                        $schema    = str_replace('"', '', $dbInfo[1]);
+                        $tableName = str_replace('"', '', $dbInfo[2]);
+                    } else {
+                        $schema    = str_replace('"', '', $dbInfo[0]);
+                        $tableName = str_replace('"', '', $dbInfo[1]);
+                    }
 
                     return '"' . $database . '"."' . $schema . '"."' . str_replace('"', '', $tableName) . '"' . $alias;
                 }
--- tests/system/Database/Builder/FromTest.php
+++ tests/system/Database/Builder/FromTest.php
@@ -167,4 +167,18 @@ final class FromTest extends CIUnitTestCase
 
         $this->assertSame($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
     }
+
+    /**
+     * @see https://github.com/codeigniter4/CodeIgniter4/issues/8697
+     */
+    public function testConstructorWithMultipleSegmentTableWithoutDatabaseWithSQLSRV(): void
+    {
+        $this->db = new MockConnection(['DBDriver' => 'SQLSRV', 'database' => 'test', 'schema' => 'dbo']);
+
+        $builder = new SQLSRVBuilder('dbo.table', $this->db);
+
+        $expectedSQL = 'SELECT * FROM "test"."dbo"."table"';
+
+        $this->assertSame($expectedSQL, str_replace("\n", ' ', $builder->getCompiledSelect()));
+    }
 }
