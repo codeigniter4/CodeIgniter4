fa06a8bf0c12396d88d791a7d9f186493e20db38
kenjis <kenji.uui@gmail.com>
2024-04-22 08:53:27 +0900

    fix: incorrect Security exception message
    This is a server side misconfiguration, not a client error.

--- system/Language/en/Security.php
+++ system/Language/en/Security.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 // Security language settings
 return [
     'disallowedAction' => 'The action you requested is not allowed.',
+    'insecureCookie'   => 'Attempted to send a secure cookie over a non-secure connection.',
 
     // @deprecated
     'invalidSameSite' => 'The SameSite value must be None, Lax, Strict, or a blank string. Given: "{0}"',
