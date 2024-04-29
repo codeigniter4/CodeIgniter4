90155299fd354986e603abc44afccc57f011de45
Moritz Küttel <moritz_kuettel+github_mkuettel@fastmail.com>
2024-04-29 16:15:04 +0200

    tests(transaction): add more test cases for when transaction nesting is enabled, plus an extreme example

--- tests/system/Database/Live/TransactionDBDebugTrueTest.php
+++ tests/system/Database/Live/TransactionDBDebugTrueTest.php
@@ -37,6 +37,7 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
     {
         // Reset connection instance.
         $this->db = Database::connect($this->DBGroup, false);
+        $this->assertFalse($this->db->enableNestedTransactions);
 
         parent::setUp();
     }
@@ -235,11 +236,14 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         $this->dontSeeInDatabase('job', ['name' => 'Grocery Sales']);
     }
 
-    public function testNestedTransactions(): void
+    /**
+     * @depends testTransStart
+     */
+    public function testNestedTransactionsWhenDisabled(): void
     {
         $builder = $this->db->table('job');
 
-        $this->db->transStart() or $this->fail('Failed to start transaction');
+        $this->db->transStart() || $this->fail('Failed to start transaction');
 
         $jobData = [
             'name'        => 'Grocery Sales',
@@ -247,7 +251,7 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transStart() or $this->fail('Failed to start inner transaction');
+        $this->db->transStart() || $this->fail('Failed to start inner transaction');
 
         $jobData = [
             'name'        => 'Comedian',
@@ -255,20 +259,22 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transComplete() or $this->fail('Failed to complete inner transaction');
+        $this->db->transComplete() || $this->fail('Failed to complete inner transaction');
 
-        $this->db->transComplete() or $this->fail('Failed to complete outer transaction');
+        $this->db->transComplete() || $this->fail('Failed to complete outer transaction');
 
         $this->seeInDatabase('job', ['name' => 'Grocery Sales']);
         $this->seeInDatabase('job', ['name' => 'Comedian']);
     }
 
-
-    public function testNestedTransactionsRollbackOuter(): void
+    /**
+     * @depends testTransStart
+     */
+    public function testNestedTransactionsWhenDisabledRollbackOuter(): void
     {
         $builder = $this->db->table('job');
 
-        $this->db->transStart() or $this->fail('Failed to start transaction');
+        $this->db->transStart() || $this->fail('Failed to start transaction');
 
         $jobData = [
             'name'        => 'Grocery Sales',
@@ -276,7 +282,7 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transStart() or $this->fail('Failed to start inner transaction');
+        $this->db->transStart() || $this->fail('Failed to start inner transaction');
 
         $jobData = [
             'name'        => 'Comedian',
@@ -284,20 +290,23 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transComplete() or $this->fail('Failed to complete inner transaction');
+        $this->db->transComplete() || $this->fail('Failed to complete inner transaction');
 
-        $this->db->transRollback() or $this->fail('Failed to rollback outer transaction');
+        $this->db->transRollback() || $this->fail('Failed to rollback outer transaction');
 
         $this->dontSeeInDatabase('job', ['name' => 'Grocery Sales']);
         $this->dontSeeInDatabase('job', ['name' => 'Comedian']);
     }
 
-
-    public function testNestedTransactionsRollbackInner(): void
+    /**
+     * @depends testTransStart
+     */
+    public function testNestedTransactionsWhenDisabledRollbackOnInnerHasNoEffect()
     {
+        $this->db->transStrict(false); // TODO: this only works when strict is disabled
         $builder = $this->db->table('job');
 
-        $this->db->transStart() or $this->fail('Failed to start transaction');
+        $this->db->transStart() || $this->fail('Failed to start transaction');
 
         $jobData = [
             'name'        => 'Grocery Sales',
@@ -305,7 +314,106 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transStart() or $this->fail('Failed to start inner transaction');
+        $this->db->transStart() || $this->fail('Failed to start inner transaction');
+
+        $jobData = [
+            'name'        => 'Comedian',
+            'description' => 'Theres something in your teeth',
+        ];
+
+        $this->db->query('invalid query to throw a Database Exception');
+        $builder->insert($jobData);
+
+        $this->db->transRollback() || $this->fail('Failed to rollback inner transaction');
+
+        $this->db->transComplete() || $this->fail('Failed to complete outer transaction');
+
+        $this->seeInDatabase('job', ['name' => 'Grocery Sales']);
+        $this->seeInDatabase('job', ['name' => 'Comedian']);
+    }
+
+    /**
+     * @depends testTransStart
+     */
+    public function testNestedTransactionsWhenDisabledAlwaysRollsBackEverythingOnDatabaseException(): void
+    {
+        $builder = $this->db->table('job');
+
+        $this->db->transStart() || $this->fail('Failed to start transaction');
+
+        $jobData = [
+            'name'        => 'Grocery Sales',
+            'description' => 'Fresh!',
+        ];
+        $builder->insert($jobData);
+
+        $this->db->transStart() || $this->fail('Failed to start inner transaction');
+
+        $jobData = [
+            'name'        => 'Comedian',
+            'description' => 'Theres something in your teeth',
+        ];
+
+        $this->db->query('invalid query to throw a Database Exception');
+        $builder->insert($jobData);
+
+        $this->db->transComplete() && $this->fail('Should not have completed inner transaction, but rolled it back');
+
+        $this->db->transComplete() && $this->fail('Should not have completed outer transaction, but rolled it back');
+
+        $this->dontSeeInDatabase('job', ['name' => 'Grocery Sales']);
+        $this->dontSeeInDatabase('job', ['name' => 'Comedian']);
+    }
+
+    /**
+     * @depends testNestedTransactionsWhenDisabled
+     */
+    public function testNestedTransactionsWhenEnabled()
+    {
+        $this->db->transNested(true);
+        if (! $this->db->enableNestedTransactions) {
+            $this->markTestSkipped('Nested transactions are not supported for this driver.');
+        }
+
+        $this->testNestedTransactionsWhenDisabled();
+    }
+
+    /**
+     * @depends testNestedTransactionsWhenDisabledRollbackOuter
+     * @depends testNestedTransactionsWhenEnabled
+     */
+    public function testNestedTransactionsWhenEnabledRollbackOuter(): void
+    {
+        $this->db->transNested(true);
+        if (! $this->db->enableNestedTransactions) {
+            $this->markTestSkipped('Nested transactions are not supported for this driver.');
+        }
+
+        $this->testNestedTransactionsWhenDisabledRollbackOuter();
+    }
+
+    /**
+     * @depends testNestedTransactionsWhenEnabled
+     */
+    public function testNestedTransactionsRollbackInner(): void
+    {
+        $this->db->transNested(true);
+        $this->db->transStrict(false); // TODO: this only works when strict is disabled
+        if (! $this->db->enableNestedTransactions) {
+            $this->markTestSkipped('Nested transactions are not supported for this driver.');
+        }
+
+        $builder = $this->db->table('job');
+
+        $this->db->transStart() || $this->fail('Failed to start transaction');
+
+        $jobData = [
+            'name'        => 'Grocery Sales',
+            'description' => 'Fresh!',
+        ];
+        $builder->insert($jobData);
+
+        $this->db->transStart() || $this->fail('Failed to start inner transaction');
 
         $jobData = [
             'name'        => 'Comedian',
@@ -313,11 +421,115 @@ class TransactionDBDebugTrueTest extends CIUnitTestCase
         ];
         $builder->insert($jobData);
 
-        $this->db->transRollback() or $this->fail('Failed to rollback inner transaction');
+        $this->db->transRollback() || $this->fail('Failed to rollback inner transaction');
 
-        $this->db->transComplete() or $this->fail('Failed to complete outer transaction');
+        $this->db->transComplete() || $this->fail('Failed to complete outer transaction');
 
         $this->seeInDatabase('job', ['name' => 'Grocery Sales']);
         $this->dontSeeInDatabase('job', ['name' => 'Comedian']);
+    }
+
+    /**
+     * @depends testNestedTransactionsWhenEnabled
+     */
+    public function testHeavilyNestedTransactionsWhenEnabled()
+    {
+        $this->db->transNested(true);
+        $this->db->transStrict(false); // TODO: this only works when strict is disabled
+        if (! $this->db->enableNestedTransactions) {
+            $this->markTestSkipped('Nested transactions are not supported for this driver.');
+        }
+
+        $sees     = [];
+        $dontSees = [];
+        $db       = $this->db;
+        $insert   = static function (string $data, bool $expectCommitted = true) use ($db, &$sees, &$dontSees) {
+            $builder = $db->table('job');
+            $jobData = [
+                'name'        => $data,
+                'description' => 'this should have been rolled back',
+            ];
+            $builder->insert($jobData);
+
+            if ($expectCommitted) {
+                $sees[] = $data;
+            } else {
+                $dontSees[] = $data;
+            }
+        };
+
+        $transaction = static function (string $data_to_insert, bool $complete = true, bool $expect_committed = true, ?callable $inner = null) use ($db, $insert) {
+            $db->transStart();
+            $insert($data_to_insert . '_before');
+            is_null($inner) || $inner();
+            $insert($data_to_insert . '_after');
+            if ($complete) {
+                $db->transComplete();
+            } else {
+                $db->transRollback();
+            }
+        };
+
+        $nest_transaction_times = static function (int $times, ...$params) use ($transaction) {
+            $extend = $params[0];
+
+            for ($i = 0; $i < $times; $i++) {
+                $params[0] .= '_' . $extend;
+                $params[3] = $transaction(...$params);
+            }
+        };
+
+        $transaction('outer', true, true, static function () use ($transaction, $nest_transaction_times) {
+            $transaction('inner_1', false, false, static function () use ($transaction) {
+                $transaction('inner_1_1', true, false);
+                $transaction('inner_1_2', false, false);
+                $transaction('inner_1_3', true, false, static function () use ($transaction) {
+                    $transaction('inner_1_3_1', true, false);
+                    $transaction('inner_1_3_2', false, false);
+                });
+                $transaction('inner_1_5', true, false);
+            });
+            $transaction('inner_2', true, true, static function () use ($transaction) {
+                $transaction('inner_2_1', true, true);
+                $transaction('inner_2_2', true, true, static function () use ($transaction) {
+                    $transaction('inner_2_2_1', true, true);
+                    $transaction('inner_2_2_2', false, false);
+                    $transaction('inner_2_2_2', true, true, static function () use ($transaction) {
+                        $transaction('inner_2_2_2_1', true, true);
+                        $transaction('inner_2_2_2_2', false, false);
+                    });
+                });
+
+                $transaction('inner_2_3', false, false);
+
+                $transaction('inner_2_4', false, false, static function () use ($transaction) {
+                    $transaction('inner_2_4_1', true, false);
+                    $transaction('inner_2_4_2', false, false);
+                });
+
+                $transaction('inner_2_5', true, false);
+            });
+
+            $nest_transaction_times(10, 'inner_3', true, true, static function () use ($transaction) {
+                $transaction('inner_3_x10_1', true, true);
+                $transaction('inner_3_x10_2', false, false);
+            });
+
+            $nest_transaction_times(10, 'inner_4', false, false, static function () use ($transaction) {
+                $transaction('inner_4_x10_1', true, false);
+                $transaction('inner_4_x10_2', false, false);
+            });
+        });
+
+        $this->assertNotEmpty($sees);
+        $this->assertNotEmpty($dontSees);
+
+        foreach ($sees as $see) {
+            $this->seeInDatabase('job', $see);
+        }
+
+        foreach ($dontSees as $see) {
+            $this->dontSeeInDatabase('job', $see);
+        }
     }
 }
