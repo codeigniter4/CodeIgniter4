53fccdc831f3afe7ac9045230f4afb4a611cb256
kenjis <kenji.uui@gmail.com>
2024-05-08 10:17:03 +0900

    docs: update sample config

--- user_guide_src/source/general/errors/013.php
+++ user_guide_src/source/general/errors/013.php
@@ -6,7 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Logger extends BaseConfig
 {
-    // .. other properties
-
-    public $threshold = 5; // originally 4 but changed to 5 to log the warnings from the deprecations
+    // ...
+    // This must contain the log level (5 for LogLevel::WARNING) corresponding to $deprecationLogLevel.
+    public $threshold = (ENVIRONMENT === 'production') ? 4 : 9;
+    // ...
 }
