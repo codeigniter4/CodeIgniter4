73d1f7b0f45827a49d62f209a444b4a9b1b5f85b
Glenn <1964713+pyromanci@users.noreply.github.com>
2024-05-14 22:53:34 -0400

    style: Forrgot to run CS before last commit.

--- tests/_support/Test/AfterAutoloadModule/Config/Services.php
+++ tests/_support/Test/AfterAutoloadModule/Config/Services.php
@@ -1,8 +1,18 @@
 <?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace AfterAutoloadModule\Config;
 
-use CodeIgniter\Config\BaseService;
 use AfterAutoloadModule\Test;
+use CodeIgniter\Config\BaseService;
 
 /**
  * This file is part of CodeIgniter 4 framework.
@@ -14,7 +24,8 @@ use AfterAutoloadModule\Test;
  */
 class Services extends BaseService
 {
-    public static function test(bool $getShared = true) {
+    public static function test(bool $getShared = true)
+    {
         if ($getShared) {
             return static::getSharedInstance('test');
         }
--- tests/_support/Test/AfterAutoloadModule/Test.php
+++ tests/_support/Test/AfterAutoloadModule/Test.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
 namespace AfterAutoloadModule;
 
 /**
@@ -12,5 +22,5 @@ namespace AfterAutoloadModule;
 class Test
 {
     private string $private = 'secret';
-    public string $public = 'public';
+    public string $public   = 'public';
 }
