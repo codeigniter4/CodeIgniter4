412d0d4740a7d0ab71462fc4c3c6fa8a282df42c
kenjis <action@github.com>
2024-06-10 07:45:42 +0000

    Update User Guide

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.5.1',
+    VERSION: '4.5.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
