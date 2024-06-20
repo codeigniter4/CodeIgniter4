542df34d0d611ae32aa32b53867e3b3bb8c4e623
ducng99 <tom@tomng.dev>
2024-06-21 01:12:23 +1200

    test: add test for foundRows db config

---
+++ tests/system/Database/Live/MySQLi/FoundRowsTest.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace CodeIgniter\Database\Live\MySQLi;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use CodeIgniter\Test\DatabaseTestTrait;
+use Config\Database;
+use PHPUnit\Framework\Attributes\Group;
+use Tests\Support\Database\Seeds\CITestSeeder;
+
+/**
+ * @internal
+ */
+#[Group('DatabaseLive')]
+final class FoundRowsTest extends CIUnitTestCase
+{
+    use DatabaseTestTrait;
+
+    /**
+     * @var array<string,mixed>
+     */
+    private $tests;
+
+    protected $refresh = true;
+    protected $seed    = CITestSeeder::class;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $config = config('Database');
+
+        $this->tests = $config->tests;
+    }
+
+    public function testEnableFoundRows(): void
+    {
+        $this->tests['foundRows'] = true;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $this->assertTrue($db1->foundRows);
+    }
+
+    public function testDisableFoundRows(): void
+    {
+        $this->tests['foundRows'] = false;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $this->assertFalse($db1->foundRows);
+    }
+
+    public function testAffectedRowsAfterEnableFoundRowsWithNoChange(): void
+    {
+        $this->tests['foundRows'] = true;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('country', 'US')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 2);
+    }
+
+    public function testAffectedRowsAfterDisableFoundRowsWithNoChange(): void
+    {
+        $this->tests['foundRows'] = false;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('country', 'US')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 0);
+    }
+
+    public function testAffectedRowsAfterEnableFoundRowsWithChange(): void
+    {
+        $this->tests['foundRows'] = true;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('country', 'NZ')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 2);
+    }
+
+    public function testAffectedRowsAfterDisableFoundRowsWithChange(): void
+    {
+        $this->tests['foundRows'] = false;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('country', 'NZ')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 2);
+    }
+
+    public function testAffectedRowsAfterEnableFoundRowsWithPartialChange(): void
+    {
+        $this->tests['foundRows'] = true;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('name', 'Derek Jones')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 2);
+    }
+
+    public function testAffectedRowsAfterDisableFoundRowsWithPartialChange(): void
+    {
+        $this->tests['foundRows'] = false;
+
+        $db1 = Database::connect($this->tests);
+
+        if ($db1->DBDriver !== 'MySQLi') {
+            $this->markTestSkipped('Only MySQLi can complete this test.');
+        }
+
+        $db1->table('db_user')
+            ->set('name', 'Derek Jones')
+            ->where('country', 'US')
+            ->update();
+
+        $affectedRows = $db1->affectedRows();
+
+        $this->assertSame($affectedRows, 1);
+    }
+}
