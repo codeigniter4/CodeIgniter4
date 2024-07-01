e7ecd55fd4ae1b2cbaa58ecb1e051db8f8bb82d9
Aselsan <syamsuddine9@gmail.com>
2024-07-02 03:27:49 +0700

    Update PHP doc block

--- system/Validation/Rules.php
+++ system/Validation/Rules.php
@@ -445,8 +445,8 @@ class Rules
      *     required_with_value[citizenship,1]
      *
      * @param string|null $str
-     * @param string|null $field with expected value that we should check if present
-     * @param array       $data  Complete list of field from the form
+     * @param string|null $fieldWithValue that we should check if present
+     * @param array       $data           Complete list of field from the form
      */
     public function required_with_value($str = null, ?string $fieldWithValue = null, array $data = []): bool
     {
