936f1f61fbc30c0ae686eb1c6c23163465a5a64c
Aselsan <syamsuddine9@gmail.com>
2024-07-04 05:44:50 +0700

    adjust `processPermitEmpty` to validate `required_with_value` rule condition correctly.

--- system/Validation/Validation.php
+++ system/Validation/Validation.php
@@ -437,7 +437,7 @@ class Validation implements ValidationInterface
                         $rule  = $match[1];
                         $param = $match[2];
 
-                        if (! in_array($rule, ['required_with', 'required_without'], true)) {
+                        if (! in_array($rule, ['required_with', 'required_without', 'required_with_value'], true)) {
                             continue;
                         }
 
