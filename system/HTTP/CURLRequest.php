a9b42fedbec77770b135d363973c3dcf8b07f4b4
Mattias Sandstrom <1013635+tangix@users.noreply.github.com>
2024-07-04 07:17:07 +0200

    Allow string as parameter to version
    As indicated in the docs.

--- system/HTTP/CURLRequest.php
+++ system/HTTP/CURLRequest.php
@@ -651,11 +651,11 @@ class CURLRequest extends OutgoingRequest
 
         // version
         if (! empty($config['version'])) {
-            if ($config['version'] === 1.0) {
+            if ($config['version'] === 1.0 || $config['version'] === "1.0") {
                 $curlOptions[CURLOPT_HTTP_VERSION] = CURL_HTTP_VERSION_1_0;
-            } elseif ($config['version'] === 1.1) {
+            } elseif ($config['version'] === 1.1 || $config['version'] === "1.1") {
                 $curlOptions[CURLOPT_HTTP_VERSION] = CURL_HTTP_VERSION_1_1;
-            } elseif ($config['version'] === 2.0) {
+            } elseif ($config['version'] === 2.0 || $config['version'] === "2.0") {
                 $curlOptions[CURLOPT_HTTP_VERSION] = CURL_HTTP_VERSION_2_0;
             }
         }
