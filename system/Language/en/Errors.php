1e06ee5078d87fde5f71ddf6ae352be59cb22599
kenjis <kenji.uui@gmail.com>
2024-07-18 05:25:58 +0900

    docs: fix by proofreading
    Co-authored-by: John Paul E. Balandan, CPA <paulbalandan@gmail.com>

--- system/Language/en/Errors.php
+++ system/Language/en/Errors.php
@@ -16,7 +16,7 @@ return [
     'pageNotFound'    => '404 - Page Not Found',
     'sorryCannotFind' => 'Sorry! Cannot seem to find the page you were looking for.',
     'badRequest'      => '400 - Bad Request',
-    'sorryBadRequest' => 'Sorry! Something wrong with your request.',
+    'sorryBadRequest' => 'Sorry! Something is wrong with your request.',
     'whoops'          => 'Whoops!',
     'weHitASnag'      => 'We seem to have hit a snag. Please try again later...',
 ];
