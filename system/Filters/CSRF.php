ed35c36128c5db13288488ea90ccc0d66d497879
kenjis <kenji.uui@gmail.com>
2024-08-10 10:24:10 +0900

    docs: update PHPDoc comments

--- system/Filters/CSRF.php
+++ system/Filters/CSRF.php
@@ -30,14 +30,7 @@ use CodeIgniter\Security\Exceptions\SecurityException;
 class CSRF implements FilterInterface
 {
     /**
-     * Do whatever processing this filter needs to do.
-     * By default it should not return anything during
-     * normal execution. However, when an abnormal state
-     * is found, it should return an instance of
-     * CodeIgniter\HTTP\Response. If it does, script
-     * execution will end and that Response will be
-     * sent back to the client, allowing for error pages,
-     * redirects, etc.
+     * CSRF verification.
      *
      * @param list<string>|null $arguments
      *
