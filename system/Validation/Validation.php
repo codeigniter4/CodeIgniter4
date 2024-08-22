99f41ee353215dfc32d3126e10508b3fc0d08e57
kenjis <kenji.uui@gmail.com>
2024-08-23 08:26:35 +0900

    fix: TypeError when there is numeric field name
    TypeError: str_contains(): Argument #1 ($haystack) must be of type string, int given

--- system/Validation/Validation.php
+++ system/Validation/Validation.php
@@ -163,6 +163,9 @@ class Validation implements ValidationInterface
         // Run through each rule. If we have any field set for
         // this rule, then we need to run them through!
         foreach ($this->rules as $field => $setup) {
+            //  An array key might be int.
+            $field = (string) $field;
+
             $rules = $setup['rules'];
 
             if (is_string($rules)) {
