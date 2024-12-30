f551dbd0adc34f94d0131c095497242dfdec5b7c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2024-12-30 20:59:00 +0700

    fix: handle namespaced helper found on Common helper

--- system/Common.php
+++ system/Common.php
@@ -598,7 +598,7 @@ if (! function_exists('helper')) {
             if (str_contains($filename, '\\')) {
                 $path = $loader->locateFile($filename, 'Helpers');
 
-                if ($path !== '') {
+                if ($path === false) {
                     throw FileNotFoundException::forFileNotFound($filename);
                 }
 
