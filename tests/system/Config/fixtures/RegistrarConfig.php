f29a8c0c0359e237c60ebad2445c0da6e58b4d29
neznaika0 <ozornick.ks@gmail.com>
2025-02-08 01:55:39 +0300

    tests: Update Registrar tests

--- tests/system/Config/fixtures/RegistrarConfig.php
+++ tests/system/Config/fixtures/RegistrarConfig.php
@@ -17,4 +17,22 @@ class RegistrarConfig extends CodeIgniter\Config\BaseConfig
     public $bar = [
         'baz',
     ];
+
+    /**
+     * @var array<string, array<string, mixed>>
+     */
+    public $cars = [
+        'Sedans' => [
+            'Toyota' => [
+                'year'  => 2018,
+                'color' => 'silver',
+            ],
+        ],
+        'Trucks' => [
+            'Volvo' => [
+                'year'  => 2019,
+                'color' => 'blue',
+            ],
+        ],
+    ];
 }
