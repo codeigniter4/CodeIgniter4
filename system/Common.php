4a0a07c834393bb4baf8ccb33191a0ee8f49a46d
Denny Septian Panggabean <xamidimura@gmail.com>
2025-02-27 16:53:22 +0700

    refactor: using generics template

--- system/Common.php
+++ system/Common.php
@@ -371,9 +371,11 @@ if (! function_exists('env')) {
      * retrieving values set from the .env file for
      * use in config files.
      *
-     * @param array<int|string, mixed>|bool|float|int|string|null $default
+     * @template T
      *
-     * @return array<int|string, mixed>|bool|float|int|string|null
+     * @param T $default
+     *
+     * @return T
      */
     function env(string $key, $default = null)
     {
