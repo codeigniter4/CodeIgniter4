fd3c076217a92937d209171b78f24a374761bcff
Denny Septian Panggabean <xamidimura@gmail.com>
2025-05-16 21:56:14 +0700

    fix: compatible function parameter

--- tests/system/HTTP/CURLRequestShareOptionsTest.php
+++ tests/system/HTTP/CURLRequestShareOptionsTest.php
@@ -28,13 +28,18 @@ use PHPUnit\Framework\Attributes\Group;
 #[Group('Others')]
 final class CURLRequestShareOptionsTest extends CURLRequestTest
 {
-    protected function getRequest(array $options = []): MockCURLRequest
+    protected function getRequest(array $options = [], bool $emptyShareConnection = false): MockCURLRequest
     {
         $uri = isset($options['baseURI']) ? new URI($options['baseURI']) : new URI();
         $app = new App();
 
         $config               = new ConfigCURLRequest();
         $config->shareOptions = true;
+
+        if ($emptyShareConnection) {
+            $config->shareConnection = [];
+        }
+
         Factories::injectMock('config', 'CURLRequest', $config);
 
         return new MockCURLRequest(($app), $uri, new Response($app), $options);
