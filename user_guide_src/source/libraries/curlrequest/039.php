9b9a9d2fd42e0bc11f781602173cecc68121e2e8
Denny Septian Panggabean <xamidimura@gmail.com>
2025-05-16 22:34:05 +0700

    docs: sample

--- user_guide_src/source/libraries/curlrequest/039.php
+++ user_guide_src/source/libraries/curlrequest/039.php
@@ -6,9 +6,21 @@ use CodeIgniter\Config\BaseConfig;
 
 class CURLRequest extends BaseConfig
 {
-    // ...
+    /**
+     * --------------------------------------------------------------------------
+     * CURLRequest Share Connection
+     * --------------------------------------------------------------------------
+     *
+     * Whether share connection between requests.
+     *
+     * @var list<int>
+     *
+     * @see https://www.php.net/manual/en/curl.constants.php#constant.curl-lock-data-connect
+     */
     public array $shareConnection = [
         CURL_LOCK_DATA_CONNECT,
         CURL_LOCK_DATA_DNS,
     ];
+
+    // ...
 }
