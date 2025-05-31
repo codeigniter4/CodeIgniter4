63ab555933485a938091ac95f2f1fb9f5a6fe3a5
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2025-05-31 15:04:47 +0800

    refactor: fix various phpstan errors in Log component (#9581)
    * refactor Logs collector
    
    * refactor Logger config
    
    * refactor Logger
    
    * refactor BaseHandler
    
    * refactor FileHandler
    
    * refactor Mock FileHandler
    
    * refactor ErrorlogHandler
    
    * refactor ChromeLoggerHandler
    
    * regenerate baseline
    
    * Fix per review

--- system/Log/Handlers/ErrorlogHandler.php
+++ system/Log/Handlers/ErrorlogHandler.php
@@ -38,14 +38,14 @@ class ErrorlogHandler extends BaseHandler
      * Says where the error should go. Currently supported are
      * 0 (`TYPE_OS`) and 4 (`TYPE_SAPI`).
      *
-     * @var int
+     * @var 0|4
      */
     protected $messageType = 0;
 
     /**
      * Constructor.
      *
-     * @param list<mixed> $config
+     * @param array{handles?: list<string>, messageType?: int} $config
      */
     public function __construct(array $config = [])
     {
@@ -78,6 +78,8 @@ class ErrorlogHandler extends BaseHandler
 
     /**
      * Extracted call to `error_log()` in order to be tested.
+     *
+     * @param 0|4 $messageType
      *
      * @codeCoverageIgnore
      */
