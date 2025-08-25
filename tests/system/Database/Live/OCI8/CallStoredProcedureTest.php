66d00ee7f0395825d39b720b373e81724e67ef99
Abdul Malik Ikhsan <samsonasik@gmail.com>
2025-08-25 23:38:32 +0700

    refactor: apply rector latest dev-main

--- tests/system/Database/Live/OCI8/CallStoredProcedureTest.php
+++ tests/system/Database/Live/OCI8/CallStoredProcedureTest.php
@@ -55,7 +55,7 @@ final class CallStoredProcedureTest extends CIUnitTestCase
             ],
         ]);
 
-        $this->assertSame('7', $result);
+        $this->assertSame(7, $result);
     }
 
     public function testCallStoredProcedure(): void
@@ -77,7 +77,7 @@ final class CallStoredProcedureTest extends CIUnitTestCase
             ],
         ]);
 
-        $this->assertSame('7', $result);
+        $this->assertSame(7, $result);
     }
 
     public function testCallStoredProcedureForCursor(): void
