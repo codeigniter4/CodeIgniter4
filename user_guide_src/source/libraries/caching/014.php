42c27b0b306771f6b29f9641b14c9d20f63fedd4
Denny Septian Panggabean <97607754+ddevsr@users.noreply.github.com>
2025-11-17 08:43:15 +0700

    Update user_guide_src/source/libraries/caching/014.php
    Co-authored-by: Michal Sniatala <michal@sniatala.pl>

--- user_guide_src/source/libraries/caching/014.php
+++ user_guide_src/source/libraries/caching/014.php
@@ -12,7 +12,7 @@ class Cache extends BaseConfig
         'host'       => '127.0.0.1',
         'password'   => null,
         'port'       => 6379,
-        'async'      => false,
+        'async'      => false, // specific to Predis and ignored by the native Redis extension
         'persistent' => false,
         'timeout'    => 0,
         'database'   => 0,
