e2ef9a935622aa3892ab04231c203538c62e96a2
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2025-12-24 04:56:44 +0330

    fix: suppress RemoveExtraParametersRector false-positive for is_cli override

--- rector.php
+++ rector.php
@@ -31,6 +31,7 @@ use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
 use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
 use Rector\Php70\Rector\FuncCall\RandomFunctionRector;
+use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\YieldDataProviderRector;
@@ -105,6 +106,10 @@ return RectorConfig::configure()
             // @TODO remove if deprecated $config is removed
             __DIR__ . '/system/HTTP/Request.php',
             __DIR__ . '/system/HTTP/Response.php',
+        ],
+
+        RemoveExtraParametersRector::class => [
+            __DIR__ . '/tests/system/Debug/ToolbarTest.php',
         ],
 
         // check on constant compare
