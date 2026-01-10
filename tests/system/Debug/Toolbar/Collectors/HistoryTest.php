85b0dd275742cb5e9b21833c6fac67704dbe4dee
michalsn <michal@sniatala.pl>
2026-01-10 12:39:55 +0100

    use F instead of f in sprintf in tests
    Co-authored-by: neznaika0 <ozornick.ks@gmail.com>

--- tests/system/Debug/Toolbar/Collectors/HistoryTest.php
+++ tests/system/Debug/Toolbar/Collectors/HistoryTest.php
@@ -34,7 +34,7 @@ final class HistoryTest extends CIUnitTestCase
     {
         parent::setUp();
 
-        $this->time = (float) sprintf('%.6f', microtime(true));
+        $this->time = (float) sprintf('%.6F', microtime(true));
     }
 
     protected function tearDown(): void
@@ -63,9 +63,9 @@ final class HistoryTest extends CIUnitTestCase
 
         // create 20 dummy debugbar json files
         for ($i = 0; $i < 20; $i++) {
-            $path = str_replace((string) $time, sprintf('%.6f', $time - self::STEP), $path);
+            $path = str_replace((string) $time, sprintf('%.6F', $time - self::STEP), $path);
             file_put_contents($path, json_encode($dummyData));
-            $time = sprintf('%.6f', $time - self::STEP);
+            $time = sprintf('%.6F', $time - self::STEP);
         }
     }
 
@@ -76,7 +76,7 @@ final class HistoryTest extends CIUnitTestCase
         // test dir is now populated with json
         $this->createDummyDebugbarJson();
 
-        $activeRowTime = $time = sprintf('%.6f', $time - self::STEP);
+        $activeRowTime = $time = sprintf('%.6F', $time - self::STEP);
 
         $history = new History();
         $history->setFiles($time, 20);
@@ -85,14 +85,14 @@ final class HistoryTest extends CIUnitTestCase
         $this->assertNotEmpty($history->display()['files'], 'Dummy Debugbar data is empty');
 
         foreach ($history->display()['files'] as $request) {
-            $this->assertSame($request['time'], sprintf('%.6f', $time));
+            $this->assertSame($request['time'], sprintf('%.6F', $time));
             $this->assertSame(
                 $request['datetime'],
                 DateTime::createFromFormat('U.u', $time)->format('Y-m-d H:i:s.u'),
             );
             $this->assertSame($request['active'], ($time === $activeRowTime));
 
-            $time = sprintf('%.6f', $time - self::STEP);
+            $time = sprintf('%.6F', $time - self::STEP);
         }
     }
 }
