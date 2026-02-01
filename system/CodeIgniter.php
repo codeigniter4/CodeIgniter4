62ac7d39ed23734bcdb1a0bd908d61e97b1259ef
John Paul E. Balandan, CPA <paulbalandan@gmail.com>
2026-02-02 01:05:34 +0800

    Prep for 4.6.5 release (#9911)

--- system/CodeIgniter.php
+++ system/CodeIgniter.php
@@ -55,7 +55,7 @@ class CodeIgniter
     /**
      * The current version of CodeIgniter Framework
      */
-    public const CI_VERSION = '4.6.4';
+    public const CI_VERSION = '4.6.5';
 
     /**
      * App startup time.
