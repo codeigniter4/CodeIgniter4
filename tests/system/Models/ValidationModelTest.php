e94aa50fbdf1f98c42092a43a46362ec00f12b79
Adi Prasetyo <adiprsa@gmail.com>
2026-02-27 09:23:38 +0700

    refactor: fix phpstan no type specified ValidationModelTest

--- tests/system/Models/ValidationModelTest.php
+++ tests/system/Models/ValidationModelTest.php
@@ -243,6 +243,7 @@ final class ValidationModelTest extends LiveModelTestCase
     public function testValidationWithGroupName(): void
     {
         $config = new class () extends Validation {
+            /** @var array<string, string|array<int, string>> @grouptest */
             public $grouptest = [
                 'id'   => 'is_natural_no_zero',
                 'name' => [
