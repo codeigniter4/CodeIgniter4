88b9756f88a549a1b1b59902bdcc075b2e83cfb8
michalsn <michal@sniatala.pl>
2026-02-28 10:43:30 +0100

    add a comment to the code example

--- user_guide_src/source/models/model/065.php
+++ user_guide_src/source/models/model/065.php
@@ -5,3 +5,15 @@ $user = $userModel->firstOrInsert(
     ['email' => 'john@example.com'],
     ['name' => 'John Doe', 'country' => 'US'],
 );
+
+// The above will trigger:
+//
+// 1) First it tries to find the record:
+// SELECT * FROM `users` WHERE `email` = 'john@example.com' LIMIT 1;
+//
+// 2) If no result is found, it inserts a new record:
+// INSERT INTO `users` (`email`, `name`, `country`)
+// VALUES ('john@example.com', 'John Doe', 'US');
+//
+// 3) Then it returns the found or newly created entity/row,
+// or false if something went wrong
