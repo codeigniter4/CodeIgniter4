13cdf5389bee905c3ce1dc2b6223991a8da2539c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-10 10:53:48 +0700

    chore: Add Structarmed to QA

---
+++ structarmed.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of CodeIgniter 4 framework.
+ *
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use Boundwize\StructArmed\Architecture;
+use Boundwize\StructArmed\Preset\Preset;
+use Boundwize\StructArmed\Preset\Presets\Psr4Preset;
+
+return Architecture::define()
+    ->skip([
+        Psr4Preset::CLASSES_MUST_MATCH_COMPOSER => [
+            __DIR__ . '/tests/system/Config/fixtures',
+        ],
+    ])
+    ->cacheDirectory(is_dir('/tmp') ? '/tmp/structarmed' : null)
+    ->withPreset(Preset::PSR4());
