7445bd480b8522d978c6c1907b5cf2d9d920b2dd
patel-vansh <developer.patelvansh@gmail.com>
2026-06-05 09:13:43 +0530

    reorder system/Config/Filters.php

--- system/Config/Filters.php
+++ system/Config/Filters.php
@@ -66,13 +66,13 @@ class Filters extends BaseConfig
      */
     public array $required = [
         'before' => [
-            'forcehttps', // Force Global Secure Requests
             // 'requestid', // Request ID for each request
+            'forcehttps', // Force Global Secure Requests
             'pagecache',  // Web Page Caching
         ],
         'after' => [
-            'pagecache',   // Web Page Caching
             // 'requestid',  // Request ID for each request
+            'pagecache',   // Web Page Caching
             'performance', // Performance Metrics
             'toolbar',     // Debug Toolbar
         ],
