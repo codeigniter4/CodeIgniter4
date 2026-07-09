365f16caa0045c342af83c3219c711f18bf5fe1a
michalsn <michal@sniatala.pl>
2026-07-09 06:59:51 +0200

    cs fix

--- user_guide_src/source/outgoing/response/040.php
+++ user_guide_src/source/outgoing/response/040.php
@@ -3,6 +3,7 @@
 // Any iterable of string chunks works, including generators.
 $chunks = static function (): \Generator {
     yield json_encode(['event' => 'started']) . "\n";
+
     yield json_encode(['event' => 'finished']) . "\n";
 };
 
