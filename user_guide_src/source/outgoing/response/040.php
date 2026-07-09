539faf80c77e33fb9d62498d006189bcd9f5f029
michalsn <michal@sniatala.pl>
2026-07-09 06:58:21 +0200

    update user guide example

--- user_guide_src/source/outgoing/response/040.php
+++ user_guide_src/source/outgoing/response/040.php
@@ -2,9 +2,8 @@
 
 // Any iterable of string chunks works, including generators.
 $chunks = static function (): \Generator {
-    foreach (model('LogModel')->findAll() as $log) {
-        yield json_encode($log) . "\n";
-    }
+    yield json_encode(['event' => 'started']) . "\n";
+    yield json_encode(['event' => 'finished']) . "\n";
 };
 
 return $this->response->stream($chunks());
