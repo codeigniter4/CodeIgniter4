9a41ed099ad30897e21c47f3ca4f5f1fe5d385b1
Bogdan Lambarski <matolq@gmail.com>
2026-09-12 13:34:09 +0200

    Update system/I18n/TimeTrait.php
    Co-authored-by: Michal Sniatala <michal@sniatala.pl>

--- system/I18n/TimeTrait.php
+++ system/I18n/TimeTrait.php
@@ -172,7 +172,7 @@ trait TimeTrait
      */
     public static function tomorrow($timezone = null, ?string $locale = null)
     {
-        return (new static(null, $timezone, $locale))->modify('+1 day')->setTime(0, 0, 0, 0);
+        return (new static(null, $timezone, $locale))->modify('tomorrow');
     }
 
     /**
