afd7a36f3a0223b90ecf0c1ad67b8f1c6a1f10a7
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-09-12 20:13:30 +0700

    register baseline

--- structarmed.php
+++ structarmed.php
@@ -169,4 +169,5 @@ return Architecture::define()
     ->skipClassViolation(DownloadResponse::class, [PagerInterface::class])
     ->skipClassViolation(SSEResponse::class, [PagerInterface::class])
     ->skipClassViolation(StreamResponse::class, [PagerInterface::class])
-    ->skipClassViolation(Validation::class, [RendererInterface::class]);
+    ->skipClassViolation(Validation::class, [RendererInterface::class])
+    ->baseline(__DIR__ . '/structarmed-baseline.php');
